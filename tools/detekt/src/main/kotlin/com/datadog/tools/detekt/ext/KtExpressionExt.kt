6e7dd9db00286ecebe48574c69361757b8c7a701
Xavier Gouchet <xavier.gouchet@gmail.com>
2019-12-05 10:09:21 +0100

    :sparkles: (RUMM-51): Create custom detekt rules
    Prevent throwing exceptions or calling check/require from within the depth of the library

---
+++ tools/detekt/src/main/kotlin/com/datadog/tools/detekt/ext/KtExpressionExt.kt
@@ -0,0 +1,28 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2016-2019 Datadog, Inc.
+ */
+
+package com.datadog.tools.detekt.ext
+
+import org.jetbrains.kotlin.psi.KtExpression
+import org.jetbrains.kotlin.psi.psiUtil.containingClass
+import org.jetbrains.kotlin.psi.psiUtil.isPublic
+import org.jetbrains.kotlin.util.containingNonLocalDeclaration
+
+internal fun KtExpression.isContainingClassPublic(): Boolean {
+    return containingClass().let { ktClass ->
+        ktClass == null || ktClass.isPublic
+    }
+}
+
+internal fun KtExpression.isContainingDeclarationPublic(): Boolean {
+    return containingNonLocalDeclaration().let { ktDeclaration ->
+        ktDeclaration == null || ktDeclaration.isPublic
+    }
+}
+
+internal fun KtExpression.isContainingEntryPointPublic(): Boolean {
+    return isContainingClassPublic() && isContainingDeclarationPublic()
+}
