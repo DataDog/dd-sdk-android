c779cf270c1dd42a38fe4a2dc538c0188cb19e0a
Xavier Gouchet <xavier.gouchet@gmail.com>
2020-01-07 16:26:48 +0100

    :building_construction: (RUMM-159): Create ImmediateFileWriter

---
+++ dd-sdk-android/src/main/kotlin/com/datadog/android/core/internal/data/file/ImmediateFileWriter.kt
@@ -0,0 +1,78 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2016-2020 Datadog, Inc.
+ */
+
+package com.datadog.android.core.internal.data.file
+
+import com.datadog.android.core.internal.data.Orchestrator
+import com.datadog.android.core.internal.data.Writer
+import com.datadog.android.core.internal.domain.Serializer
+import com.datadog.android.log.internal.utils.devLogger
+import com.datadog.android.log.internal.utils.sdkLogger
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.IOException
+
+internal class ImmediateFileWriter<T : Any>(
+    private val fileOrchestrator: Orchestrator,
+    private val serializer: Serializer<T>
+) : Writer<T> {
+
+    // region Writer
+
+    override fun write(model: T) {
+        val data = serializer.serialize(model)
+
+        if (data.length >= MAX_ITEM_SIZE) {
+            devLogger.e("Unable to persist data, serialized size is too big\n$data")
+        } else {
+            synchronized(this) {
+                writeData(data)
+            }
+        }
+    }
+
+    // endregion
+
+    // region Internal
+
+    private fun writeData(data: String) {
+        var file: File? = null
+        try {
+            val dataAsByteArray = data.toByteArray(Charsets.UTF_8)
+            file = fileOrchestrator.getWritableFile(dataAsByteArray.size)
+            if (file != null) {
+                writeDataToFile(file, dataAsByteArray)
+            } else {
+                sdkLogger.e("$TAG: Could not get a valid file")
+            }
+        } catch (e: FileNotFoundException) {
+            sdkLogger.e("$TAG: Couldn't create an output stream to file ${file?.path}", e)
+        } catch (e: IOException) {
+            sdkLogger.e("$TAG: Couldn't write data to file ${file?.path}", e)
+        } catch (e: SecurityException) {
+            sdkLogger.e("$TAG: Couldn't access file ${file?.path}", e)
+        }
+    }
+
+    private fun writeDataToFile(file: File, dataAsByteArray: ByteArray) {
+        val fileSize = file.length()
+        FileOutputStream(file, true).use {
+            if (fileSize > 0) {
+                it.write(separator)
+            }
+            it.write(dataAsByteArray)
+        }
+    }
+
+    // endregion
+
+    companion object {
+        private const val MAX_ITEM_SIZE = 256 * 1024 // 256 Kb
+        private const val TAG = "ImmediateFileWriter"
+        private val separator = ByteArray(1) { ','.toByte() }
+    }
+}
---
+++ dd-sdk-android/src/test/kotlin/com/datadog/android/core/internal/data/file/ImmediateFileWriterTest.kt
@@ -0,0 +1,141 @@
+package com.datadog.android.core.internal.data.file
+
+import android.os.Build
+import com.datadog.android.core.internal.data.Orchestrator
+import com.datadog.android.core.internal.domain.Serializer
+import com.datadog.android.core.internal.threading.AndroidDeferredHandler
+import com.datadog.android.log.forge.Configurator
+import com.datadog.tools.unit.BuildConfig
+import com.datadog.tools.unit.annotations.SystemOutStream
+import com.datadog.tools.unit.annotations.TestTargetApi
+import com.datadog.tools.unit.extensions.ApiLevelExtension
+import com.datadog.tools.unit.extensions.SystemOutputExtension
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.doAnswer
+import com.nhaarman.mockitokotlin2.doThrow
+import com.nhaarman.mockitokotlin2.whenever
+import fr.xgouchet.elmyr.Forge
+import fr.xgouchet.elmyr.junit5.ForgeConfiguration
+import fr.xgouchet.elmyr.junit5.ForgeExtension
+import java.io.ByteArrayOutputStream
+import java.io.File
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.api.extension.Extensions
+import org.junit.jupiter.api.io.TempDir
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.junit.jupiter.MockitoSettings
+import org.mockito.quality.Strictness
+
+@Extensions(
+    ExtendWith(MockitoExtension::class),
+    ExtendWith(ForgeExtension::class),
+    ExtendWith(SystemOutputExtension::class),
+    ExtendWith(ApiLevelExtension::class)
+)
+@ForgeConfiguration(Configurator::class, seed = 0x146ba9a6L)
+@MockitoSettings(strictness = Strictness.LENIENT)
+internal class ImmediateFileWriterTest {
+
+    lateinit var underTest: ImmediateFileWriter<String>
+    @Mock
+    lateinit var mockedSerializer: Serializer<String>
+    @Mock
+    lateinit var mockedOrchestrator: Orchestrator
+    @Mock
+    lateinit var mockDeferredHandler: AndroidDeferredHandler
+
+    @TempDir
+    lateinit var rootDir: File
+
+    @BeforeEach
+    fun `set up`() {
+        whenever(mockedSerializer.serialize(any())).doAnswer {
+            it.getArgument(0)
+        }
+        whenever(mockDeferredHandler.handle(any())) doAnswer {
+            val runnable = it.arguments[0] as Runnable
+            runnable.run()
+        }
+        underTest = ImmediateFileWriter(
+            mockedOrchestrator,
+            mockedSerializer
+        )
+    }
+
+    @AfterEach
+    fun `tear down`() {
+        rootDir.deleteRecursively()
+    }
+
+    @Test
+    @TestTargetApi(Build.VERSION_CODES.O)
+    fun `writes a valid model`(forge: Forge) {
+        val model = forge.anAlphabeticalString()
+        val fileNameToWriteTo = forge.anAlphaNumericalString()
+        val file = File(rootDir, fileNameToWriteTo)
+        whenever(mockedOrchestrator.getWritableFile(any())).thenReturn(file)
+
+        underTest.write(model)
+
+        assertThat(file.readText())
+            .isEqualTo(model)
+    }
+
+    @Test
+    @TestTargetApi(Build.VERSION_CODES.O)
+    fun `writes several models`(forge: Forge) {
+        val models = forge.aList { anAlphabeticalString() }
+        val fileNameToWriteTo = forge.anAlphaNumericalString()
+        val file = File(rootDir, fileNameToWriteTo)
+        whenever(mockedOrchestrator.getWritableFile(any())).thenReturn(file)
+
+        models.forEach {
+            underTest.write(it)
+        }
+
+        assertThat(file.readText())
+            .isEqualTo(models.joinToString(","))
+    }
+
+    @Test
+    @TestTargetApi(Build.VERSION_CODES.O)
+    fun `does nothing when SecurityException was thrown while providing a file`(
+        forge: Forge,
+        @SystemOutStream outputStream: ByteArrayOutputStream
+    ) {
+        val modelValue = forge.anAlphabeticalString()
+        val exception = SecurityException(forge.anAlphabeticalString())
+        doThrow(exception).whenever(mockedOrchestrator).getWritableFile(any())
+
+        underTest.write(modelValue)
+
+        if (BuildConfig.DEBUG) {
+            val logMessages = outputStream.toString().trim().split("\n")
+            assertThat(logMessages[0]).matches("E/DD_LOG: ImmediateFileWriter: .*")
+        }
+    }
+
+    @Test
+    fun `does nothing when FileOrchestrator returns a null file`(
+        forge: Forge,
+        @SystemOutStream outputStream: ByteArrayOutputStream
+    ) {
+        val modelValue = forge.anAlphabeticalString()
+        whenever(mockedOrchestrator.getWritableFile(any())).thenReturn(null)
+
+        // when
+        underTest.write(modelValue)
+
+        // then
+        if (BuildConfig.DEBUG) {
+            val logMessages = outputStream.toString().trim().split("\n")
+            assertThat(logMessages[0])
+                .matches("E/DD_LOG: ImmediateFileWriter: Could not get a valid file")
+        }
+    }
+}
