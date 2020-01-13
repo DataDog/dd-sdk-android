2edd49ed85155d134e1eceb7bcd16a021152395f
Marius Constantin <marius.constantin@datadoghq.com>
2020-01-13 09:55:34 +0100

    RUMM-169 Introduce the ProcessLifecycleMonitor

---
+++ dd-sdk-android/src/main/kotlin/com/datadog/android/core/internal/lifecycle/ProcessLifecycleMonitor.kt
@@ -0,0 +1,70 @@
+package com.datadog.android.core.internal.lifecycle
+
+import android.app.Activity
+import android.app.Application
+import android.os.Bundle
+import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.atomic.AtomicInteger
+
+class ProcessLifecycleMonitor(val callback: Callback) : Application.ActivityLifecycleCallbacks {
+
+    val activitiesResumedCounter = AtomicInteger(0)
+    val activitiesStartedCounter = AtomicInteger(0)
+    val wasPaused = AtomicBoolean(true)
+    val wasStopped = AtomicBoolean(true)
+
+    override fun onActivityPaused(activity: Activity) {
+        if (activitiesResumedCounter.decrementAndGet() == 0 &&
+            !wasPaused.getAndSet(true)) {
+            // trigger on process paused
+            callback.onPaused()
+        }
+    }
+
+    override fun onActivityStarted(activity: Activity) {
+        if (activitiesStartedCounter.incrementAndGet() == 1 &&
+            wasStopped.getAndSet(false)
+        ) {
+            // trigger on process started
+            callback.onStarted()
+        }
+    }
+
+    override fun onActivityDestroyed(activity: Activity) {
+        //  NO-OP
+    }
+
+    override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
+        //  NO-OP
+    }
+
+    override fun onActivityStopped(activity: Activity) {
+        if (activitiesStartedCounter.decrementAndGet() == 0 && wasPaused.get()) {
+            // trigger on process stopped
+            callback.onStopped()
+            wasStopped.set(true)
+        }
+    }
+
+    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+        //  NO-OP
+    }
+
+    override fun onActivityResumed(activity: Activity) {
+        if (activitiesResumedCounter.incrementAndGet() == 1 &&
+            wasPaused.getAndSet(false)
+        ) {
+            callback.onResumed()
+        }
+    }
+
+    interface Callback {
+        fun onStarted()
+
+        fun onResumed()
+
+        fun onStopped()
+
+        fun onPaused()
+    }
+}
