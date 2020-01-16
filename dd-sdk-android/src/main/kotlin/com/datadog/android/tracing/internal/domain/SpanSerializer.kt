ab98331ccd4576a395d9d3e2f360fc536a720f4d
Marius Constantin <marius.constantin@datadoghq.com>
2020-01-16 16:14:03 +0100

    RUMM-173 Introduce the TracingFileStrategy

--- dd-sdk-android/src/main/kotlin/com/datadog/android/tracing/internal/domain/SpanSerializer.kt
+++ dd-sdk-android/src/main/kotlin/com/datadog/android/tracing/internal/domain/SpanSerializer.kt
@@ -5,10 +5,21 @@ import com.fasterxml.jackson.core.JsonFactory
 import com.fasterxml.jackson.databind.ObjectMapper
 import datadog.opentracing.DDSpan
 
-class SpanSerializer : Serializer<DDSpan> {
+internal class SpanSerializer : Serializer<DDSpan> {
     private val mapper: ObjectMapper = ObjectMapper(JsonFactory())
 
     override fun serialize(model: DDSpan): String {
         return mapper.writeValueAsString(model)
+    }
+
+    companion object {
+        const val START_TIMESTAMP_KEY = "start"
+        const val DURATION_KEY = "duration"
+        const val SERVICE_NAME_KEY = "service"
+        const val TRACE_ID_KEY = "trace_id"
+        const val SPAN_ID_KEY = "span_id"
+        const val PARENT_ID_KEY = "parent_id"
+        const val RESOURCE_KEY = "resource"
+        const val OPERATION_NAME_KEY = "name"
     }
 }
