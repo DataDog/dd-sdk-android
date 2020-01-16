ad58c2c7a21dceefd9046464486d24fc4a241e1f
Marius Constantin <marius.constantin@datadoghq.com>
2020-01-16 16:14:03 +0100

    RUMM-173 Introduce the TracingFileStrategy

--- dd-sdk-android/src/test/kotlin/com/datadog/android/utils/extension/JsonObjectExt.kt
+++ dd-sdk-android/src/test/kotlin/com/datadog/android/utils/extension/JsonObjectExt.kt
@@ -1,28 +1,18 @@
 package com.datadog.android.utils.extension
 
 import com.datadog.tools.unit.assertj.JsonObjectAssert
+import com.datadog.android.tracing.internal.domain.SpanSerializer
 import com.google.gson.JsonObject
 import datadog.opentracing.DDSpan
 
 fun JsonObject.assertMatches(span: DDSpan) {
     JsonObjectAssert.assertThat(this)
-        .hasField(SpanKeys.START_TIMESTAMP_KEY, span.startTime)
-        .hasField(SpanKeys.DURATION_KEY, span.durationNano)
-        .hasField(SpanKeys.SERVICE_NAME_KEY, span.serviceName)
-        .hasField(SpanKeys.TRACE_ID_KEY, span.traceId)
-        .hasField(SpanKeys.SPAN_ID_KEY, span.spanId)
-        .hasField(SpanKeys.PARENT_ID_KEY, span.parentId)
-        .hasField(SpanKeys.RESOURCE_KEY, span.resourceName)
-        .hasField(SpanKeys.OPERATION_NAME_KEY, span.operationName)
-}
-
-object SpanKeys {
-    const val START_TIMESTAMP_KEY = "start"
-    const val DURATION_KEY = "duration"
-    const val SERVICE_NAME_KEY = "service"
-    const val TRACE_ID_KEY = "trace_id"
-    const val SPAN_ID_KEY = "span_id"
-    const val PARENT_ID_KEY = "parent_id"
-    const val RESOURCE_KEY = "resource"
-    const val OPERATION_NAME_KEY = "name"
+        .hasField(SpanSerializer.START_TIMESTAMP_KEY, span.startTime)
+        .hasField(SpanSerializer.DURATION_KEY, span.durationNano)
+        .hasField(SpanSerializer.SERVICE_NAME_KEY, span.serviceName)
+        .hasField(SpanSerializer.TRACE_ID_KEY, span.traceId)
+        .hasField(SpanSerializer.SPAN_ID_KEY, span.spanId)
+        .hasField(SpanSerializer.PARENT_ID_KEY, span.parentId)
+        .hasField(SpanSerializer.RESOURCE_KEY, span.resourceName)
+        .hasField(SpanSerializer.OPERATION_NAME_KEY, span.operationName)
 }
