d8f05149e69b31ee4212bdaf352282f6dd0ee906
Marius Constantin <marius.constantin@datadoghq.com>
2020-01-17 11:03:48 +0100

    RUMM-173 Rework the Tracer Public API

--- dd-sdk-android/src/test/kotlin/com/datadog/android/tracing/internal/TracerTest.kt
+++ dd-sdk-android/src/test/kotlin/com/datadog/android/tracing/internal/TracerTest.kt
@@ -2,7 +2,7 @@ package com.datadog.android.tracing.internal
 
 import android.app.Application
 import com.datadog.android.Datadog
-import com.datadog.android.tracing.TracerBuilder
+import com.datadog.android.tracing.Tracer
 import com.datadog.android.tracing.assertj.TracerConfigAssert.Companion.assertThat
 import com.datadog.android.utils.forge.Configurator
 import com.datadog.android.utils.mockContext
@@ -27,9 +27,9 @@ import org.mockito.quality.Strictness
 
 @MockitoSettings(strictness = Strictness.STRICT_STUBS)
 @ForgeConfiguration(Configurator::class)
-internal class AndroidTracerBuilderTest {
+internal class TracerTest {
 
-    lateinit var underTest: TracerBuilder
+    lateinit var underTest: Tracer.TracerBuilder
     lateinit var mockAppContext: Application
     lateinit var fakeToken: String
     lateinit var fakeServiceName: String
@@ -40,7 +40,6 @@ internal class AndroidTracerBuilderTest {
         fakeToken = forge.anHexadecimalString()
         mockAppContext = mockContext()
         Datadog.initialize(mockAppContext, fakeToken)
-        underTest = Datadog.tracerBuilder(fakeServiceName)
     }
 
     @AfterEach
@@ -49,13 +48,30 @@ internal class AndroidTracerBuilderTest {
     }
 
     @Test
-    fun `it will build a valid DDTracer`(forge: Forge) {
+    fun `it will build a valid Tracer`(forge: Forge) {
+        // given
+        underTest = Tracer.TracerBuilder()
+
         // when
-        val tracer = underTest.build()
-        val config = (underTest as AndroidTracerBuilder).config()
+        val tracer = underTest.setServiceName(fakeServiceName).build()
+        val config = underTest.config()
 
         // then
         assertThat(tracer).isNotNull()
         assertThat(config).hasServiceName(fakeServiceName)
+    }
+
+    @Test
+    fun `it will build a valid Tracer with a default service name if not provided`(forge: Forge) {
+        // given
+        underTest = Tracer.TracerBuilder()
+
+        // when
+        val tracer = underTest.build()
+        val config = underTest.config()
+
+        // then
+        assertThat(tracer).isNotNull()
+        assertThat(config).hasServiceName(Tracer.DEFAULT_SERVICE_NAME)
     }
 }
