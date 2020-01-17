17bf160255300b218448267964a15002b84a60f4
Marius Constantin <marius.constantin@datadoghq.com>
2020-01-17 16:54:11 +0100

    RUMM-173 Extract more abstractions into the FilePersistenceStrategy

--- dd-sdk-android/src/main/kotlin/com/datadog/android/tracing/internal/domain/TracingFileStrategy.kt
+++ dd-sdk-android/src/main/kotlin/com/datadog/android/tracing/internal/domain/TracingFileStrategy.kt
@@ -9,7 +9,7 @@ internal class TracingFileStrategy(
     context: Context,
     recentDelayMs: Long = MAX_DELAY_BETWEEN_LOGS_MS,
     maxBatchSize: Long = MAX_BATCH_SIZE,
-    maxLogPerBatch: Int = MAX_SPANS_PER_BATCH,
+    maxLogPerBatch: Int = MAX_ITEMS_PER_BATCH,
     oldFileThreshold: Long = OLD_FILE_THRESHOLD,
     maxDiskSpace: Long = MAX_DISK_SPACE
 ) : FilePersistenceStrategy<DDSpan>(
@@ -17,21 +17,16 @@ internal class TracingFileStrategy(
         context.filesDir,
         TRACES_FOLDER
     ),
+    SpanSerializer(),
+    WRITER_THREAD_NAME,
     recentDelayMs,
     maxBatchSize,
     maxLogPerBatch,
     oldFileThreshold,
-    maxDiskSpace,
-    SpanSerializer(),
-    WRITER_THREAD_NAME
+    maxDiskSpace
 ) {
 
     companion object {
-        private const val MAX_BATCH_SIZE: Long = 4 * 1024 * 1024 // 4 MB
-        internal const val MAX_SPANS_PER_BATCH: Int = 500
-        private const val OLD_FILE_THRESHOLD: Long = 18L * 60L * 60L * 1000L // 18 hours
-        private const val MAX_DISK_SPACE: Long = 128 * MAX_BATCH_SIZE // 512 MB
-
         internal const val TRACES_DATA_VERSION = 1
         internal const val DATA_FOLDER_ROOT = "dd-tracing"
         internal const val TRACES_FOLDER = "$DATA_FOLDER_ROOT-v$TRACES_DATA_VERSION"
