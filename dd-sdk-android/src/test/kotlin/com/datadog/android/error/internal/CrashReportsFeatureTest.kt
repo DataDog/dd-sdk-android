0d50d000a88f09a487007a8505de9939533f5617
Xavier Gouchet <xavier.gouchet@gmail.com>
2020-01-30 17:14:30 +0100

    :sparkles: (RUMM-97): Create an initializer object per feature

---
+++ dd-sdk-android/src/test/kotlin/com/datadog/android/error/internal/CrashReportsFeatureTest.kt
@@ -0,0 +1,236 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2016-2020 Datadog, Inc.
+ */
+
+package com.datadog.android.error.internal
+
+import android.app.Application
+import com.datadog.android.Datadog
+import com.datadog.android.DatadogConfig
+import com.datadog.android.DatadogEndpoint
+import com.datadog.android.core.internal.domain.FilePersistenceStrategy
+import com.datadog.android.core.internal.domain.NoOpPersistenceStrategy
+import com.datadog.android.core.internal.net.DataOkHttpUploader
+import com.datadog.android.core.internal.net.NoOpDataUploader
+import com.datadog.android.core.internal.net.info.NetworkInfoProvider
+import com.datadog.android.core.internal.time.TimeProvider
+import com.datadog.android.log.internal.user.UserInfoProvider
+import com.datadog.android.utils.forge.Configurator
+import com.datadog.android.utils.mockContext
+import com.datadog.tools.unit.extensions.ApiLevelExtension
+import com.datadog.tools.unit.extensions.SystemOutputExtension
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import fr.xgouchet.elmyr.Forge
+import fr.xgouchet.elmyr.junit5.ForgeConfiguration
+import fr.xgouchet.elmyr.junit5.ForgeExtension
+import java.io.File
+import java.net.URL
+import okhttp3.OkHttpClient
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.api.extension.Extensions
+import org.junit.jupiter.api.io.TempDir
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.junit.jupiter.MockitoSettings
+import org.mockito.quality.Strictness
+
+@Extensions(
+    ExtendWith(MockitoExtension::class),
+    ExtendWith(ForgeExtension::class),
+    ExtendWith(ApiLevelExtension::class),
+    ExtendWith(SystemOutputExtension::class)
+)
+@MockitoSettings(strictness = Strictness.LENIENT)
+@ForgeConfiguration(Configurator::class)
+internal class CrashReportsFeatureTest {
+
+    lateinit var mockAppContext: Application
+
+    @Mock
+    lateinit var mockNetworkInfoProvider: NetworkInfoProvider
+    @Mock
+    lateinit var mockUserInfoProvider: UserInfoProvider
+    @Mock
+    lateinit var mockTimeProvider: TimeProvider
+    @Mock
+    lateinit var mockOkHttpClient: OkHttpClient
+
+    lateinit var fakeConfig: DatadogConfig.FeatureConfig
+
+    lateinit var fakePackageName: String
+    lateinit var fakePackageVersion: String
+
+    @TempDir
+    lateinit var rootDir: File
+
+    @BeforeEach
+    fun `set up`(forge: Forge) {
+        fakeConfig = DatadogConfig.FeatureConfig(
+            clientToken = forge.anHexadecimalString(),
+            endpointUrl = forge.getForgery<URL>().toString(),
+            serviceName = forge.anAlphabeticalString()
+        )
+
+        fakePackageName = forge.anAlphabeticalString()
+        fakePackageVersion = forge.aStringMatching("\\d(\\.\\d){3}")
+
+        mockAppContext = mockContext(fakePackageName, fakePackageVersion)
+        whenever(mockAppContext.filesDir).thenReturn(rootDir)
+        whenever(mockAppContext.applicationContext) doReturn mockAppContext
+    }
+
+    @AfterEach
+    fun `tear down`() {
+        CrashReportsFeature.stop()
+
+        CrashReportsFeature.persistenceStrategy = NoOpPersistenceStrategy()
+        CrashReportsFeature.uploader = NoOpDataUploader()
+        CrashReportsFeature.clientToken = ""
+        CrashReportsFeature.endpointUrl = DatadogEndpoint.LOGS_US
+        CrashReportsFeature.serviceName = DatadogConfig.DEFAULT_SERVICE_NAME
+    }
+
+    @Test
+    fun `initializes persistence strategy`() {
+        CrashReportsFeature.initialize(
+            mockAppContext,
+            fakeConfig,
+            mockOkHttpClient,
+            mockNetworkInfoProvider,
+            mockUserInfoProvider,
+            mockTimeProvider
+        )
+
+        val persistenceStrategy = CrashReportsFeature.persistenceStrategy
+
+        assertThat(persistenceStrategy)
+            .isInstanceOf(FilePersistenceStrategy::class.java)
+    }
+
+    @Test
+    fun `initializes uploader thread`() {
+        CrashReportsFeature.initialize(
+            mockAppContext,
+            fakeConfig,
+            mockOkHttpClient,
+            mockNetworkInfoProvider,
+            mockUserInfoProvider,
+            mockTimeProvider
+        )
+
+        val uploader = CrashReportsFeature.uploader
+
+        assertThat(uploader)
+            .isInstanceOf(DataOkHttpUploader::class.java)
+    }
+
+    @Test
+    fun `initializes from configuration`() {
+        CrashReportsFeature.initialize(
+            mockAppContext,
+            fakeConfig,
+            mockOkHttpClient,
+            mockNetworkInfoProvider,
+            mockUserInfoProvider,
+            mockTimeProvider
+        )
+
+        val clientToken = CrashReportsFeature.clientToken
+        val endpointUrl = CrashReportsFeature.endpointUrl
+        val serviceName = CrashReportsFeature.serviceName
+
+        assertThat(clientToken).isEqualTo(fakeConfig.clientToken)
+        assertThat(endpointUrl).isEqualTo(fakeConfig.endpointUrl)
+        assertThat(serviceName).isEqualTo(fakeConfig.serviceName)
+    }
+
+    @Test
+    fun `initializes crash reporter`() {
+        CrashReportsFeature.initialize(
+            mockAppContext,
+            fakeConfig,
+            mockOkHttpClient,
+            mockNetworkInfoProvider,
+            mockUserInfoProvider,
+            mockTimeProvider
+        )
+
+        val handler = Thread.getDefaultUncaughtExceptionHandler()
+
+        assertThat(handler)
+            .isInstanceOf(DatadogExceptionHandler::class.java)
+    }
+
+    @Test
+    fun `restores original crash reporter on stop`() {
+        val originalHandler = Thread.getDefaultUncaughtExceptionHandler()
+        val handler: Thread.UncaughtExceptionHandler = mock()
+        Thread.setDefaultUncaughtExceptionHandler(handler)
+
+        CrashReportsFeature.initialize(
+            mockAppContext,
+            fakeConfig,
+            mockOkHttpClient,
+            mockNetworkInfoProvider,
+            mockUserInfoProvider,
+            mockTimeProvider
+        )
+        CrashReportsFeature.stop()
+
+        val finalHandler = Thread.getDefaultUncaughtExceptionHandler()
+        assertThat(finalHandler)
+            .isSameAs(handler)
+        Thread.setDefaultUncaughtExceptionHandler(originalHandler)
+    }
+
+    @Test
+    fun `ignores if initialize called more than once`(forge: Forge) {
+        Datadog.setVerbosity(android.util.Log.VERBOSE)
+        CrashReportsFeature.initialize(
+            mockAppContext,
+            fakeConfig,
+            mockOkHttpClient,
+            mockNetworkInfoProvider,
+            mockUserInfoProvider,
+            mockTimeProvider
+        )
+        val persistenceStrategy = CrashReportsFeature.persistenceStrategy
+        val uploader = CrashReportsFeature.uploader
+        val clientToken = CrashReportsFeature.clientToken
+        val endpointUrl = CrashReportsFeature.endpointUrl
+        val serviceName = CrashReportsFeature.serviceName
+
+        fakeConfig = DatadogConfig.FeatureConfig(
+            clientToken = forge.anHexadecimalString(),
+            endpointUrl = forge.getForgery<URL>().toString(),
+            serviceName = forge.anAlphabeticalString()
+        )
+        CrashReportsFeature.initialize(
+            mockAppContext,
+            fakeConfig,
+            mockOkHttpClient,
+            mockNetworkInfoProvider,
+            mockUserInfoProvider,
+            mockTimeProvider
+        )
+        val persistenceStrategy2 = CrashReportsFeature.persistenceStrategy
+        val uploader2 = CrashReportsFeature.uploader
+        val clientToken2 = CrashReportsFeature.clientToken
+        val endpointUrl2 = CrashReportsFeature.endpointUrl
+        val serviceName2 = CrashReportsFeature.serviceName
+
+        assertThat(persistenceStrategy).isSameAs(persistenceStrategy2)
+        assertThat(uploader).isSameAs(uploader2)
+        assertThat(clientToken).isSameAs(clientToken2)
+        assertThat(endpointUrl).isSameAs(endpointUrl2)
+        assertThat(serviceName).isSameAs(serviceName2)
+    }
+}
---
+++ dd-sdk-android/src/test/kotlin/com/datadog/android/tracing/internal/TracesFeatureTest.kt
@@ -0,0 +1,196 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2016-2020 Datadog, Inc.
+ */
+
+package com.datadog.android.tracing.internal
+
+import android.app.Application
+import android.os.HandlerThread
+import com.datadog.android.Datadog
+import com.datadog.android.DatadogConfig
+import com.datadog.android.DatadogEndpoint
+import com.datadog.android.core.internal.data.upload.DataUploadHandlerThread
+import com.datadog.android.core.internal.domain.AsyncWriterFilePersistenceStrategy
+import com.datadog.android.core.internal.domain.NoOpPersistenceStrategy
+import com.datadog.android.core.internal.net.info.NetworkInfoProvider
+import com.datadog.android.core.internal.system.SystemInfoProvider
+import com.datadog.android.core.internal.time.TimeProvider
+import com.datadog.android.utils.forge.Configurator
+import com.datadog.android.utils.mockContext
+import com.datadog.tools.unit.extensions.ApiLevelExtension
+import com.datadog.tools.unit.extensions.SystemOutputExtension
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.whenever
+import datadog.opentracing.DDSpan
+import fr.xgouchet.elmyr.Forge
+import fr.xgouchet.elmyr.junit5.ForgeConfiguration
+import fr.xgouchet.elmyr.junit5.ForgeExtension
+import java.io.File
+import java.net.URL
+import okhttp3.OkHttpClient
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.api.extension.Extensions
+import org.junit.jupiter.api.io.TempDir
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.junit.jupiter.MockitoSettings
+import org.mockito.quality.Strictness
+
+@Extensions(
+    ExtendWith(MockitoExtension::class),
+    ExtendWith(ForgeExtension::class),
+    ExtendWith(ApiLevelExtension::class),
+    ExtendWith(SystemOutputExtension::class)
+)
+@MockitoSettings(strictness = Strictness.LENIENT)
+@ForgeConfiguration(Configurator::class)
+internal class TracesFeatureTest {
+
+    lateinit var mockAppContext: Application
+
+    @Mock
+    lateinit var mockNetworkInfoProvider: NetworkInfoProvider
+    @Mock
+    lateinit var mockSystemInfoProvider: SystemInfoProvider
+    @Mock
+    lateinit var mockTimeProvider: TimeProvider
+    @Mock
+    lateinit var mockOkHttpClient: OkHttpClient
+
+    lateinit var fakeConfig: DatadogConfig.FeatureConfig
+
+    lateinit var fakePackageName: String
+    lateinit var fakePackageVersion: String
+
+    @TempDir
+    lateinit var rootDir: File
+
+    @BeforeEach
+    fun `set up`(forge: Forge) {
+        fakeConfig = DatadogConfig.FeatureConfig(
+            clientToken = forge.anHexadecimalString(),
+            endpointUrl = forge.getForgery<URL>().toString(),
+            serviceName = forge.anAlphabeticalString()
+        )
+
+        fakePackageName = forge.anAlphabeticalString()
+        fakePackageVersion = forge.aStringMatching("\\d(\\.\\d){3}")
+
+        mockAppContext = mockContext(fakePackageName, fakePackageVersion)
+        whenever(mockAppContext.filesDir).thenReturn(rootDir)
+        whenever(mockAppContext.applicationContext) doReturn mockAppContext
+    }
+
+    @AfterEach
+    fun `tear down`() {
+        TracesFeature.stop()
+
+        TracesFeature.persistenceStrategy = NoOpPersistenceStrategy<DDSpan>()
+        TracesFeature.uploadHandlerThread = HandlerThread("Test")
+        TracesFeature.clientToken = ""
+        TracesFeature.endpointUrl = DatadogEndpoint.TRACES_US
+        TracesFeature.serviceName = DatadogConfig.DEFAULT_SERVICE_NAME
+    }
+
+    @Test
+    fun `initializes persistence strategy`() {
+        TracesFeature.initialize(
+            mockAppContext,
+            fakeConfig,
+            mockOkHttpClient,
+            mockNetworkInfoProvider,
+            mockSystemInfoProvider,
+            mockTimeProvider
+        )
+
+        val persistenceStrategy = TracesFeature.persistenceStrategy
+        assertThat(persistenceStrategy)
+            .isInstanceOf(AsyncWriterFilePersistenceStrategy::class.java)
+    }
+
+    @Test
+    fun `initializes uploader thread`() {
+        TracesFeature.initialize(
+            mockAppContext,
+            fakeConfig,
+            mockOkHttpClient,
+            mockNetworkInfoProvider,
+            mockSystemInfoProvider,
+            mockTimeProvider
+        )
+
+        val uploadHandlerThread = TracesFeature.uploadHandlerThread
+
+        assertThat(uploadHandlerThread)
+            .isInstanceOf(DataUploadHandlerThread::class.java)
+    }
+
+    @Test
+    fun `initializes from configuration`() {
+        TracesFeature.initialize(
+            mockAppContext,
+            fakeConfig,
+            mockOkHttpClient,
+            mockNetworkInfoProvider,
+            mockSystemInfoProvider,
+            mockTimeProvider
+        )
+
+        val clientToken = TracesFeature.clientToken
+        val endpointUrl = TracesFeature.endpointUrl
+        val serviceName = TracesFeature.serviceName
+
+        assertThat(clientToken).isEqualTo(fakeConfig.clientToken)
+        assertThat(endpointUrl).isEqualTo(fakeConfig.endpointUrl)
+        assertThat(serviceName).isEqualTo(fakeConfig.serviceName)
+    }
+
+    @Test
+    fun `ignores if initialize called more than once`(forge: Forge) {
+        Datadog.setVerbosity(android.util.Log.VERBOSE)
+        TracesFeature.initialize(
+            mockAppContext,
+            fakeConfig,
+            mockOkHttpClient,
+            mockNetworkInfoProvider,
+            mockSystemInfoProvider,
+            mockTimeProvider
+        )
+        val persistenceStrategy = TracesFeature.persistenceStrategy
+        val uploadHandlerThread = TracesFeature.uploadHandlerThread
+        val clientToken = TracesFeature.clientToken
+        val endpointUrl = TracesFeature.endpointUrl
+        val serviceName = TracesFeature.serviceName
+
+        fakeConfig = DatadogConfig.FeatureConfig(
+            clientToken = forge.anHexadecimalString(),
+            endpointUrl = forge.getForgery<URL>().toString(),
+            serviceName = forge.anAlphabeticalString()
+        )
+        TracesFeature.initialize(
+            mockAppContext,
+            fakeConfig,
+            mockOkHttpClient,
+            mockNetworkInfoProvider,
+            mockSystemInfoProvider,
+            mockTimeProvider
+        )
+        val persistenceStrategy2 = TracesFeature.persistenceStrategy
+        val uploadHandlerThread2 = TracesFeature.uploadHandlerThread
+        val clientToken2 = TracesFeature.clientToken
+        val endpointUrl2 = TracesFeature.endpointUrl
+        val serviceName2 = TracesFeature.serviceName
+
+        assertThat(persistenceStrategy).isSameAs(persistenceStrategy2)
+        assertThat(uploadHandlerThread).isSameAs(uploadHandlerThread2)
+        assertThat(clientToken).isSameAs(clientToken2)
+        assertThat(endpointUrl).isSameAs(endpointUrl2)
+        assertThat(serviceName).isSameAs(serviceName2)
+    }
+}
