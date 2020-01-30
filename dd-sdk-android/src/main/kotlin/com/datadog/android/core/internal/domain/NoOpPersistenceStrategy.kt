92e6ecae8ffe5d8f97afde8b546ef9130b60d1b7
Xavier Gouchet <xavier.gouchet@gmail.com>
2020-01-30 17:11:40 +0100

    :sparkles: (RUMM-97): Create NoOp implementation of all interfaces

---
+++ dd-sdk-android/src/main/kotlin/com/datadog/android/core/internal/domain/NoOpPersistenceStrategy.kt
@@ -0,0 +1,42 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2016-2020 Datadog, Inc.
+ */
+
+package com.datadog.android.core.internal.domain
+
+import com.datadog.android.core.internal.data.Reader
+import com.datadog.android.core.internal.data.Writer
+
+internal class NoOpPersistenceStrategy<T : Any> :
+    PersistenceStrategy<T> {
+
+    override fun getWriter(): Writer<T> {
+        return object : Writer<T> {
+            override fun write(model: T) {
+                // No Op
+            }
+        }
+    }
+
+    override fun getReader(): Reader {
+        return object : Reader {
+            override fun readNextBatch(): Batch? {
+                return null
+            }
+
+            override fun releaseBatch(batchId: String) {
+                // No Op
+            }
+
+            override fun dropBatch(batchId: String) {
+                // No Op
+            }
+
+            override fun dropAllBatches() {
+                // No Op
+            }
+        }
+    }
+}
