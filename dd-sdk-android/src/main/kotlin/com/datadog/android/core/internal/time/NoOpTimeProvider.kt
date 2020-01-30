0b46c81854c40a747048506c80b7d6e3a86b726d
Xavier Gouchet <xavier.gouchet@gmail.com>
2020-01-30 17:11:40 +0100

    :sparkles: (RUMM-97): Create NoOp implementation of all interfaces

---
+++ dd-sdk-android/src/main/kotlin/com/datadog/android/core/internal/time/NoOpTimeProvider.kt
@@ -0,0 +1,34 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2016-2020 Datadog, Inc.
+ */
+
+package com.datadog.android.core.internal.time
+
+internal class NoOpTimeProvider : MutableTimeProvider {
+
+    // region MutableTimeProvider
+
+    override fun updateOffset(offsetMs: Long) {
+        // No Op
+    }
+
+    // endregion
+
+    // region TimeProvider
+
+    override fun getDeviceTimestamp(): Long {
+        return System.currentTimeMillis()
+    }
+
+    override fun getServerTimestamp(): Long {
+        return System.currentTimeMillis()
+    }
+
+    override fun getServerOffsetNanos(): Long {
+        return 0
+    }
+
+    // endregion
+}
