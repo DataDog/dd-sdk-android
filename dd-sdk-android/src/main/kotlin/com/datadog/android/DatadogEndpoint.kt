bf23be0572dcb47f42256b2eb2f8dc7812d0d579
Xavier Gouchet <xavier.gouchet@gmail.com>
2020-01-30 17:12:52 +0100

    :sparkles: (RUMM-193): Create DatadogConfig.Builder

---
+++ dd-sdk-android/src/main/kotlin/com/datadog/android/DatadogEndpoint.kt
@@ -0,0 +1,43 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2016-2020 Datadog, Inc.
+ */
+
+package com.datadog.android
+
+import com.datadog.android.Datadog.initialize
+
+/**
+ * This object contains constant values for all the Datadog Endpoint urls used in the SDK.
+ */
+object DatadogEndpoint {
+
+    /**
+     * The endpoint for Logs (US based servers), used by default by the SDK.
+     * @see [DatadogConfig]
+     */
+    const val LOGS_US: String = "https://mobile-http-intake.logs.datadoghq.com"
+
+    /**
+     * The endpoint for Logs (Europe based servers).
+     * Use this in your [DatadogConfig] if you log on
+     * [app.datadoghq.eu](https://app.datadoghq.eu/) instead of
+     * [app.datadoghq.com](https://app.datadoghq.com/)
+     */
+    const val LOGS_EU: String = "https://mobile-http-intake.logs.datadoghq.eu"
+
+    /**
+     * The endpoint for Traces (US based servers), used by default by the SDK.
+     * @see [initialize]
+     */
+    const val TRACES_US: String = "https://public-trace-http-intake.logs.datadoghq.com"
+
+    /**
+     * The endpoint for Traces (Europe based servers).
+     * Use this in your [DatadogConfig] if you log on
+     * [app.datadoghq.eu](https://app.datadoghq.eu/) instead of
+     * [app.datadoghq.com](https://app.datadoghq.com/)
+     */
+    const val TRACES_EU: String = "https://public-trace-http-intake.logs.datadoghq.eu"
+}
