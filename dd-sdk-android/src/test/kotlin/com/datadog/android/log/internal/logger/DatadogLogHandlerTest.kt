60249b9d2a6c2e364db7145e49565281fd4b5b08
Marius Constantin <marius.constantin@datadoghq.com>
2020-02-03 15:30:26 +0100

    RUMM-186 Add traceId, spanId into the Log model

--- dd-sdk-android/src/test/kotlin/com/datadog/android/log/internal/logger/DatadogLogHandlerTest.kt
+++ dd-sdk-android/src/test/kotlin/com/datadog/android/log/internal/logger/DatadogLogHandlerTest.kt
@@ -6,6 +6,8 @@
 
 package com.datadog.android.log.internal.logger
 
+import com.datadog.android.Datadog
+import com.datadog.android.DatadogConfig
 import com.datadog.android.core.internal.data.Writer
 import com.datadog.android.core.internal.net.info.NetworkInfo
 import com.datadog.android.core.internal.net.info.NetworkInfoProvider
@@ -14,8 +16,12 @@ import com.datadog.android.log.assertj.LogAssert.Companion.assertThat
 import com.datadog.android.log.internal.domain.Log
 import com.datadog.android.log.internal.user.UserInfo
 import com.datadog.android.log.internal.user.UserInfoProvider
+import com.datadog.android.tracing.Tracer
 import com.datadog.android.utils.forge.Configurator
+import com.datadog.android.utils.mockContext
+import com.datadog.tools.unit.invokeMethod
 import com.datadog.tools.unit.setFieldValue
+import com.datadog.tools.unit.setStaticValue
 import com.nhaarman.mockitokotlin2.argumentCaptor
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.verify
@@ -25,8 +31,11 @@ import fr.xgouchet.elmyr.annotation.Forgery
 import fr.xgouchet.elmyr.junit5.ForgeConfiguration
 import fr.xgouchet.elmyr.junit5.ForgeExtension
 import fr.xgouchet.elmyr.jvm.ext.aTimestamp
+import io.opentracing.noop.NoopTracerFactory
+import io.opentracing.util.GlobalTracer
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -92,6 +101,12 @@ internal class DatadogLogHandlerTest {
             mockTimeProvider,
             mockUserInfoProvider
         )
+    }
+
+    @AfterEach
+    fun `tear down`() {
+        GlobalTracer.get().setFieldValue("isRegistered", false)
+        GlobalTracer::class.java.setStaticValue("tracer", NoopTracerFactory.create())
     }
 
     @Test
@@ -212,6 +227,88 @@ internal class DatadogLogHandlerTest {
                 .hasUserInfo(fakeUserInfo)
                 .hasAttributes(emptyMap())
                 .hasTags(emptyList())
+        }
+    }
+
+    @Test
+    fun `it will add the span id and trace id if we active an active tracer`(forge: Forge) {
+        // given
+        val config = DatadogConfig.Builder(forge.anAlphabeticalString()).build()
+        Datadog.initialize(mockContext(), config)
+        val tracer = Tracer.Builder().build()
+        val span = tracer.buildSpan(forge.anAlphabeticalString()).start()
+        tracer.activateSpan(span)
+        GlobalTracer.registerIfAbsent(tracer)
+
+        // when
+        testedHandler.handleLog(
+            fakeLevel,
+            fakeMessage,
+            fakeThrowable,
+            fakeAttributes,
+            fakeTags
+        )
+
+        // then
+        argumentCaptor<Log>().apply {
+            verify(mockWriter).write(capture())
+
+            assertThat(lastValue)
+                .hasTraceId(tracer.traceId)
+                .hasSpanId(tracer.spanId)
+        }
+        Datadog.invokeMethod("stop")
+    }
+
+    @Test
+    fun `it will not add trace deps if we do not have active an active tracer`(forge: Forge) {
+        // when
+        testedHandler.handleLog(
+            fakeLevel,
+            fakeMessage,
+            fakeThrowable,
+            fakeAttributes,
+            fakeTags
+        )
+
+        // then
+        argumentCaptor<Log>().apply {
+            verify(mockWriter).write(capture())
+
+            assertThat(lastValue)
+                .hasTraceId(null)
+                .hasSpanId(null)
+        }
+    }
+
+    @Test
+    fun `it will not add trace deps if the flag was set to false`(forge: Forge) {
+        // given
+        testedHandler = DatadogLogHandler(
+            fakeServiceName,
+            fakeLoggerName,
+            mockWriter,
+            mockNetworkInfoProvider,
+            mockTimeProvider,
+            mockUserInfoProvider,
+            linkIntoCurrentTrace = false
+        )
+        // when
+        testedHandler.handleLog(
+            fakeLevel,
+            fakeMessage,
+            fakeThrowable,
+            fakeAttributes,
+            fakeTags
+        )
+
+        // then
+        argumentCaptor<Log>().apply {
+            verify(mockWriter).write(capture())
+
+            assertThat(lastValue)
+                .hasTraceId(null)
+                .hasSpanId(null)
         }
     }
 }
