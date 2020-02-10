73c041eac545c2b919d08af52e900dc6eede9276
Marius Constantin <marius.constantin@datadoghq.com>
2020-02-10 15:21:16 +0100

    RUMM-209 Add user agent into traces intake request

--- dd-sdk-android/src/main/kotlin/com/datadog/android/log/internal/net/LogsOkHttpUploader.kt
+++ dd-sdk-android/src/main/kotlin/com/datadog/android/log/internal/net/LogsOkHttpUploader.kt
@@ -1,7 +1,5 @@
 package com.datadog.android.log.internal.net
 
-import android.os.Build
-import com.datadog.android.BuildConfig
 import com.datadog.android.core.internal.net.DataOkHttpUploader
 import java.util.Locale
 import okhttp3.OkHttpClient
@@ -14,35 +12,13 @@ internal open class LogsOkHttpUploader(
 
     // region DataOkHttpUploader
 
-    override fun headers(): MutableMap<String, String> {
-        val headers = super.headers()
-        headers[HEADER_UA] = userAgent
-        return headers
-    }
-
     override fun setEndpoint(endpoint: String) {
         super.setEndpoint(buildUrl(endpoint, token))
     }
 
     // endregion
 
-    // region Internal
-
-    private val userAgent = System.getProperty(SYSTEM_UA).let {
-        if (it.isNullOrBlank()) {
-            "Datadog/${BuildConfig.VERSION_NAME} " +
-                "(Linux; U; Android ${Build.VERSION.RELEASE}; " +
-                "${Build.MODEL} Build/${Build.ID})"
-        } else {
-            it
-        }
-    }
-
-    // endregion
-
     companion object {
-        private const val HEADER_UA = "User-Agent"
-        const val SYSTEM_UA = "http.agent"
         private const val QP_SOURCE = "ddsource"
         private const val DD_SOURCE_MOBILE = "mobile"
         internal const val UPLOAD_URL =
--- dd-sdk-android/src/test/kotlin/com/datadog/android/core/internal/net/DataOkHttpUploaderTest.kt
+++ dd-sdk-android/src/test/kotlin/com/datadog/android/core/internal/net/DataOkHttpUploaderTest.kt
@@ -387,9 +387,18 @@ internal abstract class DataOkHttpUploaderTest<T : DataOkHttpUploader> {
             .isEqualTo(data)
     }
 
-    open fun assertHeaders(request: RecordedRequest) {
+    fun assertHeaders(request: RecordedRequest) {
         assertThat(request.getHeader("Content-Type"))
             .isEqualTo("application/json")
+        val expectedUserAgent = if (fakeUserAgent.isBlank()) {
+            "Datadog/${BuildConfig.VERSION_NAME} " +
+                    "(Linux; U; Android ${Build.VERSION.RELEASE}; " +
+                    "${Build.MODEL} Build/${Build.ID})"
+        } else {
+            fakeUserAgent
+        }
+        assertThat(request.getHeader("User-Agent"))
+            .isEqualTo(expectedUserAgent)
     }
 
     private fun mockResponse(code: Int): MockResponse {
