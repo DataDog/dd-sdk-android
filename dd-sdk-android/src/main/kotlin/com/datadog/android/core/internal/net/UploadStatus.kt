92ad0f8e8cfaf2cb1213c0b9d25870083fd71a8b
Xavier Gouchet <xavier.gouchet@gmail.com>
2020-02-13 10:49:15 +0100

    :sparkles: (RUMM-227): Let an UploadStatus log a developer message

--- dd-sdk-android/src/main/kotlin/com/datadog/android/core/internal/net/UploadStatus.kt
+++ dd-sdk-android/src/main/kotlin/com/datadog/android/core/internal/net/UploadStatus.kt
@@ -6,6 +6,9 @@
 
 package com.datadog.android.core.internal.net
 
+import com.datadog.android.core.internal.utils.devLogger
+
+@Suppress("StringLiteralDuplication")
 internal enum class UploadStatus {
     SUCCESS,
     NETWORK_ERROR,
@@ -13,5 +16,37 @@ internal enum class UploadStatus {
     HTTP_REDIRECTION,
     HTTP_CLIENT_ERROR,
     HTTP_SERVER_ERROR,
-    UNKNOWN_ERROR
+    UNKNOWN_ERROR;
+
+    fun logStatus(context: String) {
+        when (this) {
+            NETWORK_ERROR -> devLogger.e(
+                "Unable to send batch ($context) because of a network error; " +
+                    "we will retry later."
+            )
+            INVALID_TOKEN_ERROR -> devLogger.e(
+                "Unable to send batch ($context) because your token is invalid. " +
+                    "Make sure that the provided token still exists."
+            )
+            HTTP_REDIRECTION -> devLogger.w(
+                "Unable to send batch ($context) because of a network error; " +
+                    "we will retry later."
+            )
+            HTTP_CLIENT_ERROR -> devLogger.e(
+                "Unable to send batch ($context) because of a processing error " +
+                    "(possibly because of invalid data); the batch was dropped."
+            )
+            HTTP_SERVER_ERROR -> devLogger.e(
+                "Unable to send batch ($context) because of a server processing error; " +
+                    "we will retry later."
+            )
+            UNKNOWN_ERROR -> devLogger.e(
+                "Unable to send batch ($context) because of an unknown error; " +
+                    "we will retry later."
+            )
+            SUCCESS -> devLogger.v(
+                "Batch sent successfully ($context)."
+            )
+        }
+    }
 }
