c750ed994c02521eed6b958485f6efed8aa424ce
Marius Constantin <marius.constantin@datadoghq.com>
2020-02-18 18:56:52 +0100

    RUMM-223 Introduce the GestureDetector

--- dd-sdk-android/src/main/kotlin/com/datadog/android/instrumentation/gesture/DatadogGesturesTracker.kt
+++ dd-sdk-android/src/main/kotlin/com/datadog/android/instrumentation/gesture/DatadogGesturesTracker.kt
@@ -1,23 +1,31 @@
 package com.datadog.android.instrumentation.gesture
 
 import android.app.Activity
+import android.view.GestureDetector
+import android.view.MotionEvent
+import androidx.core.view.GestureDetectorCompat
+import com.datadog.android.tracing.Tracer
+import java.util.concurrent.TimeUnit
 
-internal class DatadogGesturesTracker : GesturesTracker {
+internal class DatadogGesturesTracker(val rumTracer: Tracer) : GesturesTracker,
+    GestureDetector.OnGestureListener {
 
+    // region GesturesTracker
     override fun startTracking(activity: Activity) {
         // Even though the decorView is marked as NonNul I've seen cases where this was null.
         // Better to be safe here.
+        val window = activity.window
         @Suppress("SENSELESS_COMPARISON")
-        if (activity.window == null || activity.window.decorView == null) {
+        if (window == null || window.decorView == null) {
             return
         }
-        val currentCallback = activity.window.callback
-        val toWrap = if (currentCallback != null) {
-            currentCallback
-        } else {
-            NoOpWindowCallback()
-        }
-        activity.window.callback = WindowCallbackWrapper(toWrap)
+        val toWrap = window.callback ?: NoOpWindowCallback()
+        // we cannot reuse a GestureDetector as we can have multiple activities
+        // running in the same time
+        window.callback = WindowCallbackWrapper(
+            toWrap,
+            generateGestureDetector(activity)
+        )
     }
 
     override fun stopTracking(activity: Activity) {
@@ -29,5 +37,69 @@ internal class DatadogGesturesTracker : GesturesTracker {
                 activity.window.callback = null
             }
         }
+    }
+
+    // endregion
+
+    // region GesturesListener
+
+    override fun onShowPress(e: MotionEvent?) {
+        // No Op
+    }
+
+    override fun onSingleTapUp(e: MotionEvent?): Boolean {
+        // TODO RUMM-236 Add more details here
+        // we just intercept but not steal the event
+        rumTracer
+            .buildSpan(UI_TAP_ACTION_EVENT)
+            .start()
+            .finish(DEFAULT_EVENT_DURATION)
+        return false
+    }
+
+    override fun onDown(e: MotionEvent?): Boolean {
+        // No Op
+        // we just intercept but not steal the event
+        return false
+    }
+
+    override fun onFling(
+        e1: MotionEvent?,
+        e2: MotionEvent?,
+        velocityX: Float,
+        velocityY: Float
+    ): Boolean {
+        // No Op
+        // we just intercept but not steal the event
+        return false
+    }
+
+    override fun onScroll(
+        e1: MotionEvent?,
+        e2: MotionEvent?,
+        distanceX: Float,
+        distanceY: Float
+    ): Boolean {
+        // No Op
+        // we just intercept but not steal the event
+        return false
+    }
+
+    override fun onLongPress(e: MotionEvent?) {
+        // No Op
+    }
+
+    // endregion
+
+    // region Internal
+
+    internal fun generateGestureDetector(activity: Activity) =
+        GestureDetectorCompat(activity, this)
+
+    // endregion
+
+    companion object {
+        internal const val UI_TAP_ACTION_EVENT = "TapEvent"
+        val DEFAULT_EVENT_DURATION = TimeUnit.MILLISECONDS.toMicros(16)
     }
 }
--- dd-sdk-android/src/test/kotlin/com/datadog/android/instrumentation/gestures/DatadogGesturesTrackerTest.kt
+++ dd-sdk-android/src/test/kotlin/com/datadog/android/instrumentation/gestures/DatadogGesturesTrackerTest.kt
@@ -1,17 +1,23 @@
 package com.datadog.android.instrumentation.gestures
 
 import android.app.Activity
+import android.view.MotionEvent
 import android.view.Window
+import androidx.core.view.GestureDetectorCompat
 import com.datadog.android.instrumentation.gesture.DatadogGesturesTracker
-import com.datadog.android.instrumentation.gesture.GesturesTracker
 import com.datadog.android.instrumentation.gesture.NoOpWindowCallback
 import com.datadog.android.instrumentation.gesture.WindowCallbackWrapper
+import com.datadog.android.tracing.Tracer
 import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.isA
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.never
+import com.nhaarman.mockitokotlin2.spy
 import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
+import datadog.opentracing.DDSpan
+import datadog.opentracing.DDTracer
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -27,15 +33,32 @@ import org.mockito.quality.Strictness
 @MockitoSettings(strictness = Strictness.LENIENT)
 internal class DatadogGesturesTrackerTest {
 
-    lateinit var underTest: GesturesTracker
+    lateinit var underTest: DatadogGesturesTracker
     @Mock
     lateinit var mockActivity: Activity
     @Mock
     lateinit var mockWindow: Window
 
+    @Mock
+    lateinit var mockRumTracer: Tracer
+    @Mock
+    lateinit var mockSpanBuilder: DDTracer.DDSpanBuilder
+    @Mock
+    lateinit var mockSpan: DDSpan
+
+    @Mock
+    lateinit var mockGestureDetector: GestureDetectorCompat
+
     @BeforeEach
     fun `set up`() {
-        underTest = DatadogGesturesTracker()
+        mockRumTracer.apply {
+            whenever(buildSpan(DatadogGesturesTracker.UI_TAP_ACTION_EVENT))
+                .thenReturn(mockSpanBuilder)
+        }
+        mockSpanBuilder.apply {
+            whenever(start()).thenReturn(mockSpan)
+        }
+        underTest = DatadogGesturesTracker(mockRumTracer)
         whenever(mockActivity.window).thenReturn(mockWindow)
         whenever(mockWindow.decorView).thenReturn(mock())
     }
@@ -43,7 +66,9 @@ internal class DatadogGesturesTrackerTest {
     @Test
     fun `will start tracking the activity`() {
         // when
-        underTest.startTracking(mockActivity)
+        val spyTest = spy(underTest)
+        doReturn(mockGestureDetector).whenever(spyTest).generateGestureDetector(mockActivity)
+        spyTest.startTracking(mockActivity)
 
         // then
         verify(mockWindow).callback = isA<WindowCallbackWrapper>()
@@ -52,7 +77,13 @@ internal class DatadogGesturesTrackerTest {
     @Test
     fun `will stop tracking the activity`() {
         // given
-        whenever(mockWindow.callback).thenReturn(WindowCallbackWrapper(NoOpWindowCallback()))
+        whenever(mockWindow.callback)
+            .thenReturn(
+                WindowCallbackWrapper(
+                    NoOpWindowCallback(),
+                    mockGestureDetector
+                )
+            )
 
         // when
         underTest.stopTracking(mockActivity)
@@ -65,7 +96,13 @@ internal class DatadogGesturesTrackerTest {
     fun `stop tracking the activity will restore the previous callback if was not null`() {
         // given
         val previousCallback: Window.Callback = mock()
-        whenever(mockWindow.callback).thenReturn(WindowCallbackWrapper(previousCallback))
+        whenever(mockWindow.callback)
+            .thenReturn(
+                WindowCallbackWrapper(
+                    previousCallback,
+                    mockGestureDetector
+                )
+            )
 
         // when
         underTest.stopTracking(mockActivity)
@@ -90,5 +127,18 @@ internal class DatadogGesturesTrackerTest {
 
         // then
         verify(mockWindow, never()).callback = any()
+    }
+
+    @Test
+    fun `when tap was detected will start and finish a span`() {
+        // given
+        val motionEvent: MotionEvent = mock()
+
+        // when
+        underTest.onSingleTapUp(motionEvent)
+
+        // verify
+        verify(mockRumTracer).buildSpan(DatadogGesturesTracker.UI_TAP_ACTION_EVENT)
+        verify(mockSpan).finish(DatadogGesturesTracker.DEFAULT_EVENT_DURATION)
     }
 }
