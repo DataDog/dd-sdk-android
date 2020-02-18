51f7ed86afb414ac0807e1c64c75f626a6612219
Marius Constantin <marius.constantin@datadoghq.com>
2020-02-18 18:56:52 +0100

    RUMM-223 Introduce the GestureDetector

--- dd-sdk-android/src/main/kotlin/com/datadog/android/instrumentation/gesture/WindowCallbackWrapper.kt
+++ dd-sdk-android/src/main/kotlin/com/datadog/android/instrumentation/gesture/WindowCallbackWrapper.kt
@@ -1,138 +1,44 @@
 package com.datadog.android.instrumentation.gesture
 
-import android.os.Build
-import android.view.ActionMode
-import android.view.KeyEvent
-import android.view.Menu
-import android.view.MenuItem
 import android.view.MotionEvent
-import android.view.SearchEvent
-import android.view.View
 import android.view.Window
-import android.view.WindowManager
-import android.view.accessibility.AccessibilityEvent
-import androidx.annotation.RequiresApi
+import androidx.core.view.GestureDetectorCompat
+import com.datadog.android.core.internal.utils.sdkLogger
+import java.lang.Exception
 
-internal class WindowCallbackWrapper(val wrappedCallback: Window.Callback) : Window.Callback {
+internal class WindowCallbackWrapper(
+    val wrappedCallback: Window.Callback,
+    val gesturesDetector: GestureDetectorCompat
+) : Window.Callback by wrappedCallback {
 
     // region Window.Callback
 
-    override fun onActionModeFinished(mode: ActionMode?) {
-        wrappedCallback.onActionModeFinished(mode)
-    }
-
-    override fun onCreatePanelView(featureId: Int): View? {
-        return wrappedCallback.onCreatePanelView(featureId)
-    }
-
     override fun dispatchTouchEvent(event: MotionEvent?): Boolean {
+        // we copy it and delegate it to the gesture detector for analysis
+        val copy = copyEvent(event)
+        @Suppress("TooGenericExceptionCaught")
+        try {
+            gesturesDetector.onTouchEvent(copy)
+        } catch (e: Exception) {
+            sdkLogger.e(
+                "$TAG: error while processing the MotionEvent", e
+            )
+        } finally {
+            sdkLogger.v("$TAG: Recycling the MotionEvent copy")
+            copy.recycle()
+        }
         return wrappedCallback.dispatchTouchEvent(event)
     }
 
-    override fun onCreatePanelMenu(featureId: Int, menu: Menu): Boolean {
-        return wrappedCallback.onCreatePanelMenu(featureId, menu)
-    }
+    // endregion
 
-    override fun onWindowStartingActionMode(callback: ActionMode.Callback?): ActionMode? {
+    // region Internal
 
-        return wrappedCallback.onWindowStartingActionMode(callback)
-    }
-
-    @RequiresApi(Build.VERSION_CODES.M)
-    override fun onWindowStartingActionMode(
-        callback: ActionMode.Callback?,
-        type: Int
-    ): ActionMode? {
-
-        return wrappedCallback.onWindowStartingActionMode(callback, type)
-    }
-
-    override fun onAttachedToWindow() {
-
-        wrappedCallback.onAttachedToWindow()
-    }
-
-    override fun dispatchGenericMotionEvent(event: MotionEvent?): Boolean {
-
-        return wrappedCallback.dispatchGenericMotionEvent(event)
-    }
-
-    @Suppress("FunctionMaxLength")
-    override fun dispatchPopulateAccessibilityEvent(event: AccessibilityEvent?): Boolean {
-
-        return wrappedCallback.dispatchPopulateAccessibilityEvent(event)
-    }
-
-    override fun dispatchTrackballEvent(event: MotionEvent?): Boolean {
-
-        return wrappedCallback.dispatchTrackballEvent(event)
-    }
-
-    override fun dispatchKeyShortcutEvent(event: KeyEvent?): Boolean {
-
-        return wrappedCallback.dispatchKeyShortcutEvent(event)
-    }
-
-    override fun dispatchKeyEvent(event: KeyEvent?): Boolean {
-
-        return wrappedCallback.dispatchKeyEvent(event)
-    }
-
-    override fun onMenuOpened(featureId: Int, menu: Menu): Boolean {
-
-        return wrappedCallback.onMenuOpened(featureId, menu)
-    }
-
-    override fun onPanelClosed(featureId: Int, menu: Menu) {
-
-        return wrappedCallback.onPanelClosed(featureId, menu)
-    }
-
-    override fun onMenuItemSelected(featureId: Int, item: MenuItem): Boolean {
-
-        return wrappedCallback.onMenuItemSelected(featureId, item)
-    }
-
-    override fun onDetachedFromWindow() {
-
-        wrappedCallback.onDetachedFromWindow()
-    }
-
-    override fun onPreparePanel(featureId: Int, view: View?, menu: Menu): Boolean {
-
-        return wrappedCallback.onPreparePanel(featureId, view, menu)
-    }
-
-    override fun onWindowAttributesChanged(attrs: WindowManager.LayoutParams?) {
-
-        return wrappedCallback.onWindowAttributesChanged(attrs)
-    }
-
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-
-        return wrappedCallback.onWindowFocusChanged(hasFocus)
-    }
-
-    override fun onContentChanged() {
-
-        wrappedCallback.onContentChanged()
-    }
-
-    override fun onSearchRequested(): Boolean {
-
-        return wrappedCallback.onSearchRequested()
-    }
-
-    @RequiresApi(Build.VERSION_CODES.M)
-    override fun onSearchRequested(searchEvent: SearchEvent?): Boolean {
-
-        return wrappedCallback.onSearchRequested(searchEvent)
-    }
-
-    override fun onActionModeStarted(mode: ActionMode?) {
-
-        wrappedCallback.onActionModeStarted(mode)
-    }
+    internal fun copyEvent(event: MotionEvent?) = MotionEvent.obtain(event)
 
     // endregion
+
+    companion object {
+        const val TAG = "WindowCallbackWrapper"
+    }
 }
