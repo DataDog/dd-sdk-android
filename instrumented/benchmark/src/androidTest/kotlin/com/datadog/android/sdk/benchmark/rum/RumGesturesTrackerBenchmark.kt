c6104bcc6b482c1177bcd26ac376c22faadf115f
Marius Constantin <marius.constantin@datadoghq.com>
2020-02-20 18:53:41 +0100

    RUMM-223 Cleanup

--- instrumented/benchmark/src/androidTest/kotlin/com/datadog/android/sdk/benchmark/rum/RumGesturesTrackerBenchmark.kt
+++ instrumented/benchmark/src/androidTest/kotlin/com/datadog/android/sdk/benchmark/rum/RumGesturesTrackerBenchmark.kt
@@ -28,7 +28,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class RumGesturesTrackerBenchmark {
+internal class RumGesturesTrackerBenchmark {
 
     @get:Rule
     val benchmark = BenchmarkRule()
