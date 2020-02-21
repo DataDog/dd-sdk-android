41159dfd05168d2e2bff064a6a4c1f475a3a863b
Xavier Gouchet <xavier.gouchet@gmail.com>
2020-02-21 16:23:25 +0100

    :sparkles: (RUMM-238): Create RumFeature

---
+++ dd-sdk-android/src/main/kotlin/com/datadog/android/rum/internal/net/RumOkHttpUploader.kt
@@ -0,0 +1,40 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2016-2020 Datadog, Inc.
+ */
+
+package com.datadog.android.rum.internal.net
+
+import com.datadog.android.core.internal.net.DataOkHttpUploader
+import java.util.Locale
+import okhttp3.OkHttpClient
+
+internal open class RumOkHttpUploader(
+    endpoint: String,
+    private val token: String,
+    client: OkHttpClient
+) : DataOkHttpUploader(buildUrl(endpoint, token), client) {
+
+    // region DataOkHttpUploader
+
+    override fun setEndpoint(endpoint: String) {
+        super.setEndpoint(buildUrl(endpoint, token))
+    }
+
+    // endregion
+
+    companion object {
+        private const val QP_SOURCE = "ddsource"
+        private const val DD_SOURCE_MOBILE = "mobile"
+        internal const val UPLOAD_URL =
+            "%s/v1/input/%s?$QP_SOURCE=$DD_SOURCE_MOBILE"
+
+        private fun buildUrl(endpoint: String, token: String): String {
+            return String.format(
+                Locale.US,
+                UPLOAD_URL, endpoint, token
+            )
+        }
+    }
+}
