bdc0bbdf9e2392a7ce0300e285bf9020feccb54b
Xavier Gouchet <xavier.gouchet@gmail.com>
2020-02-21 08:42:50 +0100

    :sparkles: (RUMM-238): Create RumEvent model and serializer

---
+++ dd-sdk-android/src/main/kotlin/com/datadog/android/rum/internal/domain/RumContext.kt
@@ -0,0 +1,15 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2016-2020 Datadog, Inc.
+ */
+
+package com.datadog.android.rum.internal.domain
+
+import java.util.UUID
+
+internal data class RumContext(
+    val applicationId: UUID = UUID(0, 0),
+    val sessionId: UUID = UUID(0, 0),
+    val viewId: UUID? = null
+)
