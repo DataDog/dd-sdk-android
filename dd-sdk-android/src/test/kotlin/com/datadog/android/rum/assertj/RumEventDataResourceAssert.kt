f8df3f268d00058c8b5b56562d5743e7cf2ee54c
Xavier Gouchet <xavier.gouchet@gmail.com>
2020-02-21 08:42:50 +0100

    :sparkles: (RUMM-238): Create RumEvent model and serializer

---
+++ dd-sdk-android/src/test/kotlin/com/datadog/android/rum/assertj/RumEventDataResourceAssert.kt
@@ -0,0 +1,67 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2016-2020 Datadog, Inc.
+ */
+
+package com.datadog.android.rum.assertj
+
+import com.datadog.android.rum.internal.domain.RumEventData
+import org.assertj.core.api.AbstractObjectAssert
+import org.assertj.core.api.Assertions.assertThat
+
+internal class RumEventDataResourceAssert(actual: RumEventData.Resource) :
+    AbstractObjectAssert<RumEventDataResourceAssert, RumEventData.Resource>(
+        actual,
+        RumEventDataResourceAssert::class.java
+    ) {
+
+    fun hasKind(expected: RumEventData.Resource.Kind): RumEventDataResourceAssert {
+        assertThat(actual.kind)
+            .overridingErrorMessage(
+                "Expected event data to have kind $expected but was ${actual.kind}"
+            )
+            .isEqualTo(expected)
+        return this
+    }
+
+    fun hasUrl(expected: String): RumEventDataResourceAssert {
+        assertThat(actual.url)
+            .overridingErrorMessage(
+                "Expected event data to have url $expected but was ${actual.url}"
+            )
+            .isEqualTo(expected)
+        return this
+    }
+
+    fun hasDuration(
+        expected: Long,
+        offset: Long = DURATION_THRESHOLD_NANOS
+    ): RumEventDataResourceAssert {
+        assertThat(actual.durationNanoSeconds)
+            .overridingErrorMessage(
+                "Expected event data to have duration $expected " +
+                    "but was ${actual.durationNanoSeconds}"
+            )
+            .isEqualTo(expected)
+        return this
+    }
+
+    fun hasDurationLowerThan(upperBound: Long): RumEventDataResourceAssert {
+        assertThat(actual.durationNanoSeconds)
+            .overridingErrorMessage(
+                "Expected event data to have duration lower than $upperBound " +
+                    "but was ${actual.durationNanoSeconds}"
+            )
+            .isLessThanOrEqualTo(upperBound)
+        return this
+    }
+
+    companion object {
+
+        internal const val DURATION_THRESHOLD_NANOS = 1000L
+
+        internal fun assertThat(actual: RumEventData.View): RumEventDataViewAssert =
+            RumEventDataViewAssert(actual)
+    }
+}
