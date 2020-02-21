aac7f049b0c3a18a218c59eff9139656c6765e02
Xavier Gouchet <xavier.gouchet@gmail.com>
2020-02-21 08:42:50 +0100

    :sparkles: (RUMM-238): Create RumEvent model and serializer

---
+++ dd-sdk-android/src/test/kotlin/com/datadog/android/rum/assertj/RumContextAssert.kt
@@ -0,0 +1,52 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2016-2020 Datadog, Inc.
+ */
+
+package com.datadog.android.rum.assertj
+
+import com.datadog.android.rum.internal.domain.RumContext
+import java.util.UUID
+import org.assertj.core.api.AbstractObjectAssert
+import org.assertj.core.api.Assertions.assertThat
+
+internal class RumContextAssert(actual: RumContext) :
+    AbstractObjectAssert<RumContextAssert, RumContext>(
+        actual,
+        RumContextAssert::class.java
+    ) {
+
+    fun hasApplicationId(expected: UUID): RumContextAssert {
+        assertThat(actual.applicationId)
+            .overridingErrorMessage(
+                "Expected context to have applicationId $expected but was ${actual.applicationId}"
+            )
+            .isEqualTo(expected)
+        return this
+    }
+
+    fun hasViewId(expected: UUID?): RumContextAssert {
+        assertThat(actual.viewId)
+            .overridingErrorMessage(
+                "Expected context to have viewId $expected but was ${actual.viewId}"
+            )
+            .isEqualTo(expected)
+        return this
+    }
+
+    fun hasSessionId(expected: UUID): RumContextAssert {
+        assertThat(actual.sessionId)
+            .overridingErrorMessage(
+                "Expected context to have sessionId $expected but was ${actual.sessionId}"
+            )
+            .isEqualTo(expected)
+        return this
+    }
+
+    companion object {
+
+        internal fun assertThat(actual: RumContext): RumContextAssert =
+            RumContextAssert(actual)
+    }
+}
