2151e1864171e0023de838895d66f4341709bf73
Marius Constantin <marius.constantin@datadoghq.com>
2020-03-02 13:03:54 +0100

    RUMM-219 Introduce the TrackingStrategies

--- dd-sdk-android/src/main/kotlin/com/datadog/android/rum/internal/instrumentation/gestures/DatadogGesturesTracker.kt
+++ dd-sdk-android/src/main/kotlin/com/datadog/android/rum/internal/instrumentation/gestures/DatadogGesturesTracker.kt
@@ -1,4 +1,4 @@
-package com.datadog.android.rum.gestures
+package com.datadog.android.rum.internal.instrumentation.gestures
 
 import android.app.Activity
 import androidx.core.view.GestureDetectorCompat
