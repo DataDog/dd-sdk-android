7df2ed668ed6f4da79d62eee535e0b9f4793e27e
Marius Constantin <marius.constantin@datadoghq.com>
2020-03-03 17:11:21 +0100

    RUMM-242 Use GesturesTracker into the sample apps

--- sample/kotlin/src/main/kotlin/com/datadog/android/sample/SampleApplication.kt
+++ sample/kotlin/src/main/kotlin/com/datadog/android/sample/SampleApplication.kt
@@ -32,6 +32,7 @@ class SampleApplication : Application() {
             .setServiceName("android-sample-kotlin")
             .setEnvironmentName("staging")
             .trackViews(DatadogConfig.ViewTrackerStrategy.TRACK_ACTIVITIES_AS_VIEWS)
+            .trackGestures()
 
         if (BuildConfig.DD_OVERRIDE_LOGS_URL.isNotBlank()) {
             configBuilder.useCustomLogsEndpoint(BuildConfig.DD_OVERRIDE_LOGS_URL)
