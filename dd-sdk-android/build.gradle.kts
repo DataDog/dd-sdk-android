8a206e324bcec73ba12cbfa23af0d89fc42f7073
Xavier Gouchet <xavier.gouchet@gmail.com>
2020-03-12 16:37:53 +0100

    Fix Dependencies

--- dd-sdk-android/build.gradle.kts
+++ dd-sdk-android/build.gradle.kts
@@ -107,7 +107,8 @@ dependencies {
     implementation(Dependencies.Libraries.Gson)
     implementation(Dependencies.Libraries.Kotlin)
     implementation(Dependencies.Libraries.OkHttp)
-    implementation(Dependencies.Libraries.JetpackWorkManager)
+    implementation(Dependencies.Libraries.AndroidXWorkManager)
+    implementation(Dependencies.Libraries.AndroidXCore)
     api(Dependencies.Libraries.TracingOt)
 
     testImplementation(project(":tools:unit"))
