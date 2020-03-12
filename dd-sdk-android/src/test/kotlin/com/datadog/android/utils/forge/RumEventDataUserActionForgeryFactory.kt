510599bf3e05b51e01afda05dd13cb1be67da05c
Xavier Gouchet <xavier.gouchet@gmail.com>
2020-03-12 10:42:28 +0100

    :sparkles: (RUMM-261): Add UserAction id field

--- dd-sdk-android/src/test/kotlin/com/datadog/android/utils/forge/RumEventDataUserActionForgeryFactory.kt
+++ dd-sdk-android/src/test/kotlin/com/datadog/android/utils/forge/RumEventDataUserActionForgeryFactory.kt
@@ -14,7 +14,8 @@ internal class RumEventDataUserActionForgeryFactory :
     ForgeryFactory<RumEventData.UserAction> {
     override fun getForgery(forge: Forge): RumEventData.UserAction {
         return RumEventData.UserAction(
-            name = forge.anAlphabeticalString()
+            name = forge.anAlphabeticalString(),
+            id = forge.getForgery()
         )
     }
 }
