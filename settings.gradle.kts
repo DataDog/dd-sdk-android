3cb8ea127ee8abd886cd4493f3bf50f64efb9da9
Xavier Gouchet <xavier.gouchet@gmail.com>
2020-03-20 14:38:33 +0100

    :recycle: (RUMM-310): Rename module dd-sdk-android-support-fragment

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -39,7 +39,7 @@ include(":dd-sdk-android")
 include(":dd-sdk-android-timber")
 include(":dd-sdk-android-ktx")
 include(":dd-sdk-android-androidx-fragment")
-include(":dd-sdk-support-fragment")
+include(":dd-sdk-android-support-fragment")
 include(":dd-sdk-android-navigation")
 
 include(":instrumented:benchmark")
