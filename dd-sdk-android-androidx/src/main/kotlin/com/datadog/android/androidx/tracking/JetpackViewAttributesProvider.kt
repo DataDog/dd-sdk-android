9f2cc2f102488de66406d967b239473a977f2a3e
Marius Constantin <marius.constantin@datadoghq.com>
2020-03-31 17:21:49 +0200

    RUMM-353 Introduce the JetpackViewAttributesProvider

---
+++ dd-sdk-android-androidx/src/main/kotlin/com/datadog/android/androidx/tracking/JetpackViewAttributesProvider.kt
@@ -0,0 +1,68 @@
+package com.datadog.android.androidx.tracking
+
+import android.content.res.Resources
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.datadog.android.rum.RumAttributes
+import com.datadog.android.rum.tracking.ViewAttributesProvider
+
+/**
+ * Provides extra attributes for the touch target View.
+ * Those special attributes are specifically related with Jetpack components.
+ * <ul>
+ *     <li> If the parent of the target view is a RecyclerView it will add the position
+ *     of the target inside the adapter attribute together
+ *     with the container class name and resource id </li>
+ * </ul>
+ * @see [RumAttributes.TAG_TARGET_POSITION_IN_SCROLLABLE_CONTAINER]
+ * @see [RumAttributes.TAG_TARGET_SCROLLABLE_CONTAINER_CLASS_NAME]
+ * @see [RumAttributes.TAG_TARGET_SCROLLABLE_CONTAINER_RESOURCE_ID]
+ */
+class JetpackViewAttributesProvider : ViewAttributesProvider {
+
+    // region ViewAttributesProvider
+
+    override fun extractAttributes(
+        view: View,
+        attributes: MutableMap<String, Any?>
+    ) {
+        // traverse the target parents
+        var parent = view.parent
+        var child: View? = view
+        while (parent != null) {
+            if (parent is RecyclerView && child != null && isDirectChildOfRecyclerView(child)) {
+                val positionInAdapter = parent.getChildAdapterPosition(child)
+                attributes[RumAttributes.TAG_TARGET_POSITION_IN_SCROLLABLE_CONTAINER] =
+                    positionInAdapter
+                attributes[RumAttributes.TAG_TARGET_SCROLLABLE_CONTAINER_CLASS_NAME] =
+                    parent.javaClass.canonicalName
+                attributes[RumAttributes.TAG_TARGET_SCROLLABLE_CONTAINER_RESOURCE_ID] =
+                    resolveIdOrResourceName(parent)
+                break
+            }
+            child = parent as? View
+            parent = parent.parent
+        }
+    }
+
+    // endregion
+
+    // region Internal
+
+    private fun isDirectChildOfRecyclerView(child: View): Boolean {
+        return child.layoutParams is RecyclerView.LayoutParams
+    }
+
+    private fun resolveIdOrResourceName(view: View): String {
+        @Suppress("SwallowedException")
+        return try {
+            view.resources.getResourceEntryName(view.id) ?: viewIdAsHexa(view)
+        } catch (e: Resources.NotFoundException) {
+            viewIdAsHexa(view)
+        }
+    }
+
+    private fun viewIdAsHexa(view: View) = "0x${view.id.toString(16)}"
+
+    // endregion
+}
