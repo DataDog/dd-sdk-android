881e4e3767b9f38bc71e99c65fd2d83d020957c2
Xavier Gouchet <xavier.gouchet@gmail.com>
2020-04-01 10:48:44 +0200

    :sparkles: (RUMM-328): Duplicate Log Errors as RUM error

--- dd-sdk-android/src/test/kotlin/com/datadog/android/log/internal/logger/DatadogLogHandlerTest.kt
+++ dd-sdk-android/src/test/kotlin/com/datadog/android/log/internal/logger/DatadogLogHandlerTest.kt
@@ -6,6 +6,7 @@
 
 package com.datadog.android.log.internal.logger
 
+import android.util.Log as AndroidLog
 import com.datadog.android.Datadog
 import com.datadog.android.DatadogConfig
 import com.datadog.android.core.internal.data.Writer
@@ -30,7 +31,6 @@ import com.datadog.tools.unit.setFieldValue
 import com.datadog.tools.unit.setStaticValue
 import com.nhaarman.mockitokotlin2.argumentCaptor
 import com.nhaarman.mockitokotlin2.doReturn
-import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.verifyZeroInteractions
 import com.nhaarman.mockitokotlin2.whenever
@@ -43,7 +43,6 @@ import io.opentracing.noop.NoopTracerFactory
 import io.opentracing.util.GlobalTracer
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
-import java.util.concurrent.atomic.AtomicBoolean
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
@@ -86,6 +85,9 @@ internal class DatadogLogHandlerTest {
     lateinit var mockWriter: Writer<Log>
 
     @Mock
+    lateinit var mockRumMonitor: RumMonitor
+
+    @Mock
     lateinit var mockNetworkInfoProvider: NetworkInfoProvider
 
     @Mock
@@ -125,12 +127,41 @@ internal class DatadogLogHandlerTest {
     fun `tear down`() {
         GlobalTracer.get().setFieldValue("isRegistered", false)
         GlobalTracer::class.java.setStaticValue("tracer", NoopTracerFactory.create())
-        GlobalRum::class.java.setStaticValue("isRegistered", AtomicBoolean(false))
-        GlobalRum::class.java.setStaticValue("monitor", NoOpRumMonitor())
+        GlobalRum.isRegistered.set(false)
+        GlobalRum.monitor = NoOpRumMonitor()
     }
 
     @Test
     fun `forward log to LogWriter`() {
+
+        testedHandler.handleLog(
+            fakeLevel,
+            fakeMessage,
+            null,
+            fakeAttributes,
+            fakeTags
+        )
+
+        argumentCaptor<Log>().apply {
+            verify(mockWriter).write(capture())
+
+            assertThat(lastValue)
+                .hasServiceName(fakeServiceName)
+                .hasLoggerName(fakeLoggerName)
+                .hasThreadName(Thread.currentThread().name)
+                .hasLevel(fakeLevel)
+                .hasMessage(fakeMessage)
+                .hasTimestamp(fakeServerDate)
+                .hasNetworkInfo(fakeNetworkInfo)
+                .hasUserInfo(fakeUserInfo)
+                .hasAttributes(fakeAttributes)
+                .hasTags(fakeTags)
+                .hasThrowable(null)
+        }
+    }
+
+    @Test
+    fun `forward log to LogWriter with throwable`() {
 
         testedHandler.handleLog(
             fakeLevel,
@@ -154,7 +185,66 @@ internal class DatadogLogHandlerTest {
                 .hasUserInfo(fakeUserInfo)
                 .hasAttributes(fakeAttributes)
                 .hasTags(fakeTags)
+                .hasThrowable(fakeThrowable)
         }
+    }
+
+    @Test
+    fun `doesn't forward low level log to RumMonitor`(forge: Forge) {
+        GlobalRum.registerIfAbsent(mockRumMonitor)
+        fakeLevel = forge.anInt(AndroidLog.VERBOSE, AndroidLog.ERROR)
+
+        testedHandler.handleLog(
+            fakeLevel,
+            fakeMessage,
+            fakeThrowable,
+            fakeAttributes,
+            fakeTags
+        )
+
+        verifyZeroInteractions(mockRumMonitor)
+    }
+
+    @Test
+    fun `forward error log to RumMonitor`(forge: Forge) {
+        GlobalRum.registerIfAbsent(mockRumMonitor)
+        fakeLevel = forge.anElementFrom(AndroidLog.ERROR, AndroidLog.ASSERT)
+
+        testedHandler.handleLog(
+            fakeLevel,
+            fakeMessage,
+            null,
+            fakeAttributes,
+            fakeTags
+        )
+
+        verify(mockRumMonitor).addError(
+            fakeMessage,
+            fakeLoggerName,
+            null,
+            fakeAttributes
+        )
+    }
+
+    @Test
+    fun `forward error log to RumMonitor with throwable`(forge: Forge) {
+        GlobalRum.registerIfAbsent(mockRumMonitor)
+        fakeLevel = forge.anElementFrom(AndroidLog.ERROR, AndroidLog.ASSERT)
+
+        testedHandler.handleLog(
+            fakeLevel,
+            fakeMessage,
+            fakeThrowable,
+            fakeAttributes,
+            fakeTags
+        )
+
+        verify(mockRumMonitor).addError(
+            fakeMessage,
+            fakeLoggerName,
+            fakeThrowable,
+            fakeAttributes
+        )
     }
 
     @Test
@@ -253,6 +343,7 @@ internal class DatadogLogHandlerTest {
 
     @Test
     fun `forward minimal log to LogWriter`() {
+        GlobalRum.isRegistered.set(false)
         testedHandler.setFieldValue("networkInfoProvider", null as NetworkInfoProvider?)
 
         testedHandler.handleLog(
@@ -277,6 +368,7 @@ internal class DatadogLogHandlerTest {
                 .hasUserInfo(fakeUserInfo)
                 .hasAttributes(emptyMap())
                 .hasTags(emptyList())
+                .hasThrowable(null)
         }
     }
 
@@ -337,14 +429,14 @@ internal class DatadogLogHandlerTest {
         val config = DatadogConfig.Builder(forge.anAlphabeticalString()).build()
         Datadog.initialize(mockContext(), config)
         val rumContext = forge.getForgery<RumContext>()
-        GlobalRum.registerIfAbsent(mock<RumMonitor>())
         GlobalRum.updateContext(rumContext)
+        GlobalRum.registerIfAbsent(mockRumMonitor)
 
         // when
         testedHandler.handleLog(
             fakeLevel,
             fakeMessage,
-            fakeThrowable,
+            null,
             fakeAttributes,
             fakeTags
         )
