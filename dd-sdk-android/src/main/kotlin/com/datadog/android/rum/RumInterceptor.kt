229c020c9c9f57eb81e1e69528d9ae66c58c75aa
Xavier Gouchet <xavier.gouchet@gmail.com>
2020-04-02 11:44:44 +0200

    :recycle: (RUMM-349): Use CombinedInterceptor for RUM

--- dd-sdk-android/src/main/kotlin/com/datadog/android/rum/RumInterceptor.kt
+++ dd-sdk-android/src/main/kotlin/com/datadog/android/rum/RumInterceptor.kt
@@ -6,15 +6,11 @@
 
 package com.datadog.android.rum
 
+import com.datadog.android.core.internal.net.CombinedInterceptor
+import com.datadog.android.rum.internal.net.RumRequestInterceptor
 import com.datadog.android.tracing.TracingInterceptor
-import datadog.opentracing.propagation.ExtractedContext
-import io.opentracing.propagation.Format
-import io.opentracing.propagation.TextMapExtract
-import io.opentracing.util.GlobalTracer
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.Response
 
 /**
  * Provides automatic RUM integration for [OkHttpClient] by way of the [Interceptor] system.
@@ -34,73 +30,5 @@ import okhttp3.Response
  *       .build();
  * ```
  */
-class RumInterceptor : Interceptor {
-
-    // region Interceptor
-
-    /** @inheritdoc */
-    @Suppress("TooGenericExceptionCaught")
-    override fun intercept(chain: Interceptor.Chain): Response {
-
-        val request = chain.request()
-        val url = request.url().toString()
-        val method = request.method()
-        val traceId = extractTraceId(request)
-
-        GlobalRum.get().startResource(
-            request,
-            method,
-            url,
-            if (traceId != null) mapOf(RumAttributes.TRACE_ID to traceId) else emptyMap()
-        )
-
-        try {
-            val response = chain.proceed(request)
-            val mimeType = response.header(HEADER_CT)
-            val kind = when {
-                method in xhrMethods -> RumResourceKind.XHR
-                mimeType == null -> RumResourceKind.UNKNOWN
-                else -> RumResourceKind.fromMimeType(mimeType)
-            }
-            GlobalRum.get().stopResource(
-                request,
-                kind,
-                mapOf(
-                    RumAttributes.HTTP_STATUS_CODE to response.code(),
-                    RumAttributes.NETWORK_BYTES_WRITTEN to
-                        (response.body()?.contentLength() ?: 0)
-                )
-            )
-            return response
-        } catch (t: Throwable) {
-            GlobalRum.get().stopResourceWithError(request, "OkHttp error on $method", "network", t)
-            throw t
-        }
-    }
-
-    private fun extractTraceId(request: Request): String? {
-        val extractedContext = GlobalTracer.get()
-            .extract(
-                Format.Builtin.TEXT_MAP_EXTRACT,
-                TextMapExtract {
-                    request.headers()
-                        .toMultimap()
-                        .map { it.key to it.value.joinToString(";") }
-                        .toMap()
-                        .toMutableMap()
-                        .iterator()
-                }
-            )
-
-        val traceId = (extractedContext as? ExtractedContext)?.traceId?.toString()
-        return traceId
-    }
-
-    // endregion
-
-    companion object {
-        private const val HEADER_CT = "Content-Type"
-
-        private val xhrMethods = arrayOf("POST", "PUT", "DELETE")
-    }
-}
+class RumInterceptor :
+    Interceptor by CombinedInterceptor(RumRequestInterceptor())
