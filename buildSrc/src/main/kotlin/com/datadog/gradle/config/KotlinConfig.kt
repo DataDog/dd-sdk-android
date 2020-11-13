95529f9ab03e182c5427804894c9027c8c3c17d7
Xavier Gouchet <xavier.gouchet@gmail.com>
2020-11-13 17:03:57 +0100

    Install CI-App

--- buildSrc/src/main/kotlin/com/datadog/gradle/config/KotlinConfig.kt
+++ buildSrc/src/main/kotlin/com/datadog/gradle/config/KotlinConfig.kt
@@ -6,6 +6,8 @@
 
 package com.datadog.gradle.config
 
+import com.android.build.gradle.tasks.factory.AndroidUnitTest
+import java.io.File
 import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
@@ -15,6 +17,22 @@ fun Project.kotlinConfig() {
     taskConfig<KotlinCompile> {
         kotlinOptions {
             jvmTarget = JavaVersion.VERSION_1_8.toString()
+        }
+    }
+
+    val moduleName = this@kotlinConfig.name
+    val javaAgentJar = File(File(rootDir, "libs"), "dd-java-agent-0.67.0.jar")
+    afterEvaluate {
+        taskConfig<AndroidUnitTest> {
+            if (environment["DD_INTEGRATION_JUNIT_5_ENABLED"] == "true") {
+                val variant = variantName.substringBeforeLast("UnitTest")
+
+                environment["DD_ENV_TESTS"]?.let { environment("DD_ENV", it) }
+                environment("DD_INTEGRATIONS_ENABLED", "false")
+                environment("DD_JMX_FETCH_ENABLED", "false")
+                environment("DD_TAGS", "test.module:$moduleName,test.variant:$variant")
+                jvmArgs("-javaagent:${javaAgentJar.absolutePath}")
+            }
         }
     }
 }
