e6964ee0cedf66c57e647573c79b8a0d7f458451
Xavier Gouchet <xavier.gouchet@gmail.com>
2021-02-05 20:13:24 +0100

    :green_heart: Fix dogfooding tool

--- dogfood.py
+++ dogfood.py
@@ -5,7 +5,7 @@
 # This product includes software developed at Datadog (https://www.datadoghq.com/).
 # Copyright 2019-Present Datadog, Inc
 
-
+import re
 import subprocess
 import sys
 import os
@@ -16,13 +16,29 @@ from typing import Tuple
 import requests
 from git import Repo
 
-REPO_ANDROID = "datadog-android"
+TARGET_APP = "app"
+TARGET_DEMO = "demo"
+
+REPOSITORIES = {TARGET_APP: "datadog-android", TARGET_DEMO: "shopist-android"}
+
+FILE_PATH = {
+    TARGET_APP: os.path.join("buildSrc", "src", "main", "java", "config", "dependency", "android", "Datadog.kt"),
+    TARGET_DEMO: os.path.join("buildSrc", "src", "main", "kotlin", "com", "datadog", "gradle", "Dependencies.kt")
+}
+
+PREFIX = {
+    TARGET_APP: "val version",
+    TARGET_DEMO: "val Datadog"
+}
 
 
 def parse_arguments(args: list) -> Namespace:
     parser = ArgumentParser()
 
     parser.add_argument("-v", "--version", required=True, help="the version of the SDK")
+    parser.add_argument("-t", "--target", required=True,
+                        choices=[TARGET_APP, TARGET_DEMO],
+                        help="the target repository")
 
     return parser.parse_args(args)
 
@@ -46,23 +62,25 @@ def github_create_pr(repository: str, branch_name: str, base_name: str, version:
         return 1
 
 
-def generate_target_code(temp_dir_path: str, version: str):
+def generate_target_code(target: str, temp_dir_path: str, version: str):
     print("… Generating code with version " + version)
-    target_file = os.path.join(temp_dir_path, "buildSrc", "src", "main", "java", "config", "dependency", "android",
-                               "Datadog.kt")
+    file_path = FILE_PATH[target]
+    target_file = os.path.join(temp_dir_path, file_path)
+    prefix = PREFIX[target]
+    regex = prefix + " = \"[0-9a-z\\.-]+\""
+
+    print("regex = ")
+    with open(target_file, 'r') as target:
+        content = target.read()
+        updated_content = re.sub(regex, prefix + " = \"" + version + "\"", content, flags=re.M)
+        # print(updated_content)
 
     with open(target_file, 'w') as target:
-        target.write("package config.dependency.android\n\n")
-        target.write("import config.dependency.Dependency\n\n")
-        target.write("object Datadog : Dependency {\n\n")
-        target.write("    override val group = \"com.datadoghq\"\n")
-        target.write("    override val artifact = \"dd-sdk-android\"\n")
-        target.write("    override val version = \"" + version + "\"\n")
-        target.write("}\n")
+        target.write(updated_content)
 
 
 def git_clone_repository(repo_name: str, gh_token: str, temp_dir_path: str) -> Tuple[Repo, str]:
-    print("… Cloning repository " + REPO_ANDROID)
+    print("… Cloning repository " + repo_name)
     url = "https://" + gh_token + ":x-oauth-basic@github.com/DataDog/" + repo_name
     repo = Repo.clone_from(url, temp_dir_path)
     base_name = repo.active_branch.name
@@ -79,17 +97,18 @@ def git_push_changes(repo: Repo, version: str):
     repo.git.push("--set-upstream", "--force", origin, repo.head.ref)
 
 
-def update_dependant(version: str, gh_token: str) -> int:
+def update_dependant(version: str, target: str, gh_token: str) -> int:
     branch_name = "update_sdk_" + version
     temp_dir = TemporaryDirectory()
     temp_dir_path = temp_dir.name
+    repo_name = REPOSITORIES[target]
 
-    repo, base_name = git_clone_repository(REPO_ANDROID, gh_token, temp_dir_path)
+    repo, base_name = git_clone_repository(repo_name, gh_token, temp_dir_path)
 
     print("… Creating branch " + branch_name)
     repo.git.checkout('HEAD', b=branch_name)
 
-    generate_target_code(temp_dir_path, version)
+    generate_target_code(target, temp_dir_path, version)
 
     if not repo.is_dirty():
         print("∅ Nothing to commit, all is in order…")
@@ -97,7 +116,7 @@ def update_dependant(version: str, gh_token: str) -> int:
 
     git_push_changes(repo, version)
 
-    return github_create_pr(REPO_ANDROID, branch_name, base_name, version, gh_token)
+    return github_create_pr(repo_name, branch_name, base_name, version, gh_token)
 
 
 def run_main() -> int:
@@ -108,7 +127,7 @@ def run_main() -> int:
     with open('gh_token', 'r') as f:
         gh_token = f.read().strip()
 
-    return update_dependant(cli_args.version, gh_token)
+    return update_dependant(cli_args.version, cli_args.target, gh_token)
 
 
 if __name__ == "__main__":
