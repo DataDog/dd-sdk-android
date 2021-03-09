3f1e817a03915065a5852322ad451da820cac222
Nikita Ogorodnikov <nikita.ogorodnikov@datadoghq.com>
2021-03-09 09:52:20 +0100

    RUMM-1153: Support simple collection types as return type for no-op factory

--- tools/noopfactory/src/test/resources/gen/NoOpGenericInterface.kt
+++ tools/noopfactory/src/test/resources/gen/NoOpGenericInterface.kt
@@ -1,6 +1,12 @@
 package com.example
 
 import kotlin.CharSequence
+import kotlin.collections.List
+import kotlin.collections.Map
+import kotlin.collections.Set
+import kotlin.collections.emptyList
+import kotlin.collections.emptyMap
+import kotlin.collections.emptySet
 
 internal class NoOpGenericInterface<T : CharSequence> : GenericInterface<T> {
 
@@ -14,4 +20,10 @@ internal class NoOpGenericInterface<T : CharSequence> : GenericInterface<T> {
     }
 
     override fun doSomethingWithNullableReturn(): T? = null
+
+    override fun doSomethingWithListReturn(): List<T> = emptyList()
+
+    override fun doSomethingWithMapReturn(): Map<T, T> = emptyMap()
+
+    override fun doSomethingWithSetReturn(): Set<T> = emptySet()
 }
--- tools/noopfactory/src/test/resources/gen/NoOpSimpleInterface.kt
+++ tools/noopfactory/src/test/resources/gen/NoOpSimpleInterface.kt
@@ -4,6 +4,12 @@ import java.util.Date
 import kotlin.ByteArray
 import kotlin.Int
 import kotlin.String
+import kotlin.collections.List
+import kotlin.collections.Map
+import kotlin.collections.Set
+import kotlin.collections.emptyList
+import kotlin.collections.emptyMap
+import kotlin.collections.emptySet
 
 internal class NoOpSimpleInterface : SimpleInterface {
 
@@ -31,4 +37,10 @@ internal class NoOpSimpleInterface : SimpleInterface {
     override fun doSomethingWithStringReturn(): String = ""
 
     override fun doSomethingWithNullableReturn(): Date? = null
+
+    override fun doSomethingWithListReturn(): List<String> = emptyList()
+
+    override fun doSomethingWithMapReturn(): Map<String, String> = emptyMap()
+
+    override fun doSomethingWithSetReturn(): Set<String> = emptySet()
 }
