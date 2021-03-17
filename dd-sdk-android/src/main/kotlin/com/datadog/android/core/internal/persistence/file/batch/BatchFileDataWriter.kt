46268cfc10ed485994986493fe4f0687166c6c7f
Xavier Gouchet <xavier.gouchet@gmail.com>
2021-03-17 19:29:00 +0100

    :recycle: (RUMM-1133): Implement Data Writer

---
+++ dd-sdk-android/src/main/kotlin/com/datadog/android/core/internal/persistence/file/batch/BatchFileDataWriter.kt
@@ -0,0 +1,75 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2016-Present Datadog, Inc.
+ */
+
+package com.datadog.android.core.internal.persistence.file.batch
+
+import com.datadog.android.core.internal.domain.PayloadDecoration
+import com.datadog.android.core.internal.persistence.DataWriter
+import com.datadog.android.core.internal.persistence.Serializer
+import com.datadog.android.core.internal.persistence.file.FileHandler
+import com.datadog.android.core.internal.persistence.file.FileOrchestrator
+
+/**
+ * A [DataWriter] storing data in batch files.
+ */
+internal open class BatchFileDataWriter<T : Any>(
+    internal val fileOrchestrator: FileOrchestrator,
+    internal val serializer: Serializer<T>,
+    internal val decoration: PayloadDecoration,
+    internal val handler: FileHandler
+) : DataWriter<T> {
+
+    // region DataWriter
+
+    override fun write(element: T) {
+        consume(element)
+    }
+
+    override fun write(data: List<T>) {
+        data.forEach { consume(it) }
+    }
+
+    // endregion
+
+    // region Protected
+
+    /**
+     * Called whenever data is written successfully.
+     * @param data the data written
+     */
+    internal open fun onDataWritten(data: T, rawData: ByteArray) {}
+
+    /**
+     * Called whenever data failed to be written.
+     * @param data the data
+     */
+    internal open fun onDataWriteFailed(data: T) {}
+
+    // endregion
+
+    // region Internal
+
+    private fun consume(data: T) {
+        val serialized = serializer.serialize(data) ?: return
+        val byteArray = serialized.toByteArray(Charsets.UTF_8)
+
+        synchronized(this) {
+            val success = writeData(byteArray)
+            if (success) {
+                onDataWritten(data, byteArray)
+            } else {
+                onDataWriteFailed(data)
+            }
+        }
+    }
+
+    private fun writeData(byteArray: ByteArray): Boolean {
+        val file = fileOrchestrator.getWritableFile(byteArray.size) ?: return false
+        return handler.writeData(file, byteArray, true, decoration.separatorBytes)
+    }
+
+    // endregion
+}
