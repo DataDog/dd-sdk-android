512ee2039ffdfe25852dd9c47fe71e508a06f6a9
Marius Constantin <marius.constantin@datadoghq.com>
2021-03-23 16:48:14 +0100

    RUMM-1170 Cleanup TracerWriter

--- dd-sdk-android/src/main/kotlin/com/datadog/android/tracing/internal/data/TraceWriter.kt
+++ dd-sdk-android/src/main/kotlin/com/datadog/android/tracing/internal/data/TraceWriter.kt
@@ -7,14 +7,8 @@
 package com.datadog.android.tracing.internal.data
 
 import com.datadog.android.core.internal.persistence.DataWriter
-import com.datadog.android.rum.GlobalRum
-import com.datadog.android.rum.RumAttributes
-import com.datadog.android.rum.RumErrorSource
-import com.datadog.android.rum.internal.monitor.AdvancedRumMonitor
 import com.datadog.opentracing.DDSpan
-import com.datadog.trace.api.DDTags
 import com.datadog.trace.common.writer.Writer
-import java.util.Locale
 
 internal class TraceWriter(
     val writer: DataWriter<DDSpan>
@@ -40,62 +34,4 @@ internal class TraceWriter(
     }
 
     // endregion
-
-    // region Internals
-
-    private fun sendRumErrorEvent(span: DDSpan) {
-        val errorType = span.tags[DDTags.ERROR_TYPE] as? String
-        val errorMessage = span.tags[DDTags.ERROR_MSG] as? String
-        val composedErrorMessage = spanErrorMessage(span.operationName, errorType, errorMessage)
-        val attributes = if (errorType != null) {
-            mapOf(RumAttributes.INTERNAL_ERROR_TYPE to errorType)
-        } else {
-            emptyMap()
-        }
-        (GlobalRum.get() as? AdvancedRumMonitor)?.addErrorWithStacktrace(
-            composedErrorMessage,
-            RumErrorSource.SOURCE,
-            span.tags[DDTags.ERROR_STACK]?.toString(),
-            attributes
-        )
-    }
-
-    private fun spanErrorMessage(
-        operationName: String,
-        errorType: String?,
-        errorMessage: String?
-    ): String {
-        return when {
-            errorMessage != null && errorType != null ->
-                SPAN_ERROR_WITH_TYPE_AND_MESSAGE_FORMAT.format(
-                    Locale.US,
-                    operationName,
-                    errorType,
-                    errorMessage
-                )
-            errorType != null ->
-                SPAN_ERROR_WITH_TYPE_FORMAT.format(
-                    Locale.US,
-                    operationName,
-                    errorType
-                )
-            errorMessage != null ->
-                SPAN_ERROR_WITH_MESSAGE_FORMAT.format(
-                    Locale.US,
-                    operationName,
-                    errorMessage
-                )
-            else -> SPAN_ERROR_FORMAT.format(Locale.US, operationName)
-        }
-    }
-
-    // endregion
-
-    companion object {
-        internal const val SPAN_ERROR_FORMAT: String = "Span error (%s)"
-        internal const val SPAN_ERROR_WITH_TYPE_AND_MESSAGE_FORMAT: String =
-            "Span error (%s): %s | %s "
-        internal const val SPAN_ERROR_WITH_TYPE_FORMAT: String = "Span error (%s): %s"
-        internal const val SPAN_ERROR_WITH_MESSAGE_FORMAT: String = "Span error (%s): %s"
-    }
 }
