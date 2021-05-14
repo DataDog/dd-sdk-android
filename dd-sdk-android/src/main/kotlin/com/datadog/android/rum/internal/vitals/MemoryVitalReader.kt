d1df87abba240b07706894c528982a0d635239c6
Xavier Gouchet <xavier.gouchet@gmail.com>
2021-05-14 14:25:17 +0200

    RUMM-1273 read memory VmRss

---
+++ dd-sdk-android/src/main/kotlin/com/datadog/android/rum/internal/vitals/MemoryVitalReader.kt
@@ -0,0 +1,41 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2016-Present Datadog, Inc.
+ */
+
+package com.datadog.android.rum.internal.vitals
+
+import com.datadog.android.core.internal.persistence.file.canReadSafe
+import com.datadog.android.core.internal.persistence.file.existsSafe
+import java.io.File
+
+/**
+ * Reads the device's `VmRSS` based on the `/proc/self/status` file.
+ * cf. documentation https://man7.org/linux/man-pages/man5/procfs.5.html
+ */
+internal class MemoryVitalReader(
+    private val statusFile: File = STATUS_FILE
+) : VitalReader {
+
+    override fun readVitalData(): Double? {
+        if (!(statusFile.existsSafe() && statusFile.canReadSafe())) {
+            return null
+        }
+
+        return statusFile.readLines()
+            .mapNotNull { line ->
+                VM_RSS_REGEX.matchEntire(line)?.groupValues?.get(1)
+            }
+            .firstOrNull()
+            ?.toDoubleOrNull()
+    }
+
+    companion object {
+
+        private const val STATUS_PATH = "/proc/self/status"
+        private val STATUS_FILE = File(STATUS_PATH)
+        private const val VM_RSS_PATTERN = "VmRSS:\\s+(\\d+) kB"
+        private val VM_RSS_REGEX = Regex(VM_RSS_PATTERN)
+    }
+}
