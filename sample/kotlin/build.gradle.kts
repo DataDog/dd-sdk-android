24c3d9435bee1f0a0120943e8118e1df5ecbfb60
Marius Constantin <marius.constantin@datadoghq.com>
2021-05-19 13:53:33 +0200

    RUMM-1263 Record  RUM events while in backround

--- sample/kotlin/build.gradle.kts
+++ sample/kotlin/build.gradle.kts
@@ -125,9 +125,10 @@ dependencies {
     // Android dependencies
     implementation(Dependencies.Libraries.AndroidXMultidex)
     implementation(Dependencies.Libraries.AndroidXNavigation)
-    implementation("androidx.appcompat:appcompat:1.2.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.1")
-    implementation("com.google.android.material:material:1.2.1")
+    implementation("androidx.appcompat:appcompat:1.3.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+    implementation("com.google.android.material:material:1.3.0")
+    implementation("androidx.media:media:1.3.1")
     implementation("androidx.vectordrawable:vectordrawable:1.1.0")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
