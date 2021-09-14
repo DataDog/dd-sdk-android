754dac59dd452468fa65f8efd0a9ee53e7f7733f
Marius Constantin <marius.constantin@datadoghq.com>
2021-09-14 14:58:46 +0200

    RUMM-1535 Add remaining nightly tests on AndroidTracer.Builder

--- build.gradle.kts
+++ build.gradle.kts
@@ -234,4 +234,4 @@ tasks.register("buildNdkIntegrationTestsArtifacts") {
     dependsOn(":instrumented:integration:assembleDebug")
 }
 
-nightlyTestsCoverageConfig(threshold = 0.76f)
+nightlyTestsCoverageConfig(threshold = 0.8f)
