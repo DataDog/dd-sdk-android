2309477accd141b48b83726703c21945a6fdef80
Marius Constantin <marius.constantin@datadoghq.com>
2021-11-04 14:48:00 +0100

    RUMM-1720 Get the webViewTrackingHosts in CoreFeature

--- dd-sdk-android/src/main/kotlin/com/datadog/android/core/internal/CoreFeature.kt
+++ dd-sdk-android/src/main/kotlin/com/datadog/android/core/internal/CoreFeature.kt
@@ -112,6 +112,7 @@ internal object CoreFeature {
 
     internal lateinit var uploadExecutorService: ScheduledThreadPoolExecutor
     internal lateinit var persistenceExecutorService: ExecutorService
+    internal lateinit var webViewTrackingHosts: List<String>
 
     fun initialize(
         appContext: Context,
@@ -129,6 +130,7 @@ internal object CoreFeature {
         initializeClockSync(appContext)
         setupOkHttpClient(configuration)
         firstPartyHostDetector.addKnownHosts(configuration.firstPartyHosts)
+        webViewTrackingHosts = configuration.webViewTrackingHosts
         setupExecutors()
         // Time Provider
         timeProvider = KronosTimeProvider(kronosClock)
