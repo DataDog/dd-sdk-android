983663e340841feb386959b42b74efca4fd641f2
Marius Constantin <marius.constantin@datadoghq.com>
2021-11-05 14:10:53 +0100

    RUMM-1720 Introduce the DatadogEventBridge interface

---
+++ dd-sdk-android/src/main/kotlin/com/datadog/android/rum/webview/DatadogEventBridge.kt
@@ -0,0 +1,52 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2016-Present Datadog, Inc.
+ */
+
+package com.datadog.android.rum.webview
+
+import android.webkit.JavascriptInterface
+import com.datadog.android.core.internal.CoreFeature
+import com.google.gson.JsonArray
+
+/**
+ * This [JavascriptInterface] is used to intercept all the Datadog events produced by
+ * the displayed web content when there is already a Datadog browser-sdk attached.
+ * The goal is to make those events part of the mobile session.
+ * Please note that the WebView will not be tracked unless you add the host name in the hosts
+ * lists in the SDK configuration.
+ * @see [com.datadog.android.core.configuration.Configuration.Builder]
+ */
+class DatadogEventBridge {
+
+    internal var webEventConsumer = WebEventConsumer(
+        RumEventConsumer(),
+        LogsEventConsumer()
+    )
+
+    /**
+     * Called from the browser-sdk side whenever there is a new RUM/LOG event
+     * available related with the tracked WebView.
+     * @param event as the bundled web event as a Json string
+     */
+    @JavascriptInterface
+    fun send(event: String) {
+        webEventConsumer.consume(event)
+    }
+
+    /**
+     * Called from the browser-sdk to get the list of hosts for which the WebView tracking is
+     * allowed.
+     * @return the list of hosts as a String JsonArray
+     */
+    @JavascriptInterface
+    fun getAllowedWebViewHosts(): String {
+        // We need to use a JsonArray here otherwise it cannot be parsed on the JS side
+        val origins = JsonArray()
+        CoreFeature.webViewTrackingHosts.forEach {
+            origins.add(it)
+        }
+        return origins.toString()
+    }
+}
---
+++ dd-sdk-android/src/test/kotlin/com/datadog/android/rum/internal/webview/WebEventConsumerTest.kt
@@ -0,0 +1,244 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2016-Present Datadog, Inc.
+ */
+
+package com.datadog.android.rum.internal.webview
+
+import android.util.Log
+import com.datadog.android.log.internal.logger.LogHandler
+import com.datadog.android.rum.webview.LogsEventConsumer
+import com.datadog.android.rum.webview.RumEventConsumer
+import com.datadog.android.rum.webview.WebEventConsumer
+import com.datadog.android.utils.forge.Configurator
+import com.datadog.android.utils.mockSdkLogHandler
+import com.datadog.android.utils.restoreSdkLogHandler
+import com.google.gson.JsonArray
+import com.google.gson.JsonObject
+import com.google.gson.JsonParseException
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.anyOrNull
+import com.nhaarman.mockitokotlin2.argThat
+import com.nhaarman.mockitokotlin2.eq
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.verifyZeroInteractions
+import fr.xgouchet.elmyr.Forge
+import fr.xgouchet.elmyr.junit5.ForgeConfiguration
+import fr.xgouchet.elmyr.junit5.ForgeExtension
+import java.util.Locale.US
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.api.extension.Extensions
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.junit.jupiter.MockitoSettings
+import org.mockito.quality.Strictness
+
+@Extensions(
+    ExtendWith(MockitoExtension::class),
+    ExtendWith(ForgeExtension::class),
+)
+@MockitoSettings(strictness = Strictness.LENIENT)
+@ForgeConfiguration(Configurator::class)
+internal class WebEventConsumerTest {
+
+    lateinit var testedWevEventConsumer: WebEventConsumer
+
+    @Mock
+    lateinit var mockRumEventConsumer: RumEventConsumer
+
+    @Mock
+    lateinit var mockLogsEventConsumer: LogsEventConsumer
+
+    @Mock
+    lateinit var mockSdkLogHandler: LogHandler
+
+    lateinit var originalSdkLogHandler: LogHandler
+
+    @BeforeEach
+    fun `set up`() {
+        originalSdkLogHandler = mockSdkLogHandler(mockSdkLogHandler)
+        testedWevEventConsumer = WebEventConsumer(mockRumEventConsumer, mockLogsEventConsumer)
+    }
+
+    @AfterEach
+    fun `tear down`() {
+        restoreSdkLogHandler(originalSdkLogHandler)
+    }
+
+    // region Unit Tests
+
+    @Test
+    fun `M delegate to RumEventConsumer W consume() { any event type except LOG }`(forge: Forge) {
+        // Given
+        val fakeBundledEvent = forge.getForgery<JsonObject>()
+        var fakeEventType = forge.anAlphabeticalString()
+        while (fakeEventType == WebEventConsumer.LOG_EVENT_TYPE) {
+            fakeEventType = forge.anAlphabeticalString()
+        }
+        val fakeWebEvent = bundleWebEvent(fakeBundledEvent, fakeEventType)
+
+        // When
+        testedWevEventConsumer.consume(fakeWebEvent.toString())
+
+        // Then
+        verify(mockRumEventConsumer).consume(
+            fakeBundledEvent,
+            fakeEventType
+        )
+    }
+
+    @Test
+    fun `M delegate to LogsEventConsumer W consume() { LOG eventType }`(forge: Forge) {
+        // Given
+        val fakeBundledEvent = forge.getForgery<JsonObject>()
+        val fakeWebEvent = bundleWebEvent(fakeBundledEvent, WebEventConsumer.LOG_EVENT_TYPE)
+
+        // When
+        testedWevEventConsumer.consume(fakeWebEvent.toString())
+
+        // Then
+        verify(mockLogsEventConsumer).consume(fakeBundledEvent)
+    }
+
+    @Test
+    fun `M do nothing W consume() { no event type }`(forge: Forge) {
+        // Given
+        val fakeBundledEvent = forge.getForgery<JsonObject>()
+        val fakeWebEvent = bundleWebEvent(fakeBundledEvent, null)
+
+        // When
+        testedWevEventConsumer.consume(fakeWebEvent.toString())
+
+        // Then
+        verifyZeroInteractions(mockLogsEventConsumer)
+        verifyZeroInteractions(mockRumEventConsumer)
+    }
+
+    @Test
+    fun `M log internal error event W consume() { no event type }`(forge: Forge) {
+        // Given
+        val fakeBundledEvent = forge.getForgery<JsonObject>()
+        val fakeWebEvent = bundleWebEvent(fakeBundledEvent, null)
+
+        // When
+        testedWevEventConsumer.consume(fakeWebEvent.toString())
+
+        // Then
+        verify(mockSdkLogHandler).handleLog(
+            Log.ERROR,
+            WebEventConsumer.WEB_EVENT_MISSING_TYPE_ERROR_MESSAGE.format(
+                US,
+                fakeWebEvent
+            )
+        )
+    }
+
+    @Test
+    fun `M do nothing W consume() { no event }`(forge: Forge) {
+        // Given
+        val fakeEventType = forge.anAlphabeticalString()
+        val fakeWebEvent = bundleWebEvent(null, fakeEventType)
+
+        // When
+        testedWevEventConsumer.consume(fakeWebEvent.toString())
+
+        // Then
+        verifyZeroInteractions(mockLogsEventConsumer)
+        verifyZeroInteractions(mockRumEventConsumer)
+    }
+
+    @Test
+    fun `M log internal error event W consume() { no event }`(forge: Forge) {
+        // Given
+        val fakeEventType = forge.anAlphabeticalString()
+        val fakeWebEvent = bundleWebEvent(null, fakeEventType)
+
+        // When
+        testedWevEventConsumer.consume(fakeWebEvent.toString())
+
+        // Then
+        verify(mockSdkLogHandler).handleLog(
+            Log.ERROR,
+            WebEventConsumer.WEB_EVENT_MISSING_WRAPPED_EVENT.format(US, fakeWebEvent)
+        )
+    }
+
+    @Test
+    fun `M do nothing W consume(){ bad json format }`(forge: Forge) {
+        // Given
+        val fakeBundledEvent = forge.getForgery<JsonObject>()
+        var fakeEventType = forge.anAlphabeticalString()
+        while (fakeEventType == WebEventConsumer.LOG_EVENT_TYPE) {
+            fakeEventType = forge.anAlphabeticalString()
+        }
+        val fakeWebEvent = bundleWebEvent(fakeBundledEvent, fakeEventType)
+        val fakeBadJsonFormatEvent = fakeWebEvent.toString() + forge.anAlphabeticalString()
+
+        // When
+        testedWevEventConsumer.consume(fakeBadJsonFormatEvent)
+
+        // Then
+        verifyZeroInteractions(mockLogsEventConsumer)
+        verifyZeroInteractions(mockRumEventConsumer)
+    }
+
+    @Test
+    fun `M log internal error W consume(){ bad json format }`(forge: Forge) {
+        // Given
+        val fakeBundledEvent = forge.getForgery<JsonObject>()
+        var fakeEventType = forge.anAlphabeticalString()
+        while (fakeEventType == WebEventConsumer.LOG_EVENT_TYPE) {
+            fakeEventType = forge.anAlphabeticalString()
+        }
+        val fakeWebEvent = bundleWebEvent(fakeBundledEvent, fakeEventType)
+        val fakeBadJsonFormatEvent = fakeWebEvent.toString() + forge.anAlphabeticalString()
+
+        // When
+        testedWevEventConsumer.consume(fakeBadJsonFormatEvent)
+
+        // Then
+        verify(mockSdkLogHandler).handleLog(
+            eq(Log.ERROR),
+            eq(
+                WebEventConsumer.WEB_EVENT_PARSING_ERROR_MESSAGE.format(
+                    US,
+                    fakeBadJsonFormatEvent
+                )
+            ),
+            argThat { this is JsonParseException },
+            any(),
+            any(),
+            anyOrNull()
+        )
+    }
+
+    // endregion
+
+    // region Internal
+
+    private fun bundleWebEvent(
+        fakeBundledEvent: JsonObject?,
+        eventType: String?,
+        tags: List<String> = emptyList()
+    ): JsonObject {
+        val fakeWebEvent = JsonObject()
+        val tagsAsJsonArray = JsonArray()
+        tags.forEach {
+            tagsAsJsonArray.add(it)
+        }
+        fakeBundledEvent?.let {
+            fakeWebEvent.add(WebEventConsumer.EVENT_KEY, it)
+        }
+        eventType?.let {
+            fakeWebEvent.addProperty(WebEventConsumer.EVENT_TYPE_KEY, it)
+        }
+        fakeWebEvent.add(WebEventConsumer.EVENT_TAGS, tagsAsJsonArray)
+        return fakeWebEvent
+    }
+
+    // endregion
+}
