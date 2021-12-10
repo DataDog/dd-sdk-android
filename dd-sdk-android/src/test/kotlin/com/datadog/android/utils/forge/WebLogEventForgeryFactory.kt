54c539c528635fe95b2bb31c023a852474b6df61
Marius Constantin <marius.constantin@datadoghq.com>
2021-12-10 09:55:17 +0100

    RUMM-1825 Add dedicated WebView tracking features

---
+++ dd-sdk-android/src/test/kotlin/com/datadog/android/utils/forge/WebLogEventForgeryFactory.kt
@@ -0,0 +1,38 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2016-Present Datadog, Inc.
+ */
+
+package com.datadog.android.utils.forge
+
+import com.datadog.android.log.model.LogEvent
+import com.datadog.android.log.model.WebLogEvent
+import fr.xgouchet.elmyr.Forge
+import fr.xgouchet.elmyr.ForgeryFactory
+
+internal class WebLogEventForgeryFactory : ForgeryFactory<WebLogEvent> {
+    override fun getForgery(forge: Forge): WebLogEvent {
+        val reservedKeysAsSet = mutableSetOf<String>().apply {
+            LogEvent.RESERVED_PROPERTIES.forEach {
+                this.add(it)
+            }
+        }
+
+        return WebLogEvent(
+            service = forge.anAlphabeticalString(),
+            status = forge.aValueFrom(WebLogEvent.Status::class.java),
+            message = forge.anAlphabeticalString(),
+            date = forge.aFormattedTimestamp(),
+            additionalProperties = forge.exhaustiveAttributes(
+                excludedKeys = reservedKeysAsSet,
+                filterThreshold = 0f
+            ),
+            ddtags = forge.exhaustiveTags().joinToString(separator = ",")
+        )
+    }
+
+    private fun Forge.exhaustiveTags(): List<String> {
+        return aList { aStringMatching("[a-z]([a-z0-9_:./-]{0,198}[a-z0-9_./-])?") }
+    }
+}
