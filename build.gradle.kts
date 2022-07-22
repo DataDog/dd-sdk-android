43910d69bd0a81dcfb84a126c2db79078fc1a786
Marius Constantin <marius.constantin@datadoghq.com>
2022-07-22 10:14:57 +0200

    Adjust nightly tests coverage

--- build.gradle.kts
+++ build.gradle.kts
@@ -262,7 +262,7 @@ tasks.register("buildNdkIntegrationTestsArtifacts") {
     dependsOn(":instrumented:integration:assembleDebug")
 }
 
-nightlyTestsCoverageConfig(threshold = 0.90f)
+nightlyTestsCoverageConfig(threshold = 0.89f)
 kover {
     isDisabled = false
     disabledProjects = setOf(
