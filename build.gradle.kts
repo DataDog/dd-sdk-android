759fb891cfe5aae1ad3440fb9452435441807619
Nikita Ogorodnikov <nikita.ogorodnikov@datadoghq.com>
2022-09-05 11:54:53 +0200

    Update nightly test coverage threshold

--- build.gradle.kts
+++ build.gradle.kts
@@ -257,7 +257,7 @@ tasks.register("buildNdkIntegrationTestsArtifacts") {
     dependsOn(":instrumented:integration:assembleDebug")
 }
 
-nightlyTestsCoverageConfig(threshold = 0.92f)
+nightlyTestsCoverageConfig(threshold = 0.91f)
 kover {
     isDisabled = false
     disabledProjects = setOf(
