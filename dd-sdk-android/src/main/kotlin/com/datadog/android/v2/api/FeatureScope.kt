3eaf25d9b0a6127d3331b148032820af2092cfd9
Nikita Ogorodnikov <nikita.ogorodnikov@datadoghq.com>
2023-01-04 16:45:33 +0100

    RUMM-2834: Tracing feature stores context in the common context storage

--- dd-sdk-android/src/main/kotlin/com/datadog/android/v2/api/FeatureScope.kt
+++ dd-sdk-android/src/main/kotlin/com/datadog/android/v2/api/FeatureScope.kt
@@ -17,7 +17,9 @@ interface FeatureScope {
      * @param forceNewBatch if `true` forces the [EventBatchWriter] to write in a new file and
      * not reuse the already existing pending data persistence file. By default it is `false`.
      * @param callback an operation called with an up-to-date [DatadogContext]
-     * and an [EventBatchWriter]. Callback will be executed on a worker thread from I/O pool
+     * and an [EventBatchWriter]. Callback will be executed on a worker thread from I/O pool.
+     * [DatadogContext] will have a state created at the moment this method is called, before the
+     * thread switch for the callback invocation.
      */
     fun withWriteContext(
         forceNewBatch: Boolean = false,
