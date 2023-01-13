7d97ab30b8954a52cf6dbf2e58ca26fc731b2a69
Marius Constantin <marius.constantin@datadoghq.com>
2023-01-13 10:26:06 +0100

    Promote to 1.17.0-sr-beta1-snapshot

--- buildSrc/src/main/kotlin/com/datadog/gradle/config/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/com/datadog/gradle/config/AndroidConfig.kt
@@ -18,7 +18,7 @@ object AndroidConfig {
     const val MIN_SDK_FOR_COMPOSE = 21
     const val BUILD_TOOLS_VERSION = "33.0.0"
 
-    val VERSION = Version(1, 17, 0, Version.Type.SrAlphaSnapshot(1))
+    val VERSION = Version(1, 17, 0, Version.Type.SrBetaSnapshot(1))
 }
 
 @Suppress("UnstableApiUsage")
