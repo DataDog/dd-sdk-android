e910f0b07af5a6d3c50bb393df7258129dddaf3a
Marius Constantin <marius.constantin@datadoghq.com>
2023-03-08 14:19:52 +0100

    Add function wildcard generics capability for ApiSurfaceGenerator

--- buildSrc/src/test/kotlin/com/datadog/gradle/plugin/apisurface/KotlinFileVisitorTest.kt
+++ buildSrc/src/test/kotlin/com/datadog/gradle/plugin/apisurface/KotlinFileVisitorTest.kt
@@ -467,6 +467,29 @@ internal class KotlinFileVisitorTest {
     }
 
     @Test
+    fun `describes wildcard generics in functions`() {
+        tempFile.writeText(
+            """
+            package foo.bar
+            import java.io.IOException
+            class Spam {
+                fun <K, V> doSomethingElse(map : Map<*, *>) : Pair<List<*>, List<*>> {
+                    TODO()
+                }
+            }
+            """.trimIndent()
+        )
+
+        testedVisitor.visitFile(tempFile)
+
+        assertEquals(
+            "class foo.bar.Spam\n" +
+                "  fun <K, V> doSomethingElse(Map<*, *>): Pair<List<*>, List<*>>\n",
+            testedVisitor.description.toString()
+        )
+    }
+
+    @Test
     fun `describes parent class and interfaces in class types`() {
         tempFile.writeText(
             """
