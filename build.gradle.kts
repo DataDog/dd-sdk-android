1755b811e4bad07be2f53b2d9200a810713eb092
Nikita Ogorodnikov <nikita.ogorodnikov@datadoghq.com>
2023-06-07 11:13:03 +0200

    Fix typos in some modules names

--- build.gradle.kts
+++ build.gradle.kts
@@ -274,7 +274,7 @@ tasks.register("buildNdkIntegrationTestsArtifacts") {
     dependsOn(":instrumented:integration:assembleDebug")
 }
 
-nightlyTestsCoverageConfig(threshold = 0.84f)
+nightlyTestsCoverageConfig(threshold = 0.83f)
 kover {
     isDisabled = false
     disabledProjects = setOf(
--- local_ci.sh
+++ local_ci.sh
@@ -76,7 +76,7 @@ if [[ $CLEANUP == 1 ]]; then
   rm -rf features/dd-sdk-android-ndk/build/
   rm -rf features/dd-sdk-android-rum/build/
   rm -rf features/dd-sdk-android-session-replay/build/
-  rm -rf features/dd-sdk-android-session-material/build/
+  rm -rf features/dd-sdk-android-session-replay-material/build/
   rm -rf features/dd-sdk-android-trace/build/
   rm -rf features/dd-sdk-android-webview/build/
   rm -rf integrations/dd-sdk-android-coil/build/
