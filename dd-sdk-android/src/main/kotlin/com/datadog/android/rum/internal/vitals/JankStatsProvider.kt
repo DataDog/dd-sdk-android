85038b5f83b3b32130ef561f3b2eb14e02780543
Xavier Gouchet <xavier.gouchet@gmail.com>
2023-06-29 16:52:13 +0200

    RUMM-3356 update the JankStats usage to report refresh rate metrics

---
+++ dd-sdk-android/src/main/kotlin/com/datadog/android/rum/internal/vitals/JankStatsProvider.kt
@@ -0,0 +1,46 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2016-Present Datadog, Inc.
+ */
+
+package com.datadog.android.rum.internal.vitals
+
+import android.view.Window
+import androidx.annotation.UiThread
+import androidx.metrics.performance.JankStats
+import com.datadog.android.v2.api.InternalLogger
+
+internal interface JankStatsProvider {
+
+    @UiThread
+    fun createJankStatsAndTrack(
+        window: Window,
+        listener: JankStats.OnFrameListener,
+        internalLogger: InternalLogger
+    ): JankStats?
+
+    companion object {
+
+        val DEFAULT = object : JankStatsProvider {
+            @UiThread
+            override fun createJankStatsAndTrack(
+                window: Window,
+                listener: JankStats.OnFrameListener,
+                internalLogger: InternalLogger
+            ): JankStats? {
+                return try {
+                    JankStats.createAndTrack(window, listener)
+                } catch (e: IllegalStateException) {
+                    internalLogger.log(
+                        InternalLogger.Level.ERROR,
+                        InternalLogger.Target.MAINTAINER,
+                        "Unable to attach JankStats to the current window",
+                        e
+                    )
+                    null
+                }
+            }
+        }
+    }
+}
