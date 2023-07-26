b6336b0c9f1ab0b35650c424499236ebc67a98d3
Nikita Ogorodnikov <nikita.ogorodnikov@datadoghq.com>
2023-07-26 12:52:18 +0200

    Prepare 2.0.0-beta3 release

--- buildSrc/src/main/kotlin/com/datadog/gradle/config/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/com/datadog/gradle/config/AndroidConfig.kt
@@ -18,7 +18,7 @@ object AndroidConfig {
     const val MIN_SDK_FOR_WEAR = 23
     const val BUILD_TOOLS_VERSION = "33.0.2"
 
-    val VERSION = Version(2, 0, 0, Version.Type.Beta(2))
+    val VERSION = Version(2, 0, 0, Version.Type.Beta(3))
 }
 
 // TODO RUMM-3263 Switch to Java 17 bytecode
