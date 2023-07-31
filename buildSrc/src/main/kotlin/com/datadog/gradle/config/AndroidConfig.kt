0b41406daa4163c85fbf4af3177b30020064e14a
Marius Constantin <marius.constantin@datadoghq.com>
2023-07-31 13:11:53 +0200

    Next dev cycle 2.1.0

--- buildSrc/src/main/kotlin/com/datadog/gradle/config/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/com/datadog/gradle/config/AndroidConfig.kt
@@ -18,7 +18,7 @@ object AndroidConfig {
     const val MIN_SDK_FOR_WEAR = 23
     const val BUILD_TOOLS_VERSION = "33.0.2"
 
-    val VERSION = Version(2, 0, 0, Version.Type.Snapshot)
+    val VERSION = Version(2, 1, 0, Version.Type.Snapshot)
 }
 
 // TODO RUMM-3263 Switch to Java 17 bytecode
