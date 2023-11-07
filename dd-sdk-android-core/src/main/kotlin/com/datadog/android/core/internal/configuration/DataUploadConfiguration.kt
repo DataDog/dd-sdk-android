7dbfa0a2517bd84b9b0eed844b6c1e3da6c614aa
Marius Constantin <marius.constantin@datadoghq.com>
2023-11-07 14:07:14 +0100

    RUM-1983 Introduce the BatchProcessingLevel API  RUM-1983 Introduce the BatchProcessingLevel API RUM-1983 Introduce the BatchProcessingLevel API

--- dd-sdk-android-core/src/main/kotlin/com/datadog/android/core/internal/configuration/DataUploadConfiguration.kt
+++ dd-sdk-android-core/src/main/kotlin/com/datadog/android/core/internal/configuration/DataUploadConfiguration.kt
@@ -8,10 +8,14 @@ package com.datadog.android.core.internal.configuration
 
 import com.datadog.android.core.configuration.UploadFrequency
 
-internal data class DataUploadConfiguration(internal val frequency: UploadFrequency) {
+internal data class DataUploadConfiguration(
+    internal val frequency: UploadFrequency,
+    internal val maxBatchesPerUploadJob: Int
+) {
     internal val minDelayMs = MIN_DELAY_FACTOR * frequency.baseStepMs
     internal val maxDelayMs = MAX_DELAY_FACTOR * frequency.baseStepMs
     internal val defaultDelayMs = DEFAULT_DELAY_FACTOR * frequency.baseStepMs
+
     companion object {
         internal const val MIN_DELAY_FACTOR = 1
         internal const val MAX_DELAY_FACTOR = 10
