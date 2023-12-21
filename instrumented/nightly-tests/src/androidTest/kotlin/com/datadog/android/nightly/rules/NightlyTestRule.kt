378540994c899594d2dcdf39711dd1e451abdf0a
Nikita Ogorodnikov <nikita.ogorodnikov@datadoghq.com>
2023-12-21 16:23:18 +0100

    Add delay before executors are flushed and stopped in nightly tests

--- instrumented/nightly-tests/src/androidTest/kotlin/com/datadog/android/nightly/rules/NightlyTestRule.kt
+++ instrumented/nightly-tests/src/androidTest/kotlin/com/datadog/android/nightly/rules/NightlyTestRule.kt
@@ -17,6 +17,19 @@ class NightlyTestRule : ExternalResource() {
     override fun after() {
         super.after()
         InstrumentationRegistry.getInstrumentation().waitForIdleSync()
+
+        // waitForIdleSync above waits only for idle of the main thread. We need this delay to be
+        // able to digest all the events, especially for RUM. Flushing and stopping executors below
+        // is not enough, because write logic may be complicated. Imagine resource error in
+        // background tracking in RUM:
+        // - error is submitted to RUM monitor
+        // - we flush and shutdown executors. During this process we process tasks in RUM (error),
+        // and process tasks in persistence thread (write this error)
+        // - once error is written it will trigger event saying it is written. This event is
+        // submitted to RUM (which was already stopped), so that we can send 1st view event
+        // (impossible to process - RUM is stopped; impossible to write - persistence executor is stopped)
+        Thread.sleep(200)
+
         flushAndShutdownExecutors()
         stopSdk()
         cleanStorageFiles()
