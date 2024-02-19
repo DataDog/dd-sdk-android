3536a5f9ec20b050998124e63e58c2ec9a98a8d9
Xavier F. Gouchet <github@xgouchet.fr>
2024-02-19 11:09:19 +0100

    RUM-2895 Read composition group data parameter fields

---
+++ features/dd-sdk-android-session-replay-compose/src/main/kotlin/com/datadog/android/sessionreplay/compose/internal/reflection/ComposeReflection.kt
@@ -0,0 +1,14 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2016-Present Datadog, Inc.
+ */
+
+package com.datadog.android.sessionreplay.compose.internal.reflection
+
+import java.lang.reflect.Field
+
+internal fun Field.accessible(): Field {
+    isAccessible = true
+    return this
+}
