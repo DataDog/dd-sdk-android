4ad6c3c989d7c1b49b9b9fa4c3c7f450f6173fc5
Xavier F. Gouchet <github@xgouchet.fr>
2024-05-13 11:43:49 +0200

    Improve UT for SR Obfuscators

--- features/dd-sdk-android-session-replay/src/main/kotlin/com/datadog/android/sessionreplay/internal/recorder/obfuscator/AndroidNStringObfuscator.kt
+++ features/dd-sdk-android-session-replay/src/main/kotlin/com/datadog/android/sessionreplay/internal/recorder/obfuscator/AndroidNStringObfuscator.kt
@@ -15,8 +15,9 @@ import com.datadog.android.sessionreplay.internal.recorder.obfuscator.StringObfu
  */
 @RequiresApi(Build.VERSION_CODES.N)
 class AndroidNStringObfuscator : StringObfuscator {
+
     override fun obfuscate(stringValue: String): String {
-        return buildString {
+        return buildString(stringValue.length) {
             stringValue.codePoints().forEach {
                 if (Character.isWhitespace(it)) {
                     // I don't think we should log this case here. I could not even reproduce it
