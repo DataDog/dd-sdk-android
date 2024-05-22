292ee30c9f8fb33aa41c4b2afb741ee6a029a229
Marius Constantin <marius.constantin@datadoghq.com>
2024-05-22 14:35:50 +0200

    Remove java.util.Stream package usage

--- features/dd-sdk-android-trace/src/main/java/com/datadog/trace/core/util/TagsMatcher.java
+++ features/dd-sdk-android-trace/src/main/java/com/datadog/trace/core/util/TagsMatcher.java
@@ -2,27 +2,28 @@ package com.datadog.trace.core.util;
 
 import com.datadog.trace.core.CoreSpan;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 public class TagsMatcher {
 
   public static TagsMatcher create(Map<String, String> tags) {
-    Map<String, Matcher> matchers =
-        tags.entrySet().stream()
-            .collect(
-                Collectors.toMap(
-                    Map.Entry::getKey,
-                    entry -> {
-                      String tagValue = entry.getValue();
-                      if (Matchers.isExact(tagValue)) {
-                        return new Matchers.ExactMatcher(tagValue);
-                      } else {
-                        Pattern pattern = GlobPattern.globToRegexPattern(tagValue);
-                        return new Matchers.PatternMatcher(pattern);
-                      }
-                    }));
+      // this method does not have to support concurrency so it is safe just to use an iterator
+      final Set<Map.Entry<String, String>> entries = tags.entrySet();
+      final Map<String, Matcher>  matchers = new HashMap<>();
+      for(final Map.Entry<String, String> entry : entries) {
+          final String tagKey = entry.getKey();
+          final String tagValue = entry.getValue();
+          if (Matchers.isExact(tagValue)) {
+              matchers.put(tagKey,new Matchers.ExactMatcher(tagValue));
+          } else {
+              Pattern pattern = GlobPattern.globToRegexPattern(tagValue);
+              matchers.put(tagKey, new Matchers.PatternMatcher(pattern));
+          }
+      }
     return new TagsMatcher(matchers);
   }
 
@@ -33,11 +34,14 @@ public class TagsMatcher {
   }
 
   public <T extends CoreSpan<T>> boolean matches(T span) {
-    return matchers.entrySet().stream()
-        .allMatch(
-            entry -> {
-              String tagValue = span.getTag(entry.getKey());
-              return tagValue != null && entry.getValue().matches(tagValue);
-            });
+    // we will do a copy just in case
+    final Set<Map.Entry<String, Matcher>> copyEntrySet = new HashSet<>(matchers.entrySet());
+    for(final Map.Entry<String, Matcher> entry : copyEntrySet) {
+        final String tagValue = span.getTag(entry.getKey());
+        if (tagValue == null || !entry.getValue().matches(tagValue)) {
+            return false;
+        }
+    }
+    return true;
   }
 }
