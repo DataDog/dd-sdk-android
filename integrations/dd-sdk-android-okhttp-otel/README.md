90e7b17d7ae4302876bcfcaa791f02be668d4229
Marius Constantin <marius.constantin@datadoghq.com>
2024-06-05 15:34:25 +0200

    RUM-3438 Introduce the dd-sdk-android-okhttp-otel module

---
+++ integrations/dd-sdk-android-okhttp-otel/README.md
@@ -0,0 +1,7 @@
+# Datadog SDK OpenTelemetry extensions for OkHttp
+
+See the dedicated [Datadog SDK instrumentation for OkHttp documentation][1] to learn how track network requests made by `OkHttp` library automatically.
+See the dedicated [Datadog SDK Android for OpenTelemetry documentation][2] to learn how to add a parent span to network requests made by `OkHttp` library.
+
+[1]: https://docs.datadoghq.com/real_user_monitoring/android/advanced_configuration/?tab=kotlin#automatically-track-network-requests
+[2]: https://docs.datadoghq.com/tracing/trace_collection/custom_instrumentation/android?tab=kotlin
---
+++ integrations/dd-sdk-android-okhttp-otel/src/main/kotlin/com/datadog/android/okhttp/otel/OkHttpExt.kt
@@ -0,0 +1,32 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2016-Present Datadog, Inc.
+ */
+
+package com.datadog.android.okhttp.otel
+
+import com.datadog.android.okhttp.TraceContext
+import com.datadog.trace.api.sampling.PrioritySampling
+import io.opentelemetry.api.trace.Span
+import io.opentelemetry.api.trace.SpanContext
+import okhttp3.Request
+
+/**
+ * Add the current span context as the parent span for the distributed trace created around the Request.
+ * @param span the parent span to add to the request.
+ * @return the modified Request.Builder instance
+ */
+fun Request.Builder.addParentSpan(span: Span): Request.Builder {
+    val context = span.spanContext
+    val prioritySampling = resolveSamplingPriority(context)
+    // the context will always be a TraceContext
+    @Suppress("UnsafeThirdPartyFunctionCall")
+    tag(TraceContext::class.java, TraceContext(context.traceId, context.spanId, prioritySampling))
+    return this
+}
+
+@Suppress("PackageNameVisibility")
+private fun resolveSamplingPriority(context: SpanContext): Int {
+    return if (context.isSampled) PrioritySampling.SAMPLER_KEEP.toInt() else PrioritySampling.UNSET.toInt()
+}
