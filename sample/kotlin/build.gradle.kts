66ccc93395f38105aa3096b1539cf45c54fd303f
Nikita Ogorodnikov <nikita.ogorodnikov@datadoghq.com>
2024-06-18 11:00:45 +0200

    Remove duplicated Proguard configuration in the sample app

--- sample/kotlin/build.gradle.kts
+++ sample/kotlin/build.gradle.kts
@@ -137,7 +137,6 @@ android {
                 "proguard-rules.pro"
             )
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
             if (e2ePassword != null) {
                 signingConfig = signingConfigs.getByName("release")
             }
