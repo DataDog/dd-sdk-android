953b791fb7f36fcf0d601cfa746229a92154d609
Marius Constantin <marius.constantin@datadoghq.com>
2024-10-17 16:09:34 +0200

    RUM-6596 Make sure the legacy integration test fail the CI job if failing

--- instrumented/integration/src/androidTest/kotlin/com/datadog/android/sdk/integration/rum/FragmentTrackingTest.kt
+++ instrumented/integration/src/androidTest/kotlin/com/datadog/android/sdk/integration/rum/FragmentTrackingTest.kt
@@ -45,7 +45,6 @@ internal abstract class FragmentTrackingTest :
         val fragmentAViewUrl = currentFragmentViewUrl(activity)
         // ignore view event for view start, it will be reduced
 
-        // view stopped
         expectedEvents.add(
             ExpectedViewEvent(
                 fragmentAViewUrl,
@@ -55,13 +54,14 @@ internal abstract class FragmentTrackingTest :
         )
 
         // swipe to change the fragment
+        instrumentation.waitForIdleSync()
         onView(ViewMatchers.withId(R.id.btn_next)).perform(ViewActions.click())
         instrumentation.waitForIdleSync()
-        Thread.sleep(200) // give time to the view id to update
         val fragmentBViewUrl = currentFragmentViewUrl(activity)
         mockServerRule.activity.supportFragmentManager.fragments
         // ignore view event for updating the time, it will be reduced
         // view stopped
+        waitForPendingRUMEvents()
         expectedEvents.add(
             ExpectedViewEvent(
                 fragmentBViewUrl,
@@ -73,8 +73,7 @@ internal abstract class FragmentTrackingTest :
         // swipe to close the view
         onView(ViewMatchers.withId(R.id.btn_last)).perform(ViewActions.click())
         instrumentation.waitForIdleSync()
-        Thread.sleep(200) // give time to the view id to update
-
+        waitForPendingRUMEvents()
         // for updating the time
         expectedEvents.add(
             ExpectedViewEvent(
@@ -83,15 +82,6 @@ internal abstract class FragmentTrackingTest :
                 currentFragmentExtras(activity)
             )
         )
-
-        // view stopped
-//        expectedEvents.add(
-//            ExpectedViewEvent(
-//                fragmentAViewUrl,
-//                3,
-//                currentFragmentExtras(activity)
-//            )
-//        )
 
         instrumentation.runOnMainSync {
             instrumentation.callActivityOnStop(mockServerRule.activity)
