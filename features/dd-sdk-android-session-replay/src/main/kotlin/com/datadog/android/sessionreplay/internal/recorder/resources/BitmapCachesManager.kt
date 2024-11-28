450f230e9dc756e119fa3b7723e4ace14fd89009
jonathanmos <48201295+jonathanmos@users.noreply.github.com>
2024-11-28 15:21:00 +0200

    RUM-6195: Expose BitmapCachesManager

--- features/dd-sdk-android-session-replay/src/main/kotlin/com/datadog/android/sessionreplay/internal/recorder/resources/BitmapCachesManager.kt
+++ features/dd-sdk-android-session-replay/src/main/kotlin/com/datadog/android/sessionreplay/internal/recorder/resources/BitmapCachesManager.kt
@@ -12,19 +12,105 @@ import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import androidx.annotation.MainThread
 import com.datadog.android.api.InternalLogger
+import com.datadog.android.lint.InternalApi
 
-internal class BitmapCachesManager(
-    private val resourcesLRUCache: Cache<Drawable, ByteArray>,
-    private val bitmapPool: BitmapPool,
+/**
+ * Manages the Bitmap caches used by the Session Replay feature.
+ */
+@InternalApi
+class BitmapCachesManager {
+
+    /**
+     * Creates a new [BitmapCachesManager] instance.
+     * @param logger the logger used to log internal logs
+     */
+    constructor(
+        logger: InternalLogger
+    ) : this(ResourcesLRUCache(), BitmapPool(), InternalLogger.UNBOUND)
+
+    internal constructor(
+        resourcesLRUCache: Cache<Drawable, ByteArray>,
+        bitmapPool: BitmapPool,
+        logger: InternalLogger
+    ) {
+        this.resourcesLRUCache = resourcesLRUCache
+        this.bitmapPool = bitmapPool
+        this.logger = logger
+    }
+
+    private val resourcesLRUCache: Cache<Drawable, ByteArray>
+    private val bitmapPool: BitmapPool
     private val logger: InternalLogger
-) {
     private var isResourcesCacheRegisteredForCallbacks: Boolean = false
     private var isBitmapPoolRegisteredForCallbacks: Boolean = false
 
+    /**
+     * Registers the caches for the [ComponentCallbacks2] callbacks.
+     * @param applicationContext the application context
+     */
     @MainThread
-    internal fun registerCallbacks(applicationContext: Context) {
+    fun registerCallbacks(applicationContext: Context) {
         registerResourceLruCacheForCallbacks(applicationContext)
         registerBitmapPoolForCallbacks(applicationContext)
+    }
+
+    /**
+     * Puts a resourceId in the resources cache.
+     * @param key the key to use to store the resourceId
+     * @param resourceId the resourceId to store
+     */
+    fun putInResourceCache(key: String, resourceId: String) {
+        resourcesLRUCache.put(key, resourceId.toByteArray(Charsets.UTF_8))
+    }
+
+    /**
+     * Gets a resourceId from the resources cache.
+     * @param key the key to use to retrieve the resourceId
+     * @return the resourceId or null if not found
+     */
+    fun getFromResourceCache(key: String): String? {
+        val resourceId = resourcesLRUCache.get(key) ?: return null
+        return String(resourceId, Charsets.UTF_8)
+    }
+
+    /**
+     * Generates a key from a drawable.
+     * @param drawable the drawable to generate the key from
+     * @return the generated key or null if unsuccessful
+     */
+    fun generateResourceKeyFromDrawable(drawable: Drawable): String? {
+        return (resourcesLRUCache as? ResourcesLRUCache)?.generateKeyFromDrawable(drawable)
+    }
+
+    /**
+     * Puts a bitmap in the bitmap pool.
+     * @param bitmap the bitmap to put in the pool
+     */
+    fun putInBitmapPool(bitmap: Bitmap) {
+        bitmapPool.put(bitmap)
+    }
+
+    /**
+     * Gets a bitmap from the bitmap pool by the properties of the bitmap.
+     * @param width the width of the bitmap
+     * @param height the height of the bitmap
+     * @param config the config of the bitmap
+     * @return the bitmap or null if not found
+     */
+    fun getBitmapByProperties(
+        width: Int,
+        height: Int,
+        config: Bitmap.Config
+    ): Bitmap? {
+        return bitmapPool.getBitmapByProperties(width, height, config)
+    }
+
+    @MainThread
+    private fun registerBitmapPoolForCallbacks(applicationContext: Context) {
+        if (isBitmapPoolRegisteredForCallbacks) return
+
+        applicationContext.registerComponentCallbacks(bitmapPool)
+        isBitmapPoolRegisteredForCallbacks = true
     }
 
     @MainThread
@@ -41,38 +127,5 @@ internal class BitmapCachesManager(
                 messageBuilder = { Cache.DOES_NOT_IMPLEMENT_COMPONENTCALLBACKS }
             )
         }
-    }
-
-    @MainThread
-    private fun registerBitmapPoolForCallbacks(applicationContext: Context) {
-        if (isBitmapPoolRegisteredForCallbacks) return
-
-        applicationContext.registerComponentCallbacks(bitmapPool)
-        isBitmapPoolRegisteredForCallbacks = true
-    }
-
-    internal fun putInResourceCache(key: String, resourceId: String) {
-        resourcesLRUCache.put(key, resourceId.toByteArray(Charsets.UTF_8))
-    }
-
-    internal fun getFromResourceCache(key: String): String? {
-        val resourceId = resourcesLRUCache.get(key) ?: return null
-        return String(resourceId, Charsets.UTF_8)
-    }
-
-    internal fun generateResourceKeyFromDrawable(drawable: Drawable): String? {
-        return (resourcesLRUCache as? ResourcesLRUCache)?.generateKeyFromDrawable(drawable)
-    }
-
-    internal fun putInBitmapPool(bitmap: Bitmap) {
-        bitmapPool.put(bitmap)
-    }
-
-    internal fun getBitmapByProperties(
-        width: Int,
-        height: Int,
-        config: Bitmap.Config
-    ): Bitmap? {
-        return bitmapPool.getBitmapByProperties(width, height, config)
     }
 }
