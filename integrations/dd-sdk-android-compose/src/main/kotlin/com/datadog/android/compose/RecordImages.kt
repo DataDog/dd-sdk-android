e4252d9aea53483e765d11131d181a854d3167b0
luyi <yi.lu@datadoghq.com>
2025-03-24 17:40:31 +0000

    Add Compose instrumentation API for view tracking and image recording

---
+++ integrations/dd-sdk-android-compose/src/main/kotlin/com/datadog/android/compose/RecordImages.kt
@@ -0,0 +1,12 @@
+package com.datadog.android.compose
+
+/**
+ * Indicates that the annotated composable function should be instrumented for
+ * Session Replay.
+ *
+ * When this annotation is applied, the Datadog compiler plugin will automatically
+ * apply the [datadog] modifier to all components within the function, ensuring they
+ * are included in the semantics tree and can be recorded appropriately.
+ */
+@Retention(AnnotationRetention.SOURCE)
+annotation class RecordImages
