b689926468c7dd75819451e2e8655565bb4698df
Timur Valeev <timur.valeev@datadoghq.com>
2025-04-07 17:31:05 +0100

    Innovation week: InsightsCollector support

--- sample/kotlin/build.gradle.kts
+++ sample/kotlin/build.gradle.kts
@@ -148,6 +148,7 @@ dependencies {
     // Datadog Libraries
     implementation(project(":features:dd-sdk-android-logs"))
     implementation(project(":features:dd-sdk-android-rum"))
+    implementation(project(":features:dd-sdk-instant-insights"))
     implementation(project(":features:dd-sdk-android-trace"))
     implementation(project(":features:dd-sdk-android-trace-otel"))
     implementation(project(":features:dd-sdk-android-ndk"))
