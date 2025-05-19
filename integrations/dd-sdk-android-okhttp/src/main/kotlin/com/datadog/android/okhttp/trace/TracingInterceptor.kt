80b7fa387a09186f3d34e962305ddeac82cd33e1
Timur Valeev <timur.valeev@datadoghq.com>
2025-05-19 17:12:24 +0100

    RUM-9899: Removing tracer object dependency from sample decision methods flow

--- integrations/dd-sdk-android-okhttp/src/main/kotlin/com/datadog/android/okhttp/trace/TracingInterceptor.kt
+++ integrations/dd-sdk-android-okhttp/src/main/kotlin/com/datadog/android/okhttp/trace/TracingInterceptor.kt
@@ -215,7 +215,7 @@ internal constructor(
         tracer: Tracer
     ): Response {
         val span: Span = buildSpan(tracer, request)
-        val isSampled = span.sample(tracer, request)
+        val isSampled = span.sample(request)
 
         if (span is DDSpan && span.isRootSpan) {
             val samplingPriority = if (isSampled) PrioritySampling.SAMPLER_KEEP else PrioritySampling.SAMPLER_DROP
@@ -324,7 +324,7 @@ internal constructor(
     }
 
     @Suppress("ReturnCount")
-    private fun extractSamplingDecision(tracer: Tracer, request: Request): Boolean? {
+    private fun extractSamplingDecision(request: Request): Boolean? {
         val headerSamplingPriority = extractSamplingDecisionFromHeader(request)
         if (headerSamplingPriority != null) return headerSamplingPriority
 
@@ -642,12 +642,12 @@ internal constructor(
 
     private fun Span.drop() = (this as? MutableSpan)?.drop()
 
-    private fun Span.sample(tracer: Tracer, request: Request): Boolean {
+    private fun Span.sample(request: Request): Boolean {
         val samplingPriority = (this as? DDSpan)?.samplingPriority
         return if (samplingPriority != null) {
             samplingPriority > 0
         } else {
-            extractSamplingDecision(tracer, request) ?: traceSampler.sample(this)
+            extractSamplingDecision(request) ?: traceSampler.sample(this)
         }
     }
 
