6b4d78b34412ce7643bf75e1b665671b4ffecd6b
Timur Valeev <timur.valeev@datadoghq.com>
2025-05-23 17:46:42 +0100

    RUM-9899: refactor TraceContext.kt

--- integrations/dd-sdk-android-okhttp/src/main/kotlin/com/datadog/android/okhttp/TraceContext.kt
+++ integrations/dd-sdk-android-okhttp/src/main/kotlin/com/datadog/android/okhttp/TraceContext.kt
@@ -8,9 +8,6 @@ package com.datadog.android.okhttp
 
 import com.datadog.android.lint.InternalApi
 import com.datadog.trace.api.DDTraceId
-import com.datadog.trace.bootstrap.instrumentation.api.AgentSpan
-import com.datadog.trace.bootstrap.instrumentation.api.AgentTrace
-import com.datadog.trace.bootstrap.instrumentation.api.AgentTracer
 
 /**
  * The context of a trace to be propagated through the OkHttp requests for Datadog tracing.
@@ -20,26 +17,13 @@ data class TraceContext(
     /**
      * The trace id.
      */
-    private val traceId: DDTraceId,
+    val traceId: DDTraceId,
     /**
      * The span id.
      */
-    private val spanId: Long,
+    val spanId: Long,
     /**
      * The sampling priority.
      */
-    private val samplingPriority: Int
-) : AgentSpan.Context {
-
-    override fun getTraceId(): DDTraceId = traceId
-
-    override fun getSpanId(): Long = spanId
-
-    override fun getTrace(): AgentTrace = AgentTracer.NoopAgentTrace.INSTANCE
-
-    override fun getSamplingPriority() = samplingPriority
-
-    override fun baggageItems() = null
-
-    override fun getPathwayContext() = null
-}
+    val samplingPriority: Int
+)
