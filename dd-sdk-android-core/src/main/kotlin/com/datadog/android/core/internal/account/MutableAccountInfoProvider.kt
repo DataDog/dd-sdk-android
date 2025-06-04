4dfa69d54641caa3ac8eb2f7e0429f9950f45ab5
luyi <yi.lu@datadoghq.com>
2025-06-04 10:13:52 +0200

    RUM-10242: Add AccountInfo data class and provider

---
+++ dd-sdk-android-core/src/main/kotlin/com/datadog/android/core/internal/account/MutableAccountInfoProvider.kt
@@ -0,0 +1,23 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2016-Present Datadog, Inc.
+ */
+
+package com.datadog.android.core.internal.account
+
+import com.datadog.tools.annotation.NoOpImplementation
+
+@NoOpImplementation
+internal interface MutableAccountInfoProvider : AccountInfoProvider {
+
+    fun setAccountInfo(
+        id: String?,
+        name: String?,
+        extraInfo: Map<String, Any?>
+    )
+
+    fun addExtraInfo(extraInfo: Map<String, Any?>)
+
+    fun clearAccountInfo()
+}
