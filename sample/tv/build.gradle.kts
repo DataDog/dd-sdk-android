0b008be49c363145921eb6112715f68cd1353871
Xavier F. Gouchet <github@xgouchet.fr>
2025-06-04 13:31:33 +0200

    Create sample tv module

---
+++ sample/tv/build.gradle.kts
@@ -0,0 +1,118 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2016-Present Datadog, Inc.
+ */
+
+import com.datadog.gradle.config.AndroidConfig
+import com.datadog.gradle.config.configureFlavorForTvApp
+import com.datadog.gradle.config.dependencyUpdateConfig
+import com.datadog.gradle.config.java17
+import com.datadog.gradle.config.javadocConfig
+import com.datadog.gradle.config.junitConfig
+import com.datadog.gradle.config.kotlinConfig
+import com.datadog.gradle.config.taskConfig
+
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    id("com.github.ben-manes.versions")
+    alias(libs.plugins.datadogGradlePlugin)
+}
+
+android {
+    namespace = "com.datadog.android.tv.sample"
+    compileSdk = AndroidConfig.TARGET_SDK
+    buildToolsVersion = AndroidConfig.BUILD_TOOLS_VERSION
+
+    defaultConfig {
+        minSdk = AndroidConfig.MIN_SDK_FOR_WEAR_AND_TV
+        targetSdk = AndroidConfig.TARGET_SDK
+        versionCode = AndroidConfig.VERSION.code
+        versionName = AndroidConfig.VERSION.name
+        multiDexEnabled = true
+
+        vectorDrawables.useSupportLibrary = true
+
+        configureFlavorForTvApp(project.rootDir)
+    }
+
+    compileOptions {
+        java17()
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+
+    sourceSets.named("main") {
+        java.srcDir("src/main/kotlin")
+    }
+    sourceSets.named("test") {
+        java.srcDir("src/test/kotlin")
+    }
+    sourceSets.named("androidTest") {
+        java.srcDir("src/androidTest/kotlin")
+    }
+}
+
+repositories {
+    maven { setUrl("https://jitpack.io") }
+}
+
+dependencies {
+
+    implementation(project(":dd-sdk-android-core"))
+    implementation(project(":features:dd-sdk-android-rum"))
+    implementation(project(":features:dd-sdk-android-logs"))
+    implementation(project(":features:dd-sdk-android-session-replay"))
+    implementation(project(":features:dd-sdk-android-session-replay-material"))
+    implementation(project(":integrations:dd-sdk-android-okhttp"))
+    implementation(project(":integrations:dd-sdk-android-timber"))
+    implementation(project(":integrations:dd-sdk-android-tv"))
+
+    implementation(libs.kotlin)
+
+    // Android dependencies
+    implementation(libs.androidXCore)
+    implementation(libs.androidXCoreKtx)
+    implementation(libs.androidXAppCompat)
+    implementation(libs.googleMaterial)
+    implementation(libs.androidXRecyclerView)
+    implementation(libs.androidXConstraintLayout)
+    implementation(libs.androidXLifecycleLiveDataKtx)
+    implementation(libs.androidXLifecycleViewModelKtx)
+    implementation(libs.bundles.androidXNavigation)
+    implementation(libs.androidXNavigationUIKtx)
+
+    // Network
+    implementation(libs.okHttp)
+    implementation(libs.gson)
+
+    // Misc
+    implementation(libs.timber)
+
+    // Video
+    implementation(libs.bundles.exoplayer)
+    implementation(libs.newPipeExtractor)
+}
+
+kotlinConfig(evaluateWarningsAsErrors = false)
+taskConfig<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    compilerOptions {
+        freeCompilerArgs.add("-opt-in=kotlin.RequiresOptIn")
+    }
+}
+junitConfig()
+dependencyUpdateConfig()
