9dfb846c34330cc673128b6511a6ebe8b5c0dd5d
Maciej Burda <maciej.burda@datadoghq.com>
2025-07-01 10:32:37 +0100

    Update dd-sdk-android-core/src/main/kotlin/com/datadog/android/Datadog.kt
    Co-authored-by: Jonathan Moskovich <48201295+jonathanmos@users.noreply.github.com>

--- dd-sdk-android-core/src/main/kotlin/com/datadog/android/Datadog.kt
+++ dd-sdk-android-core/src/main/kotlin/com/datadog/android/Datadog.kt
@@ -309,7 +309,7 @@ object Datadog {
      * Clear the current user information.
      *
      * User information will be set to null.
-     * Following Logs, Traces, RUM Events will not include the user information anymore.
+     * After calling this api, Logs, Traces, RUM Events will not include the user information anymore.
      *
      * Any active RUM Session, active RUM View at the time of call will have their `usr` attribute cleared.
      *
