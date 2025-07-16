cb5cf60ae8ec6d046b9f143df12023398b987cfe
Timur Valeev <timur.valeev@datadoghq.com>
2025-07-16 16:49:19 +0100

    RUM-9902 - post review fixes

--- reliability/single-fit/trace/src/test/kotlin/com/datadog/android/trace/integration/otel/OtelTraceConfigurationTest.kt
+++ reliability/single-fit/trace/src/test/kotlin/com/datadog/android/trace/integration/otel/OtelTraceConfigurationTest.kt
@@ -25,7 +25,6 @@ import fr.xgouchet.elmyr.junit5.ForgeExtension
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.RepeatedTest
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.jupiter.api.extension.Extensions
 import org.mockito.junit.jupiter.MockitoExtension
