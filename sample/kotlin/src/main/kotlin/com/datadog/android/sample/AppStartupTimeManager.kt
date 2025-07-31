69f25e7514e65d706f31ea4ea45ee238a9c2d39b
Aleksandr Gringauz <aleksandr.gringauz@datadoghq.com>
2025-07-31 14:23:50 +0200

    save

--- sample/kotlin/src/main/kotlin/com/datadog/android/sample/AppStartupTimeManager.kt
+++ sample/kotlin/src/main/kotlin/com/datadog/android/sample/AppStartupTimeManager.kt
@@ -13,6 +13,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import android.view.ViewTreeObserver
 import com.datadog.android.api.SdkCore
 import com.datadog.android.core.InternalSdkCore
 import io.opentelemetry.api.trace.Span
@@ -30,11 +31,16 @@ class AppStartupTypeManager(
     private val context: Context,
     private val tracer: Tracer,
     private val sdkCore: SdkCore,
-): Application.ActivityLifecycleCallbacks {
+): Application.ActivityLifecycleCallbacks, ViewTreeObserver.OnDrawListener {
 
     private val epoch = Instant.now()
 
     private val handler = Handler(Looper.getMainLooper())
+
+    private var firstActivityDrawn: Boolean = false
+
+    private var fullDisplayCalled: Boolean = false
+    private var fullDisplayInstant: Instant = Instant.MIN
 
     private val processStartTimeNs: Long by lazy {
         (sdkCore as InternalSdkCore).appStartTimeNs
@@ -48,14 +54,51 @@ class AppStartupTypeManager(
 
     private var activityCreatedStartTime: Instant = Instant.MIN
     private var activityStartedStartTime: Instant = Instant.MIN
+    private var activityResumeStartTime: Instant = Instant.MIN
 
     init {
         (context.applicationContext as Application).registerActivityLifecycleCallbacks(this)
     }
 
+    fun reportFullyDisplayed() {
+        if (!fullDisplayCalled) {
+            fullDisplayCalled = true
+
+            val span = tracer.spanBuilder("ttfd")
+                .setParent(io.opentelemetry.context.Context.current().with(appStartupSpan))
+                .setStartTimestamp(epoch)
+                .startSpan()
+
+            val now = relativeNow()
+
+            span.end(now)
+        }
+    }
+
     override fun onActivityPreCreated(activity: Activity, savedInstanceState: Bundle?) {
+        if (!firstActivityDrawn) {
+            activity.window.decorView.viewTreeObserver.addOnDrawListener(this)
+            activityCreatedStartTime = relativeNow()
+        }
+
         Log.w("WAHAHA", "${activity::class.java.name} onActivityPreCreated")
-        activityCreatedStartTime = relativeNow()
+    }
+
+    override fun onActivityPreResumed(activity: Activity) {
+        activityResumeStartTime = relativeNow()
+    }
+
+    override fun onActivityPostResumed(activity: Activity) {
+        if (!firstActivityDrawn) {
+            val span = tracer.spanBuilder("Activity.onResume")
+                .setParent(io.opentelemetry.context.Context.current().with(appStartupSpan))
+                .setStartTimestamp(activityResumeStartTime)
+                .startSpan()
+
+            val now = relativeNow()
+            Log.w("WAHAHA", "app_startup: $now, process_start: $processStartTimeNs")
+            span.end(now)
+        }
     }
 
     override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
@@ -64,12 +107,15 @@ class AppStartupTypeManager(
 
     override fun onActivityPostCreated(activity: Activity, savedInstanceState: Bundle?) {
         Log.w("WAHAHA", "${activity::class.java.name} onActivityPostCreated")
-        val span = tracer.spanBuilder("Activity.onCreate")
-            .setParent(io.opentelemetry.context.Context.current().with(appStartupSpan))
-            .setStartTimestamp(activityCreatedStartTime)
-            .startSpan()
+        if (!firstActivityDrawn) {
+            val span = tracer.spanBuilder("Activity.onCreate")
+                .setParent(io.opentelemetry.context.Context.current().with(appStartupSpan))
+                .setStartTimestamp(activityCreatedStartTime)
+                .startSpan()
 
-        span.end(relativeNow())
+            span.end(relativeNow())
+        }
+
     }
 
     override fun onActivityDestroyed(activity: Activity) {
@@ -99,16 +145,17 @@ class AppStartupTypeManager(
 
     override fun onActivityPostStarted(activity: Activity) {
         Log.w("WAHAHA", "${activity::class.java.name} onActivityPostStarted")
-        val span = tracer.spanBuilder("Activity.onStart")
-            .setParent(io.opentelemetry.context.Context.current().with(appStartupSpan))
-            .setStartTimestamp(activityStartedStartTime)
-            .startSpan()
 
-        val now = relativeNow()
-        Log.w("WAHAHA", "app_startup: $now, process_start: $processStartTimeNs")
-        span.end(now)
-        handler.postDelayed({appStartupSpan.end(relativeNow())}, 5000)
+        if (!firstActivityDrawn) {
+            val span = tracer.spanBuilder("Activity.onStart")
+                .setParent(io.opentelemetry.context.Context.current().with(appStartupSpan))
+                .setStartTimestamp(activityStartedStartTime)
+                .startSpan()
 
+            val now = relativeNow()
+            Log.w("WAHAHA", "app_startup: $now, process_start: $processStartTimeNs")
+            span.end(now)
+        }
     }
 
     override fun onActivityStopped(activity: Activity) {
@@ -121,5 +168,25 @@ class AppStartupTypeManager(
 
     private fun addStartupTypeAttribute(type: AppStartupType) {
         appStartupSpan.setAttribute("app_startup_type", type.name)
+    }
+
+    override fun onDraw() {
+        if (!firstActivityDrawn) {
+            firstActivityDrawn = true
+
+            val span = tracer.spanBuilder("ttid")
+                .setParent(io.opentelemetry.context.Context.current().with(appStartupSpan))
+                .setStartTimestamp(epoch)
+                .startSpan()
+
+            val now = relativeNow()
+
+            span.end(now)
+
+            handler.postDelayed({
+                reportFullyDisplayed()
+                appStartupSpan.end(relativeNow())
+            }, 2000)
+        }
     }
 }
