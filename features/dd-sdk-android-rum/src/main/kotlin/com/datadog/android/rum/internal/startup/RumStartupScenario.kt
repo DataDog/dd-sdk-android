37a879d615ad342ac7142fb90ae40fd8a5fece25
Aleksandr Gringauz <aleksandr.gringauz@datadoghq.com>
2025-09-08 13:32:13 +0200

    RUM-11555: Implement AppStartupTypeDetector

---
+++ features/dd-sdk-android-rum/src/main/kotlin/com/datadog/android/rum/internal/startup/RumStartupScenario.kt
@@ -0,0 +1,33 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2016-Present Datadog, Inc.
+ */
+
+package com.datadog.android.rum.internal.startup
+
+import android.app.Activity
+
+internal sealed interface RumStartupScenario {
+    val initialTimeNanos: Long
+    val hasSavedInstanceStateBundle: Boolean
+    val activity: Activity
+
+    data class Cold(
+        override val initialTimeNanos: Long,
+        override val hasSavedInstanceStateBundle: Boolean,
+        override val activity: Activity
+    ) : RumStartupScenario
+
+    data class WarmFirstActivity(
+        override val initialTimeNanos: Long,
+        override val hasSavedInstanceStateBundle: Boolean,
+        override val activity: Activity
+    ) : RumStartupScenario
+
+    data class WarmAfterActivityDestroyed(
+        override val initialTimeNanos: Long,
+        override val hasSavedInstanceStateBundle: Boolean,
+        override val activity: Activity
+    ) : RumStartupScenario
+}
