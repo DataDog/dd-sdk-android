7d44c5843dbaf384d6086594aac13f6123509a9d
Aleksandr Gringauz <aleksandr.gringauz@datadoghq.com>
2025-09-12 15:01:50 +0200

    better

--- dd-sdk-android-internal/src/test/java/com/datadog/internal/utils/WindowCallbackUtilsTest.kt
+++ dd-sdk-android-internal/src/test/java/com/datadog/internal/utils/WindowCallbackUtilsTest.kt
@@ -8,34 +8,44 @@ package com.datadog.internal.utils
 
 import android.view.Window
 import com.datadog.android.internal.utils.onContentChanged
-import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.RepeatedTest
 import org.junit.jupiter.api.Test
+import org.mockito.Mock
 import org.mockito.Mockito.mock
 import org.mockito.junit.jupiter.MockitoSettings
 import org.mockito.kotlin.any
 import org.mockito.kotlin.doAnswer
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.inOrder
-import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 import org.mockito.quality.Strictness
 
 @MockitoSettings(strictness = Strictness.LENIENT)
 class WindowCallbackUtilsTest {
-    @Test
-    fun `test one`() {
-        val window = mock<Window>()
 
-        val existingCallback = mock<Window.Callback>()
-        val onContentChanged = mock<() -> Boolean>()
+    @Mock
+    private lateinit var window: Window
 
-        var callback: Window.Callback? = existingCallback
+    @Mock
+    private lateinit var existingCallback: Window.Callback
+
+    private var callback: Window.Callback? = null
+
+    @BeforeEach
+    fun `set up`() {
+        callback = existingCallback
 
         whenever(window.callback) doReturn callback
         whenever(window.setCallback(any())).doAnswer {
             val arg = it.getArgument<Window.Callback>(0)
             callback = arg
         }
+    }
+
+    @RepeatedTest(16)
+    fun `M notify both callbacks W onContentChanged`() {
+        val onContentChanged = mock<() -> Boolean>()
 
         whenever(onContentChanged.invoke()) doReturn true
 
