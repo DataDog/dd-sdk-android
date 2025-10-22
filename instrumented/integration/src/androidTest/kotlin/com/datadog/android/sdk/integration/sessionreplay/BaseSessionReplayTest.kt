dbb7aed480ac1580b5e1f697958a6fe6d939cb53
jonathanmos <48201295+jonathanmos@users.noreply.github.com>
2025-10-22 17:39:50 +0300

    Cleanup json testing remnants

--- instrumented/integration/src/androidTest/kotlin/com/datadog/android/sdk/integration/sessionreplay/BaseSessionReplayTest.kt
+++ instrumented/integration/src/androidTest/kotlin/com/datadog/android/sdk/integration/sessionreplay/BaseSessionReplayTest.kt
@@ -12,24 +12,17 @@ import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.platform.app.InstrumentationRegistry
 import com.datadog.android.Datadog
 import com.datadog.android.rum.GlobalRumMonitor
-import com.datadog.android.sdk.integration.RuntimeConfig
 import com.datadog.android.sdk.rules.HandledRequest
-import com.datadog.android.sdk.rules.SessionReplayTestRule
 import com.datadog.android.sdk.utils.waitFor
 import com.datadog.tools.unit.ConditionWatcher
-import com.google.gson.GsonBuilder
 import com.google.gson.JsonArray
 import com.google.gson.JsonElement
 import com.google.gson.JsonObject
 import com.google.gson.JsonParser
-import com.google.gson.JsonPrimitive
-import com.google.gson.internal.LazilyParsedNumber
 import okio.Buffer
 import org.assertj.core.api.Assertions.assertThat
-import org.assertj.core.api.recursive.comparison.RecursiveComparisonConfiguration
 import org.junit.After
 import java.io.ByteArrayOutputStream
-import java.io.File
 import java.util.Locale
 import java.util.concurrent.TimeUnit
 import java.util.zip.Inflater
@@ -81,32 +74,6 @@ internal abstract class BaseSessionReplayTest<R : Activity> {
             .isNotNull
     }
 
-    @Deprecated(
-        message = "Use semantic assertions instead of JSON comparison",
-        replaceWith = ReplaceWith("extractRecordsFromRequests(requests)")
-    )
-    protected fun assessSrPayload(payloadFileName: String, rule: SessionReplayTestRule<R>) {
-        if (isPayloadUpdateRequest()) {
-            ConditionWatcher {
-                updateExpectedDataInPayload(
-                    rule.getRequests(RuntimeConfig.sessionReplayEndpointUrl),
-                    payloadFileName
-                )
-                true
-            }.doWait(timeoutMs = INITIAL_WAIT_MS)
-        } else {
-            ConditionWatcher {
-                // verify the captured log events into the MockedWebServer
-                verifyExpectedSrData(
-                    rule.getRequests(RuntimeConfig.sessionReplayEndpointUrl),
-                    payloadFileName,
-                    MatchingStrategy.CONTAINS
-                )
-                true
-            }.doWait(timeoutMs = INITIAL_WAIT_MS)
-        }
-    }
-
     protected fun runInstrumentationScenario() {
         val instrumentation = InstrumentationRegistry.getInstrumentation()
         instrumentation.waitForIdleSync()
@@ -114,88 +81,6 @@ internal abstract class BaseSessionReplayTest<R : Activity> {
         // SurfaceFlinger to call the onDraw method which will trigger the screen snapshot.
         Espresso.onView(ViewMatchers.isRoot()).perform(waitFor(UI_THREAD_DELAY_IN_MS))
         instrumentation.waitForIdleSync()
-    }
-
-    private fun verifyExpectedSrData(
-        handledRequests: List<HandledRequest>,
-        expectedPayloadFileName: String,
-        matchingStrategy: MatchingStrategy = MatchingStrategy.EXACT
-    ) {
-        val records = handledRequests
-            .mapNotNull { it.extractSrSegmentAsJson()?.asJsonObject }
-            .flatMap { it.getAsJsonArray("records") }
-            .map { it.dropInconsistentProperties() }
-        val expectedPayload = resolveTestExpectedPayload(expectedPayloadFileName)
-            .asJsonArray
-            .map { it.dropInconsistentProperties() }
-        val assertion = assertThat(records)
-            .usingRecursiveFieldByFieldElementComparator(
-                RecursiveComparisonConfiguration
-                    .builder()
-                    .withComparatorForType(
-                        jsonPrimitivesComparator,
-                        JsonPrimitive::class.java
-                    ).build()
-            )
-        when (matchingStrategy) {
-            MatchingStrategy.EXACT -> assertion.containsExactlyElementsOf(expectedPayload)
-            MatchingStrategy.CONTAINS -> assertion.containsAll(expectedPayload)
-        }
-    }
-
-    private fun updateExpectedDataInPayload(
-        handledRequests: List<HandledRequest>,
-        expectedPayloadFileName: String
-    ) {
-        val records = handledRequests
-            .mapNotNull { it.extractSrSegmentAsJson()?.asJsonObject }
-            .flatMap { it.getAsJsonArray("records") }
-            // we are dropping the incremental records as they are producing noise and
-            // flakiness. In the end we are only interested in full snapshot records.
-            .filter {
-                it.asJsonObject.get("type").asString != "11"
-            }
-            .map { it.dropInconsistentProperties() }
-        // we make sure the payload is valid before updating it
-        validatePayload(records)
-        val payloadUpdateFile = resolvePayloadUpdateFile(expectedPayloadFileName)
-        if (!payloadUpdateFile.exists()) {
-            payloadUpdateFile.createNewFile()
-        }
-        // we only take the first 3 records which will contain the:
-        // 1. Meta Record
-        // 2. Focus Record
-        // 3. Full Snapshot Record
-        val payloadUpdateData = records.take(3).toPrettyString()
-        payloadUpdateFile.writeText(payloadUpdateData)
-    }
-
-    private fun validatePayload(records: List<JsonObject>) {
-        // the payload should contain at least 3 record
-        // first record should always be a MetaRecord
-        // second record should always be the FocusRecord
-        assertThat(records.size).isGreaterThan(2)
-        assertThat(records[0].asJsonObject.get("type").asString).isEqualTo("4")
-        assertThat(records[1].asJsonObject.get("type").asString).isEqualTo("6")
-    }
-
-    private fun isPayloadUpdateRequest(): Boolean {
-        return InstrumentationRegistry.getArguments().getString(PAYLOAD_UPDATE_REQUEST)
-            ?.toBoolean() ?: false
-    }
-
-    private fun resolveTestExpectedPayload(fileName: String): JsonElement {
-        return InstrumentationRegistry
-            .getInstrumentation()
-            .context
-            .assets
-            .open("$PAYLOADS_FOLDER/$fileName").use {
-                JsonParser.parseString(it.readBytes().toString(Charsets.UTF_8))
-            }
-    }
-
-    private fun resolvePayloadUpdateFile(payloadFileName: String): File {
-        return File(resolvePayloadsDirectory(), payloadFileName)
     }
 
     private fun HandledRequest.extractSrSegmentAsJson(): JsonElement? {
@@ -250,104 +135,12 @@ internal abstract class BaseSessionReplayTest<R : Activity> {
         return bos.toByteArray()
     }
 
-    private val jsonPrimitivesComparator: (o1: JsonPrimitive, o2: JsonPrimitive) -> Int =
-        { o1, o2 ->
-            if (comparingFloatAndLazilyParsedNumber(o1, o2)) {
-                // when comparing a float with a LazilyParsedNumber the `JsonPrimitive#equals`
-                // method uses Double.parseValue(value) to convert the value from the
-                // LazilyParsedNumber and this method uses an extra precision. This will
-                // create assertion issues because even though the original values
-                // are the same the parsed values are no longer matching.
-                if (o1.asString.toDouble() == o2.asString.toDouble()) {
-                    0
-                } else {
-                    -1
-                }
-            } else {
-                if (o1 == o2) {
-                    0
-                } else {
-                    -1
-                }
-            }
-        }
-
-    private fun comparingFloatAndLazilyParsedNumber(o1: JsonPrimitive, o2: JsonPrimitive): Boolean {
-        return (o1.isNumber && o2.isNumber) &&
-            (o1.asNumber is Float || o2.asNumber is Float) &&
-            (o1.asNumber is LazilyParsedNumber || o2.asNumber is LazilyParsedNumber)
-    }
-
-    private fun JsonElement.dropInconsistentProperties(): JsonObject {
-        // We need to remove all the inconsistent properties from the payload as they will alter
-        // the tests. The timestamps and ids are auto - generated and we could not predict them.
-        // For the wireframes x,y and positions we need to remove them because the tests
-        // will be executed in Bitrise and currently Bitrise does not own a specific model for the
-        // API 33. They only have a standard emulator for this API with the required screen size and
-        // X,Y positions are different from the ones we have in our local emulator.
-        // Also the base64 and resourceId encoded images values are inconsistent from one run to another so will
-        // be removed from the payload.
-
-        return this.asJsonObject.apply {
-            remove("timestamp")
-            get("data")?.asJsonObject?.let { dataObject ->
-                dataObject.get("wireframes")?.asJsonArray
-                    ?.mapNotNull { wireframe ->
-                        val wireframeJson = wireframe.asJsonObject
-                        wireframeJson.remove("id")
-                        wireframeJson.remove("x")
-                        wireframeJson.remove("y")
-                        wireframeJson.remove("base64")
-                        wireframeJson.remove("resourceId")
-                        wireframeJson.remove("width")
-                        wireframeJson.remove("height")
-                        wireframeJson.remove("shapeStyle")
-                        wireframeJson
-                    }?.fold(JsonArray()) { acc, jsonObject ->
-                        acc.add(jsonObject)
-                        acc
-                    }?.let { sanitizedWireframes ->
-                        dataObject.add("wireframes", sanitizedWireframes)
-                    }
-            }
-        }
-    }
-
-    private fun List<JsonObject>.toPrettyString(): String {
-        val gson = GsonBuilder().setPrettyPrinting().create()
-        return this.map { gson.toJson(it) }.toString()
-    }
-
-    @Suppress("UseCheckOrError")
-    private fun resolvePayloadsDirectory(): File {
-        return InstrumentationRegistry.getInstrumentation().targetContext.externalCacheDir?.let {
-            val payloadsDir = File(it, PAYLOADS_FOLDER).apply {
-                if (!this.exists()) {
-                    this.mkdirs()
-                }
-            }
-            return payloadsDir
-        } ?: throw IllegalStateException("Could not resolve the payloads directory")
-    }
-
-    /**
-     * The matching strategy to use when comparing the expected payload with the actual one.
-     * @see EXACT will compare the payloads exactly as they are.
-     * @see CONTAINS will check if the actual payload contains all the expected elements.
-     */
-    enum class MatchingStrategy {
-        EXACT,
-        CONTAINS
-    }
-
     companion object {
         internal val INITIAL_WAIT_MS = TimeUnit.SECONDS.toMillis(30)
         private const val UI_THREAD_DELAY_IN_MS = 1000L
-        private const val PAYLOAD_UPDATE_REQUEST = "updateSrPayloads"
         private val SEGMENT_FORM_DATA_REGEX =
             Regex("content-disposition: form-data; name=\"segment\"; filename=\"(.+)\"")
         private val CONTENT_LENGTH_REGEX =
             Regex("content-length: (\\d+)")
-        private const val PAYLOADS_FOLDER = "session_replay_payloads"
     }
 }
