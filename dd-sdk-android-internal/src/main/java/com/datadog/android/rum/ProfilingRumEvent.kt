d362b0f3790678df8439fbe36dcfafd04bc8a277
luyi <yi.lu@datadoghq.com>
2025-10-23 14:59:06 +0200

    [Dogfood] Add RumProfilingEvent

---
+++ dd-sdk-android-internal/src/main/java/com/datadog/android/rum/ProfilingRumEvent.kt
@@ -0,0 +1,25 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2016-Present Datadog, Inc.
+ */
+
+package com.datadog.android.rum
+
+/**
+ * Class representing a profiling event. (Only for Dogfooding purpose)
+ *
+ * @param success True if the profiling was successful, false otherwise
+ * @param errorCode The error code of the profiling
+ * @param errorMessage The error message of the profiling
+ * @param duration The duration of the profiling
+ * @param fileSize The file size of the profiling
+ */
+// TODO RUM-11043: Update or remove this for proper RUM profiling event.
+data class ProfilingRumEvent(
+    val success: Boolean,
+    val errorCode: Int,
+    val errorMessage: String?,
+    val duration: Long,
+    val fileSize: Long
+)
