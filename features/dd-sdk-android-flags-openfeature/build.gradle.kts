06861c08a54f30a02a03ce3c937f398f40257edc
Tyler Potter <tyler.potter@datadoghq.com>
2025-11-09 23:39:40 -0700

    scaffold openfeature provider module

---
+++ features/dd-sdk-android-flags-openfeature/build.gradle.kts
@@ -0,0 +1,89 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2016-Present Datadog, Inc.
+ */
+
+import com.datadog.gradle.config.androidLibraryConfig
+import com.datadog.gradle.config.dependencyUpdateConfig
+import com.datadog.gradle.config.detektCustomConfig
+import com.datadog.gradle.config.javadocConfig
+import com.datadog.gradle.config.junitConfig
+import com.datadog.gradle.config.kotlinConfig
+import com.datadog.gradle.config.publishingConfig
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    // Build
+    id("com.android.library")
+    kotlin("android")
+    id("com.google.devtools.ksp")
+
+    // Publish
+    `maven-publish`
+    signing
+    id("org.jetbrains.dokka-javadoc")
+
+    // Analysis tools
+    id("com.github.ben-manes.versions")
+
+    // Tests
+    id("de.mobilej.unmock")
+    id("org.jetbrains.kotlinx.kover")
+
+    // Internal Generation
+    id("com.datadoghq.dependency-license")
+    id("apiSurface")
+    id("transitiveDependencies")
+    id("verificationXml")
+    id("binary-compatibility-validator")
+}
+
+android {
+    namespace = "com.datadog.android.flags.openfeature"
+}
+
+dependencies {
+    // datadog
+    api(project(":dd-sdk-android-core"))
+    api(project(":features:dd-sdk-android-flags"))
+    implementation(project(":dd-sdk-android-internal"))
+
+    // OpenFeature SDK
+    api("dev.openfeature:kotlin-sdk:0.6.2")
+
+    implementation(libs.kotlin)
+    implementation(libs.androidXAnnotation)
+
+    // Generate NoOp implementations
+    ksp(project(":tools:noopfactory"))
+
+    testImplementation(project(":tools:unit")) {
+        attributes {
+            attribute(
+                com.android.build.api.attributes.ProductFlavorAttr.of("platform"),
+                objects.named("jvm")
+            )
+        }
+    }
+    testImplementation(testFixtures(project(":dd-sdk-android-core")))
+    testImplementation(testFixtures(project(":features:dd-sdk-android-rum")))
+    testImplementation(libs.bundles.jUnit5)
+    testImplementation(libs.bundles.testTools)
+    unmock(libs.robolectric)
+}
+
+unMock {
+    keepStartingWith("org.json")
+}
+
+kotlinConfig(jvmBytecodeTarget = JvmTarget.JVM_11)
+androidLibraryConfig()
+junitConfig()
+javadocConfig()
+dependencyUpdateConfig()
+publishingConfig(
+    "OpenFeature Provider integration for the Datadog Feature Flags " +
+        "library for Android applications."
+)
+detektCustomConfig()
