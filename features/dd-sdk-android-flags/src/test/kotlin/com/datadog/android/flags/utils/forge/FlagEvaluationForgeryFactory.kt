b875959bdb381fa713e71a99d17d67d322aa2491
Tyler Potter <tyler.potter@datadoghq.com>
2026-01-27 13:12:00 -0700

    clean up tests

--- features/dd-sdk-android-flags/src/test/kotlin/com/datadog/android/flags/utils/forge/FlagEvaluationForgeryFactory.kt
+++ features/dd-sdk-android-flags/src/test/kotlin/com/datadog/android/flags/utils/forge/FlagEvaluationForgeryFactory.kt
@@ -6,29 +6,31 @@
 
 package com.datadog.android.flags.utils.forge
 
-import com.datadog.android.flags.model.BatchedFlagEvaluations
+import com.datadog.android.flags.model.BatchedFlagEvaluations.Error
+import com.datadog.android.flags.model.BatchedFlagEvaluations.FlagEvaluation
+import com.datadog.android.flags.model.BatchedFlagEvaluations.Identifier
 import com.datadog.android.flags.model.ResolutionReason
 import fr.xgouchet.elmyr.Forge
 import fr.xgouchet.elmyr.ForgeryFactory
 
-internal class FlagEvaluationForgeryFactory : ForgeryFactory<BatchedFlagEvaluations.FlagEvaluation> {
-    override fun getForgery(forge: Forge): BatchedFlagEvaluations.FlagEvaluation {
+internal class FlagEvaluationForgeryFactory : ForgeryFactory<FlagEvaluation> {
+    override fun getForgery(forge: Forge): FlagEvaluation {
         val reason = forge.aValueFrom(ResolutionReason::class.java)
         val isDefaultOrError = reason == ResolutionReason.DEFAULT || reason == ResolutionReason.ERROR
         val hasError = forge.aBool() && (reason == ResolutionReason.ERROR || forge.aBool())
         val firstEvalTimestamp = forge.aPositiveLong()
         val lastEvalTimestamp = firstEvalTimestamp + forge.aLong(min = 0, max = 10_000)
 
-        return BatchedFlagEvaluations.FlagEvaluation(
+        return FlagEvaluation(
             timestamp = firstEvalTimestamp,
-            flag = BatchedFlagEvaluations.Identifier(forge.anAlphabeticalString()),
+            flag = Identifier(forge.anAlphabeticalString()),
             variant = if (!isDefaultOrError) {
-                BatchedFlagEvaluations.Identifier(forge.anAlphabeticalString())
+                Identifier(forge.anAlphabeticalString())
             } else {
                 null
             },
             allocation = if (!isDefaultOrError) {
-                BatchedFlagEvaluations.Identifier(forge.anAlphabeticalString())
+                Identifier(forge.anAlphabeticalString())
             } else {
                 null
             },
@@ -36,7 +38,7 @@ internal class FlagEvaluationForgeryFactory : ForgeryFactory<BatchedFlagEvaluati
             targetingKey = forge.aNullable { forge.anAlphabeticalString() },
             context = null,
             error = if (hasError) {
-                BatchedFlagEvaluations.Error(message = forge.anAlphabeticalString())
+                Error(message = forge.anAlphabeticalString())
             } else {
                 null
             },
