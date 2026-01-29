ca02fe17b7616f441a44378bbe6a3f8ebbb22688
Aleksandr Gringauz <aleksandr.gringauz@datadoghq.com>
2026-01-29 16:14:54 +0100

    remove code about dictionary

--- zstd/src/main/cpp/jni_zstd.c
+++ zstd/src/main/cpp/jni_zstd.c
@@ -1,15 +1,11 @@
-#ifndef ZSTD_STATIC_LINKING_ONLY
-#define ZSTD_STATIC_LINKING_ONLY
-#endif
 #include <jni.h>
 #include <zstd.h>
 #include <zstd_errors.h>
-#include <stdint.h>
 
 /*
  * Class:     com_github_luben_zstd_Zstd
  * Method:    isError
- * Signature: (J)I
+ * Signature: (J)Z
  */
 JNIEXPORT jboolean JNICALL Java_com_github_luben_zstd_Zstd_isError
   (JNIEnv *env, jclass obj, jlong code) {
@@ -39,186 +35,10 @@ JNIEXPORT jlong JNICALL Java_com_github_luben_zstd_Zstd_getErrorCode
 
 /*
  * Class:     com_github_luben_zstd_Zstd
- * Method:    loadDictCompress
- * Signature: (J[BI)I
+ * Method:    errMemoryAllocation
+ * Signature: ()J
  */
-JNIEXPORT jint JNICALL Java_com_github_luben_zstd_Zstd_loadDictCompress
-  (JNIEnv *env, jclass obj, jlong stream, jbyteArray dict, jint dict_size) {
-    if (dict == NULL) return -ZSTD_error_dictionary_wrong;
-    size_t size = -ZSTD_error_memory_allocation;
-    void *dict_buff = (*env)->GetPrimitiveArrayCritical(env, dict, NULL);
-    if (dict_buff == NULL) goto E1;
-
-    size = ZSTD_CCtx_loadDictionary((ZSTD_CCtx *)(intptr_t) stream, dict_buff, dict_size);
-E1:
-    (*env)->ReleasePrimitiveArrayCritical(env, dict, dict_buff, JNI_ABORT);
-    return size;
-}
-
-/*
- * Class:     com_github_luben_zstd_Zstd
- * Method:    loadFastDictCompress
- * Signature: (J)I
- */
-JNIEXPORT jint JNICALL Java_com_github_luben_zstd_Zstd_loadFastDictCompress
-  (JNIEnv *env, jclass obj, jlong stream, jobject dict) {
-    if (dict == NULL) return -ZSTD_error_dictionary_wrong;
-    jclass dict_clazz = (*env)->GetObjectClass(env, dict);
-    jfieldID compress_dict = (*env)->GetFieldID(env, dict_clazz, "nativePtr", "J");
-    ZSTD_CDict* cdict = (ZSTD_CDict*)(intptr_t)(*env)->GetLongField(env, dict, compress_dict);
-    if (cdict == NULL) return -ZSTD_error_dictionary_wrong;
-    return ZSTD_CCtx_refCDict((ZSTD_CCtx *)(intptr_t) stream, cdict);
-}
-
-/*
- * Class:     com_github_luben_zstd_Zstd
- * Method:    setCompressionChecksums
- * Signature: (JZ)I
- */
-JNIEXPORT jint JNICALL Java_com_github_luben_zstd_Zstd_setCompressionChecksums
-  (JNIEnv *env, jclass obj, jlong stream, jboolean enabled) {
-    int checksum = enabled ? 1 : 0;
-    return ZSTD_CCtx_setParameter((ZSTD_CCtx *)(intptr_t) stream, ZSTD_c_checksumFlag, checksum);
-}
-
-/*
- * Class:     com_github_luben_zstd_Zstd
- * Method:    setCompressionLevel
- * Signature: (JI)I
- */
-JNIEXPORT jint JNICALL Java_com_github_luben_zstd_Zstd_setCompressionLevel
-  (JNIEnv *env, jclass obj, jlong stream, jint level) {
-    return ZSTD_CCtx_setParameter((ZSTD_CCtx *)(intptr_t) stream, ZSTD_c_compressionLevel, level);
-}
-
-/*
- * Class:     com_github_luben_zstd_Zstd
- * Method:    setCompressionLong
- * Signature: (JI)I
- */
-JNIEXPORT jint JNICALL Java_com_github_luben_zstd_Zstd_setCompressionLong
-  (JNIEnv *env, jclass obj, jlong stream, jint windowLog) {
-    ZSTD_CCtx* cctx = (ZSTD_CCtx*)(intptr_t) stream;
-    if (windowLog < ZSTD_WINDOWLOG_MIN || windowLog > ZSTD_WINDOWLOG_LIMIT_DEFAULT) {
-      // disable long matching and reset to default windowLog size
-      ZSTD_CCtx_setParameter(cctx, ZSTD_c_enableLongDistanceMatching, ZSTD_ps_disable);
-      ZSTD_CCtx_setParameter(cctx, ZSTD_c_windowLog, 0);
-    } else {
-      ZSTD_CCtx_setParameter(cctx, ZSTD_c_enableLongDistanceMatching, ZSTD_ps_enable);
-      ZSTD_CCtx_setParameter(cctx, ZSTD_c_windowLog, windowLog);
-    }
-    return 0;
-}
-
-/*
- * Class:     com_github_luben_zstd_Zstd
- * Method:    setCompressionWorkers
- * Signature: (JI)I
- */
-JNIEXPORT jint JNICALL Java_com_github_luben_zstd_Zstd_setCompressionWorkers
-  (JNIEnv *env, jclass obj, jlong stream, jint workers) {
-    return ZSTD_CCtx_setParameter((ZSTD_CCtx *)(intptr_t) stream, ZSTD_c_nbWorkers, workers);
-}
-
-/*
- * Class:     com_github_luben_zstd_Zstd
- * Method:    setCompressionJobSize
- * Signature: (JI)I
- */
-JNIEXPORT jint JNICALL Java_com_github_luben_zstd_Zstd_setCompressionJobSize
-  (JNIEnv *env, jclass obj, jlong stream, jint jobSize) {
-    return ZSTD_CCtx_setParameter((ZSTD_CCtx *)(intptr_t) stream, ZSTD_c_jobSize, jobSize);
-}
-
-/*
- * Class:     com_github_luben_zstd_Zstd
- * Method:    setCompressionOverlapLog
- * Signature: (JI)I
- */
-JNIEXPORT jint JNICALL Java_com_github_luben_zstd_Zstd_setCompressionOverlapLog
-  (JNIEnv *env, jclass obj, jlong stream, jint overlapLog) {
-    return ZSTD_CCtx_setParameter((ZSTD_CCtx *)(intptr_t) stream, ZSTD_c_overlapLog, overlapLog);
-}
-
-/*
- * Class:     com_github_luben_zstd_Zstd
- * Method:    setCompressionWindowLog
- * Signature: (JI)I
- */
-JNIEXPORT jint JNICALL Java_com_github_luben_zstd_Zstd_setCompressionWindowLog
-  (JNIEnv *env, jclass obj, jlong stream, jint windowLog) {
-    return ZSTD_CCtx_setParameter((ZSTD_CCtx *)(intptr_t) stream, ZSTD_c_windowLog, windowLog);
-}
-
-/*
- * Class:     com_github_luben_zstd_Zstd
- * Method:    setCompressionHashLog
- * Signature: (JI)I
- */
-JNIEXPORT jint JNICALL Java_com_github_luben_zstd_Zstd_setCompressionHashLog
-  (JNIEnv *env, jclass obj, jlong stream, jint hashLog) {
-    return ZSTD_CCtx_setParameter((ZSTD_CCtx *)(intptr_t) stream, ZSTD_c_hashLog, hashLog);
-}
-
-/*
- * Class:     com_github_luben_zstd_Zstd
- * Method:    setCompressionChainLog
- * Signature: (JI)I
- */
-JNIEXPORT jint JNICALL Java_com_github_luben_zstd_Zstd_setCompressionChainLog
-  (JNIEnv *env, jclass obj, jlong stream, jint chainLog) {
-    return ZSTD_CCtx_setParameter((ZSTD_CCtx *)(intptr_t) stream, ZSTD_c_chainLog, chainLog);
-}
-
-/*
- * Class:     com_github_luben_zstd_Zstd
- * Method:    setCompressionSearchLog
- * Signature: (JI)I
- */
-JNIEXPORT jint JNICALL Java_com_github_luben_zstd_Zstd_setCompressionSearchLog
-  (JNIEnv *env, jclass obj, jlong stream, jint searchLog) {
-    return ZSTD_CCtx_setParameter((ZSTD_CCtx *)(intptr_t) stream, ZSTD_c_searchLog, searchLog);
-}
-
-/*
- * Class:     com_github_luben_zstd_Zstd
- * Method:    setCompressionMinMatch
- * Signature: (JI)I
- */
-JNIEXPORT jint JNICALL Java_com_github_luben_zstd_Zstd_setCompressionMinMatch
-  (JNIEnv *env, jclass obj, jlong stream, jint minMatch) {
-    return ZSTD_CCtx_setParameter((ZSTD_CCtx *)(intptr_t) stream, ZSTD_c_minMatch, minMatch);
-}
-
-/*
- * Class:     com_github_luben_zstd_Zstd
- * Method:    setCompressionTargetLength
- * Signature: (JI)I
- */
-JNIEXPORT jint JNICALL Java_com_github_luben_zstd_Zstd_setCompressionTargetLength
-  (JNIEnv *env, jclass obj, jlong stream, jint targetLength) {
-    return ZSTD_CCtx_setParameter((ZSTD_CCtx *)(intptr_t) stream, ZSTD_c_targetLength, targetLength);
-}
-
-/*
- * Class:     com_github_luben_zstd_Zstd
- * Method:    setCompressionStrategy
- * Signature: (JI)I
- */
-JNIEXPORT jint JNICALL Java_com_github_luben_zstd_Zstd_setCompressionStrategy
-  (JNIEnv *env, jclass obj, jlong stream, jint strategy) {
-    return ZSTD_CCtx_setParameter((ZSTD_CCtx *)(intptr_t) stream, ZSTD_c_strategy, strategy);
-}
-
-JNIEXPORT jint JNICALL Java_com_github_luben_zstd_Zstd_defaultCompressionLevel
+JNIEXPORT jlong JNICALL Java_com_github_luben_zstd_Zstd_errMemoryAllocation
   (JNIEnv *env, jclass obj) {
-    return ZSTD_CLEVEL_DEFAULT;
+    return ZSTD_error_memory_allocation;
 }
-
-#define JNI_ZSTD_ERROR(err, name) \
-  JNIEXPORT jlong JNICALL Java_com_github_luben_zstd_Zstd_err##name \
-    (JNIEnv *env, jclass obj) { \
-      return ZSTD_error_##err; \
-  }
-
-JNI_ZSTD_ERROR(memory_allocation,             MemoryAllocation)
--- zstd/src/main/java/com/github/luben/zstd/Zstd.java
+++ zstd/src/main/java/com/github/luben/zstd/Zstd.java
@@ -8,25 +8,6 @@ public class Zstd {
         Native.load();
     }
 
-    public static native int setCompressionLevel(long stream, int level);
-    public static native int setCompressionChecksums(long stream, boolean useChecksums);
-    public static native int setCompressionLong(long stream, int windowLog);
-    public static native int setCompressionWorkers(long stream, int workers);
-    public static native int setCompressionOverlapLog(long stream, int overlapLog);
-    public static native int setCompressionJobSize(long stream, int jobSize);
-    public static native int setCompressionTargetLength(long stream, int targetLength);
-    public static native int setCompressionMinMatch(long stream, int minMatch);
-    public static native int setCompressionSearchLog(long stream, int searchLog);
-    public static native int setCompressionChainLog(long stream, int chainLog);
-    public static native int setCompressionHashLog(long stream, int hashLog);
-    public static native int setCompressionWindowLog(long stream, int windowLog);
-    public static native int setCompressionStrategy(long stream, int strategy);
-
-    public static native int loadDictCompress(long stream, byte[] dict, int dict_size);
-    public static native int loadFastDictCompress(long stream, ZstdDictCompress dict);
-
-    public static native int defaultCompressionLevel();
-
     public static native boolean isError(long code);
     public static native String  getErrorName(long code);
     public static native long    getErrorCode(long code);
