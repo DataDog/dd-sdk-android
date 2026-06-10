aea05eb1f3f7c6a0179351a87e5153128a65a530
jonathanmos <48201295+jonathanmos@users.noreply.github.com>
2026-06-10 13:43:05 +0300

    PANA-5681: Add heatmaps feature flag

--- features/dd-sdk-android-session-replay/src/main/kotlin/com/datadog/android/sessionreplay/internal/DefaultRecorderProvider.kt
+++ features/dd-sdk-android-session-replay/src/main/kotlin/com/datadog/android/sessionreplay/internal/DefaultRecorderProvider.kt
@@ -66,7 +66,8 @@ internal class DefaultRecorderProvider(
     private val customOptionSelectorDetectors: List<OptionSelectorDetector>,
     private val customDrawableMappers: List<DrawableToColorMapper>,
     private val dynamicOptimizationEnabled: Boolean,
-    private val internalCallback: SessionReplayInternalCallback
+    private val internalCallback: SessionReplayInternalCallback,
+    private val heatmapsEnabled: Boolean
 ) : RecorderProvider {
 
     override fun provideSessionReplayRecorder(
@@ -92,7 +93,7 @@ internal class DefaultRecorderProvider(
             sdkCore = sdkCore,
             dynamicOptimizationEnabled = dynamicOptimizationEnabled,
             internalCallback = internalCallback,
-            heatmapIdentifierRegistry = LazyHeatmapIdentifierRegistry(sdkCore)
+            heatmapIdentifierRegistry = if (heatmapsEnabled) LazyHeatmapIdentifierRegistry(sdkCore) else null
         )
     }
 
