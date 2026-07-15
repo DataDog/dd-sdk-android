214860fb54d288d642bcb396ca035dade124afad
Sasha Gringauz <aleksandr.gringauz@datadoghq.com>
2026-07-15 14:24:36 +0200

    RUM-16305: Change some hard-coded data limits

--- dd-sdk-android-core/src/main/kotlin/com/datadog/android/core/internal/persistence/file/FilePersistenceConfig.kt
+++ dd-sdk-android-core/src/main/kotlin/com/datadog/android/core/internal/persistence/file/FilePersistenceConfig.kt
@@ -19,11 +19,11 @@ internal data class FilePersistenceConfig(
     val cleanupFrequencyThreshold: Long = CLEANUP_FREQUENCY_THRESHOLD_MS
 ) {
     companion object {
-        internal const val MAX_BATCH_SIZE: Long = 4L * 1024 * 1024 // 4 MB
-        internal const val MAX_ITEMS_PER_BATCH: Int = 500
-        internal const val MAX_ITEM_SIZE: Long = 512L * 1024 // 512 KB
+        internal const val MAX_BATCH_SIZE: Long = 5L * 1024 * 1024 // 5 MB
+        internal const val MAX_ITEMS_PER_BATCH: Int = 1000
+        internal const val MAX_ITEM_SIZE: Long = 1024L * 1024 // 1 MB
         internal const val OLD_FILE_THRESHOLD: Long = 18L * 60L * 60L * 1000L // 18 hours
-        internal const val MAX_DISK_SPACE: Long = 128 * MAX_BATCH_SIZE // 512 MB
+        internal const val MAX_DISK_SPACE: Long = 128 * MAX_BATCH_SIZE // 640 MB
         internal const val MAX_DELAY_BETWEEN_MESSAGES_MS = 5000L
         internal const val CLEANUP_FREQUENCY_THRESHOLD_MS = 5000L // 5s
     }
